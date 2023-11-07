98712884648ee9addb397624a548f921a6495c04
Daniel Clarke <u8sand@gmail.com>
2023-11-07 15:03:37 -0500

    Add developer user

--- drc-portals/lib/auth/index.ts
+++ drc-portals/lib/auth/index.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { NextAuthOptions } from 'next-auth'
 import prisma from '@/lib/prisma'
+import CredentialsProvider from 'next-auth/providers/credentials'
 import EmailProvider from 'next-auth/providers/email'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
@@ -8,6 +9,24 @@ import ORCIDProvider from '@/lib/auth/providers/orcid'
 import type { OAuthConfig } from 'next-auth/providers/index'
 
 const providers = [
+  /* In development, we'll add a button for creating a random user */
+  process.env.NODE_ENV === 'development' ? CredentialsProvider({
+    name: 'Developer Account',
+    credentials: {},
+    async authorize(credentials, req) {
+      const user = await prisma.user.upsert({
+        create: {
+          id: process.env.NEXTAUTH_SECRET ?? '',
+          name: 'Developer',
+        },
+        where: {
+          id: process.env.NEXTAUTH_SECRET ?? '',
+        },
+        update: {},
+      })
+      return user
+    }
+  }) : undefined,
   process.env.NEXTAUTH_EMAIL ? EmailProvider(JSON.parse(process.env.NEXTAUTH_EMAIL)) : undefined,
   process.env.NEXTAUTH_GITHUB ? GithubProvider(JSON.parse(process.env.NEXTAUTH_GITHUB)) : undefined,
   process.env.NEXTAUTH_GOOGLE ? GoogleProvider(JSON.parse(process.env.NEXTAUTH_GOOGLE)) : undefined,
@@ -17,4 +36,5 @@ const providers = [
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers,
+  session: { strategy: 'jwt' },
 }
