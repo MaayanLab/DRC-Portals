d4f9a2da9d3d820e47a41859d6a5a431c8670f8d
John Erol Evangelista <je.evangelista04@gmail.com>
2023-11-10 11:38:13 -0500

    returned info

---
+++ drc-portals/app/info/layout.tsx
@@ -0,0 +1,20 @@
+import Grid  from '@mui/material/Grid'
+import Container  from '@mui/material/Container'
+import Header  from '@/components/Header/info'
+import Footer  from '@/components/Footer/info'
+
+
+
+export default function InfoLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <Grid container justifyContent={'space-between'} direction={"column"} spacing={2}>
+      <Grid item><Header/></Grid>
+      <Grid item><Container maxWidth="lg">{children}</Container></Grid>
+      <Grid item><Footer/></Grid>
+    </Grid>
+  )
+}
--- drc-portals/app/layout.tsx
+++ drc-portals/app/layout.tsx
@@ -1,21 +1,8 @@
 import type { Metadata } from 'next'
-import Link from 'next/link'
-import Image from 'next/image'
-import dynamic from 'next/dynamic'
 import { Inter } from 'next/font/google'
 import NextAuthProvider from '@/lib/auth/client'
 import ThemeRegistry from './ThemeRegistry'
 import './globals.css'
-import { Typography } from '@mui/material'
-import { mdiGithub, mdiBugOutline} from '@mdi/js';
-
-const Icon = dynamic(()=>import('@mdi/react'))
-const Grid = dynamic(()=>import('@mui/material/Grid'))
-const Paper = dynamic(()=>import('@mui/material/Paper'))
-const Container = dynamic(()=>import('@mui/material/Container'))
-const Stack = dynamic(()=>import('@mui/material/Stack'))
-const Header = dynamic(()=>import('../components/Header/info'))
-const Footer = dynamic(()=>import('../components/Footer/info'))
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -43,11 +30,7 @@ export default function RootLayout({
         <ThemeRegistry options={{ key: 'mui' }}>
           <NextAuthProvider>
             
-              <Grid container justifyContent={'space-between'} direction={"column"} spacing={2}>
-                <Grid item><Header/></Grid>
-                <Grid item><Container maxWidth="lg">{children}</Container></Grid>
-                <Grid item><Footer/></Grid>
-              </Grid>
+              {children}
           </NextAuthProvider>
         </ThemeRegistry>
       </body>
