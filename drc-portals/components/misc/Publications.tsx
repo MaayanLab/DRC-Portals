4c4a1fb69ee53771e4d30a2c0455810388b882f2
John Erol Evangelista <je.evangelista04@gmail.com>
2023-11-15 16:40:59 -0500

    mod publications

--- drc-portals/components/misc/Publications.tsx
+++ drc-portals/components/misc/Publications.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
 import Button from "@mui/material/Button"
+import Chip from "@mui/material/Chip"
 
 import prisma from '@/lib/prisma'
 
@@ -14,7 +15,14 @@ async function Publications({all=false}: {all:boolean}) {
               {
                 year: 'desc'
               }
-            ]
+            ],
+            include: {
+              dccs: {
+                include: {
+                  dcc: true
+                }
+              }
+            },
           })
     } else {
         publications = await prisma.publication.findMany({
@@ -23,19 +31,50 @@ async function Publications({all=false}: {all:boolean}) {
                 year: 'desc'
               }
             ],
+            include: {
+              dccs: {
+                include: {
+                  dcc: true
+                }
+              }
+            },
             take: 5,
           })
     }
     return (
         <Paper sx={{boxShadow: "none", height: "100%"}}>
-            <Typography variant="h2" className="mb-5">Publications</Typography>
+            <Typography variant="h2">Publications</Typography>
+            {all &&
+              <div className="mb-5">
+                <Typography variant="subtitle1">
+                  Discover and cite publications from the Common Fund Data Ecosystem Programs, Data Coordination Centers and community.
+                </Typography>
+              </div>
+            }
             {publications.map((pub, i)=>(
-            <div key={i} style={{marginBottom: 10}}>
-                <Link href={pub.doi || ''}>
+            <div key={i} className="mb-2 space-x-1">
                 <Typography variant="caption">
                     {pub.authors}. {pub.year}. <b>{pub.title}.</b> {pub.journal}. {pub.volume}. {pub.page}
                 </Typography>
-                </Link>
+                { all &&
+                <>
+                  { pub.pmid && 
+                    <Link target="_blank" rel="noopener noreferrer" href={`https://pubmed.ncbi.nlm.nih.gov/${pub.pmid}/`}>
+                      <Chip color="secondary" variant="outlined" label={"PubMed"} sx={{minWidth: 100}}/>
+                    </Link>
+                  }
+                  { pub.pmcid && 
+                    <Link target="_blank" rel="noopener noreferrer" href={`https://www.ncbi.nlm.nih.gov/pmc/articles/${pub.pmcid}/`}>
+                      <Chip color="secondary" variant="outlined"  label={"PMC"} sx={{minWidth: 100}}/>
+                    </Link>
+                  }
+                  { pub.doi && 
+                    <Link target="_blank" rel="noopener noreferrer" href={`https://www.doi.org/${pub.doi}`}>
+                      <Chip color="secondary" variant="outlined" label={"DOI"} sx={{minWidth: 100}}/>
+                    </Link>
+                  }
+                </>
+                }
             </div>
             ))}
             {!all && <Link href="/info/publications"><Button color="secondary" variant="outlined">Show More</Button></Link>}
