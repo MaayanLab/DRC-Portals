e000ef4a9d774711a81f5df1f81b12b3aaca97c4
John Erol Evangelista <je.evangelista04@gmail.com>
2023-11-15 16:35:46 -0500

    added indicators in carousel

--- drc-portals/components/misc/Carousel/ClientCarousel.tsx
+++ drc-portals/components/misc/Carousel/ClientCarousel.tsx
@@ -1,13 +1,18 @@
 'use client'
 import Carousel from 'react-material-ui-carousel'
-
+import Typography from "@mui/material/Typography"
 
 
 export default function ClientCarousel({children}: {children: React.ReactNode}) {
     return (
-      <Carousel sx={{height: 400, width:400}}>
-          {children}
-      </Carousel>
+      <div className='flex flex-col text-center'>
+        <Carousel sx={{height: 500, width:400}} indicators={true}>
+            {children}
+        </Carousel>
+        <Typography variant="subtitle1" color="primary">
+          PRODUCTS & PARTNERSHIPS
+        </Typography>
+      </div>
     )
   }
   
