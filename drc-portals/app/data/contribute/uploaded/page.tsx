6f9feacbc76c3d3a807bbcfd4446fa64f9d3a8d5
Daniel Clarke <u8sand@gmail.com>
2023-11-15 17:57:15 -0500

    Fixup contribute/uploaded

--- drc-portals/app/data/contribute/uploaded/page.tsx
+++ drc-portals/app/data/contribute/uploaded/page.tsx
@@ -1,77 +1,74 @@
-'use client'
 import * as React from 'react';
-import FilesTable from '@/components/FilesTable';
 import Container from '@mui/material/Container'
-import { BsCheckCircleFill, BsCheckCircle } from "react-icons/bs";
-import { ImNotification } from "react-icons/im";
+// import { BsCheckCircleFill, BsCheckCircle } from "react-icons/bs";
 import { Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
-import { useSession } from 'next-auth/react';
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import prisma from '@/lib/prisma';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-
-function createData(
-    date: string,
-    uploadedBy: string,
-    email: string,
-    dcc: string,
-    fileType: string,
-    uploadedFile: string,
-    status: React.JSX.Element,
-) {
-    return { date, uploadedBy, email, dcc, fileType, uploadedFile, status };
-}
-
-
-function UserFiles(props: any) {
-    const { data: session, status } = useSession();
-    const [rowData, setRowData] = useState<any[]>([])
-    const [dataLoaded, setDataLoaded] = useState<boolean>(false)
-
-
-
-
-
-    useEffect(() => {
-        fetch('/api/getuserfiles')
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(JSON.parse(data.message)[0])
-                let sessionEmail = ''
-                console.log(session?.user.email)
-                if (session?.user.email != null) {
-                    sessionEmail = session?.user.email
-                }
-                console.log(sessionEmail)
-                let mappedData = JSON.parse(data.message).map((row : any) => {
-                    return createData(row.lastmodified.toString().split('T')[0], row.creator, sessionEmail, 'LINCS', row.filetype, 'S3 bucket link', <ImNotification />)
-                })
-                console.log(mappedData)
-                setRowData(mappedData)
-                setDataLoaded(true)
-            })
-    }, [])
-
-    console.log(rowData)
-
-
-
-    if (!dataLoaded) return <p>Loading...</p>
-
+export default async function UserFiles(props: any) {
+    const session = await getServerSession(authOptions)
+    const user = await prisma.user.findFirst({
+        where: {
+            id: session?.user.id,
+        },
+    })
+    if (!user?.name) throw new Error('Missing name')
+    const userFiles = await prisma.dccAsset.findMany({
+        where: {
+            creator: user?.name,
+        },
+    })
+    let sessionEmail = ''
+    if (session?.user.email != null) {
+        sessionEmail = session?.user.email
+    }
 
     return (
         <>
             <Container className="mt-10 justify-content-center">
                 <Typography variant="h3" className='text-center p-5'>Uploaded Files</Typography>
-                <FilesTable rows={rowData} />
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                        <TableCell>Date Uploaded</TableCell>
+                        <TableCell align="center">Uploaded By</TableCell>
+                        <TableCell align="center">DCC</TableCell>
+                        <TableCell align="center">File Type</TableCell>
+                        <TableCell align="center">Uploaded File</TableCell>
+                        <TableCell align="center">Additional Info</TableCell>
+                        <TableCell align="center">Status</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {userFiles.map((row) => (
+                            <TableRow
+                                key={row.link}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">{row.lastmodified.toString().split('T')[0]}</TableCell>
+                                <TableCell align="right">{row.creator}</TableCell>
+                                <TableCell align="right">{row.dcc_id}</TableCell>
+                                <TableCell align="right">{row.filetype}</TableCell>
+                                <TableCell align="right">{row.filename}</TableCell>
+                                <TableCell align="right"></TableCell>
+                                <TableCell align="right">{row.approved}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                    </Table>
+                </TableContainer>
             </Container>
 
 
         </>
     );
 }
-
-export default UserFiles;
-
-
-
-
