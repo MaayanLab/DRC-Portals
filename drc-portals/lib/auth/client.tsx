cb17477fb09538a17f638a8584c3378f08ba483c
Daniel Clarke <u8sand@gmail.com>
2023-11-15 11:24:07 -0500

    Use full url for NEXTAUTH_URL

--- drc-portals/lib/auth/client.tsx
+++ drc-portals/lib/auth/client.tsx
@@ -4,5 +4,5 @@ import React from 'react'
 import { SessionProvider } from 'next-auth/react'
 
 export default function NextAuthProvider({ children }: React.PropsWithChildren<{}>) {
-  return <SessionProvider basePath="/auth">{children}</SessionProvider>
+  return <SessionProvider basePath={process.env.NEXTAUTH_URL || '/auth'}>{children}</SessionProvider>
 }
