18d023ba601510d736b07fff97db62975211b314
stephanieolaiya <stephanieolaiya@gmail.com>
2023-11-16 15:32:45 -0500

    upload status example manifest file

---
+++ drc-portals/app/data/contribute/form/Status.tsx
@@ -0,0 +1,59 @@
+'use client'
+import { Alert, IconButton } from "@mui/material";
+import { useS3UploadStatus } from "./S3UploadForm";
+import CloseIcon from '@mui/icons-material/Close';
+import React, { useEffect, useState } from "react";
+
+export default function Status() {
+    let status = useS3UploadStatus();
+    const [open, setOpen] = useState<boolean>(false)
+    useEffect(
+        () => { setOpen(true) },
+        [status]
+    );
+
+    return (
+        <>
+            {open && status.error && <Alert severity="error"
+                action={
+                    <IconButton
+                        aria-label="close"
+                        color="inherit"
+                        size="small"
+                        onClick={() => {
+                            setOpen(false);
+                        }}
+                    >
+                        <CloseIcon fontSize="inherit" />
+                    </IconButton>
+                }>Error! Please make sure manifest.json file and metadata files are formatted properly</Alert>}
+            {open && status.loading && <Alert onClose={() => { }} severity="info"
+                action={
+                    <IconButton
+                        aria-label="close"
+                        color="inherit"
+                        size="small"
+                        onClick={() => {
+                            setOpen(false);
+                        }}
+                    >
+                        <CloseIcon fontSize="inherit" />
+                    </IconButton>
+                }>Upload in Progress!</Alert>}
+            {open && status.success && <Alert onClose={() => { }}
+                action={
+                    <IconButton
+                        aria-label="close"
+                        color="inherit"
+                        size="small"
+                        onClick={() => {
+                            setOpen(false);
+                        }}
+                    >
+                        <CloseIcon fontSize="inherit" />
+                    </IconButton>
+                }>Success! File Uploaded</Alert>}
+        </>
+
+    )
+}
