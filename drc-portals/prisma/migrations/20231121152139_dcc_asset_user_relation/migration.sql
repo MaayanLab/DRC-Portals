bfde6d9cda25802fac6e14df80b93396ca9b29fe
Daniel Clarke <u8sand@gmail.com>
2023-11-21 17:50:27 -0500

    Fixup typo in manual migration

--- drc-portals/prisma/migrations/20231121152139_dcc_asset_user_relation/migration.sql
+++ drc-portals/prisma/migrations/20231121152139_dcc_asset_user_relation/migration.sql
@@ -2,7 +2,7 @@
 ALTER TABLE "dcc_assets" ALTER COLUMN "creator" DROP NOT NULL;
 
 -- Fixup
-update "dcc_assets" where "creator" = 'DCC' set "creator" = null;
+update "dcc_assets" set "creator" = null where "creator" = 'DCC';
 
 -- AddForeignKey
 ALTER TABLE "dcc_assets" ADD CONSTRAINT "dcc_assets_creator_fkey" FOREIGN KEY ("creator") REFERENCES "User"("email") ON DELETE SET NULL ON UPDATE CASCADE;
