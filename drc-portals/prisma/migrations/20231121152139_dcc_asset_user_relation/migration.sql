b29e65019ed6b6b9251041742388eedf8339cc17
Daniel Clarke <u8sand@gmail.com>
2023-11-21 17:13:59 -0500

    Fixup migrations

--- drc-portals/prisma/migrations/20231121152139_dcc_asset_user_relation/migration.sql
+++ drc-portals/prisma/migrations/20231121152139_dcc_asset_user_relation/migration.sql
@@ -1,2 +1,8 @@
+-- AlterTable
+ALTER TABLE "dcc_assets" ALTER COLUMN "creator" DROP NOT NULL;
+
+-- Fixup
+update "dcc_assets" where "creator" = 'DCC' set "creator" = null;
+
 -- AddForeignKey
-ALTER TABLE "dcc_assets" ADD CONSTRAINT "dcc_assets_creator_fkey" FOREIGN KEY ("creator") REFERENCES "User"("email") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "dcc_assets" ADD CONSTRAINT "dcc_assets_creator_fkey" FOREIGN KEY ("creator") REFERENCES "User"("email") ON DELETE SET NULL ON UPDATE CASCADE;
