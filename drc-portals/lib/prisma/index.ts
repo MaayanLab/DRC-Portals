23543166cec381efc7c933ff56890ebe81ad84ff
Daniel Clarke <u8sand@gmail.com>
2023-11-22 10:50:05 -0500

    Log db queries in development

--- drc-portals/lib/prisma/index.ts
+++ drc-portals/lib/prisma/index.ts
@@ -1,4 +1,20 @@
 import { PrismaClient } from '@prisma/client'
 import singleton from '@/lib/singleton'
 
-export default singleton('prisma', () => new PrismaClient())
+export default singleton('prisma', () => {
+  if (process.env.NODE_ENV === 'development') {
+    const prisma = new PrismaClient({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+      ],
+    })
+    prisma.$on('query', async (e) => {
+        console.log(`${e.query} ${e.params}`)
+    })
+    return prisma
+  }
+  return new PrismaClient()
+})
