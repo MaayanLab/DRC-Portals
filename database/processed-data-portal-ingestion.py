be971f661f899cae1140741f77d1324ef19c5156
Daniel Clarke <u8sand@gmail.com>
2023-11-22 12:06:44 -0500

    Add c2m2 datapackage file.tsv ingest

--- database/processed-data-portal-ingestion.py
+++ database/processed-data-portal-ingestion.py
@@ -4,6 +4,7 @@ import psycopg2
 import pathlib
 import csv
 import contextlib
+import zipfile
 from urllib.parse import urlparse
 from dotenv import load_dotenv
 from uuid import UUID, uuid5
@@ -222,3 +223,56 @@ with xentity_xset_helper.writer() as xentity_xset:
                       A=entity_id,
                       B=set_id,
                     ))
+
+#%%
+# Ingest C2M2
+
+c2m2s = dcc_assets[dcc_assets['filetype'] == 'C2M2']
+c2m2s_path = ingest_path / 'c2m2s'
+
+c2m2datapackage_helper = TableHelper('c2m2datapackage', ('id', 'dcc_asset_link',), pk_columns=('id',))
+c2m2file_helper = TableHelper('c2m2file', ('id', 'datapackage_id', 'creation_time', 'persistent_id', 'size_in_bytes', 'file_format', 'data_type', 'assay_type',), pk_columns=('id',))
+xidentity_helper = TableHelper('xidentity', ('id', 'type', 'label', 'description', 'c2m2file_id',), pk_columns=('id',))
+
+with c2m2file_helper.writer() as c2m2file:
+  with xidentity_helper.writer() as xidentity:
+    with c2m2datapackage_helper.writer() as c2m2datapackage:
+      for _, c2m2 in c2m2s.iterrows():
+        c2m2_path = c2m2s_path/c2m2['dcc_short_label']/c2m2['filename']
+        c2m2_path.parent.mkdir(parents=True, exist_ok=True)
+        if not c2m2_path.exists():
+          import urllib.request
+          urllib.request.urlretrieve(c2m2['link'], c2m2_path)
+        c2m2_extract_path = c2m2_path.parent / c2m2_path.stem
+        if not c2m2_extract_path.exists():
+          with zipfile.ZipFile(c2m2_path, 'r') as c2m2_zip:
+            c2m2_zip.extractall(c2m2_extract_path)
+        c2m2datapackage_id = str(uuid5(uuid0, c2m2['link']))
+        c2m2datapackage.writerow(dict(
+          id=c2m2datapackage_id,
+          dcc_asset_link=c2m2['link'],
+        ))
+        # ingest files for this datapackage
+        c2m2_extracted_files_tsv_path, = c2m2_extract_path.rglob('file.tsv')
+        # TODO: also ingest
+        with open(c2m2_extracted_files_tsv_path, 'r') as fr:
+          c2m2_files_reader = csv.DictReader(fr, fieldnames=next(fr).strip().split('\t'), delimiter='\t')
+          for file in c2m2_files_reader:
+            c2m2file_id = str(uuid5(uuid0, '\t'.join((c2m2datapackage_id, file['id_namespace'], file['local_id']))))
+            c2m2file.writerow(dict(
+              id=c2m2file_id,
+              datapackage_id=c2m2datapackage_id,
+              creation_time=file['creation_time'],
+              persistent_id=file['persistent_id'],
+              size_in_bytes=file['size_in_bytes'],
+              file_format=file['file_format'],
+              data_type=file['data_type'],
+              assay_type=file['assay_type'],
+            ))
+            xidentity.writerow(dict(
+              id=c2m2file_id,
+              type='c2m2file',
+              label=file['filename'],
+              description='TODO',
+              c2m2file_id=c2m2file_id,
+            ))
