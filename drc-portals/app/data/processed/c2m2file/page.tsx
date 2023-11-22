8199775ca11daa98c4b1cd280f4d2d87b6238b74
Daniel Clarke <u8sand@gmail.com>
2023-11-22 14:11:28 -0500

    Optimize c2m2 file search

--- drc-portals/app/data/processed/c2m2file/page.tsx
+++ drc-portals/app/data/processed/c2m2file/page.tsx
@@ -6,6 +6,7 @@ import FormPagination from "@/app/data/processed/FormPagination";
 import SearchField from "@/app/data/processed/SearchField";
 import Image from "next/image";
 import { format_description } from "@/app/data/processed/utils";
+import { Prisma } from "@prisma/client";
 
 const pageSize = 10
 
@@ -24,51 +25,66 @@ export default async function Page(props: { searchParams: Record<string, string 
   }).parse(props.searchParams)
   const offset = (searchParams.p - 1)*pageSize
   const limit = pageSize
-  const [items, count] = await prisma.$transaction([
-    prisma.c2M2File.findMany({
-      where: searchParams.q ? {
-        identity: {
-          OR: [{ label: { mode: 'insensitive', contains: searchParams.q } }, { description: { search: searchParams.q } }]
-        }
-      } : {},
-      select: {
-        id: true,
-        data_type: true,
-        assay_type: true,
-        identity: {
-          select: {
-            type: true,
-            label: true,
-            description: true,
-          }
-        },
-        datapackage: {
-          select: {
-            dcc_asset: {
-              select: {
-                dcc: {
-                  select: {
-                    short_label: true,
-                    icon: true,
-                  },
-                },
-              },
-            },
-          },
-        },
+  const [results] = await prisma.$queryRaw<Array<{
+    items: {
+      id: string,
+      data_type: string,
+      assay_type: string,
+      identity: {
+        type: string,
+        label: string,
+        description: string,
       },
-      skip: offset,
-      take: limit,
-    }),
-    prisma.c2M2File.count({
-      where: searchParams.q ? {
-        identity: {
-          OR: [{ label: { mode: 'insensitive', contains: searchParams.q } }, { description: { search: searchParams.q } }]
-        }
-      } : {},
-    }),
-  ])
-  const ps = Math.floor(count / pageSize) + 1
+      dcc: {
+        short_label: string,
+        icon: string,
+      } | null,
+    }[]
+    count: number,
+  }>>`
+    with results as (
+      select *
+      from "c2m2file"
+      inner join "xidentity" on "c2m2file"."id" = "xidentity"."c2m2file_id"
+      ${searchParams.q ? Prisma.sql`
+        where to_tsvector('english', "xidentity"."searchable") @@ to_tsquery('english', ${searchParams.q})
+        order by ts_rank_cd(to_tsvector('english', "xidentity"."searchable"), to_tsquery('english', ${searchParams.q})) desc
+      ` : Prisma.sql`
+        order by "c2m2file"."id"
+      `}
+    ), items as (
+      select
+        results."c2m2file_id" as id,
+        results."data_type",
+        results."assay_type",
+        jsonb_build_object(
+          'type', results."type",
+          'label', results."label",
+          'description', results."description"
+        ) as identity,
+        (
+          select jsonb_build_object(
+            'short_label', short_label,
+            'icon', icon
+          )
+          from "c2m2datapackage"
+          inner join "dcc_assets" on "c2m2datapackage"."dcc_asset_link" = "dcc_assets"."link"
+          inner join "dccs" on "dcc_assets"."dcc_id" = "dccs"."id"
+          where results."datapackage_id" = "c2m2datapackage"."id"
+        ) as dcc
+      from results
+      offset ${offset}
+      limit ${limit}
+    ), total_count as (
+      select count("c2m2file_id")::int as count
+      from results
+    )
+    select 
+      (select coalesce(jsonb_agg(items.*), '[]'::jsonb) from items) as items,
+      (select count from total_count) as count
+    ;
+  `
+  const ps = Math.floor(results.count / pageSize) + 1
   return (
     <Container component="form" action="" method="GET">
       <SearchField q={searchParams.q ?? ''} />
@@ -94,15 +110,15 @@ export default async function Page(props: { searchParams: Record<string, string 
             </TableRow>
           </TableHead>
           <TableBody>
-            {items.map(item => (
+            {results.items.map(item => (
                 <TableRow
                     key={item.id}
                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                 >
                     <TableCell className="w-4 relative">
-                      {item.datapackage.dcc_asset.dcc?.icon ?
-                        <Link href={`/data/matrix/${item.datapackage.dcc_asset.dcc.short_label}`}>
-                          <Image className="p-2 object-contain" src={item.datapackage.dcc_asset.dcc.icon} alt={item.datapackage.dcc_asset.dcc.short_label ?? ''} fill />
+                      {item.dcc?.icon ?
+                        <Link href={`/data/matrix/${item.dcc.short_label}`}>
+                          <Image className="p-2 object-contain" src={item.dcc.icon} alt={item.dcc.short_label ?? ''} fill />
                         </Link>
                         : null}
                     </TableCell>
