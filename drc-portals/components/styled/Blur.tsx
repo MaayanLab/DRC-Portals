cf200ad0ff1588b13b7e5ebcdae024cbeb34fb9a
John Erol Evangelista <je.evangelista04@gmail.com>
2023-11-29 17:36:03 -0500

    added blur

--- drc-portals/components/styled/Blur.tsx
+++ drc-portals/components/styled/Blur.tsx
@@ -9,3 +9,12 @@ export const BlurSmall = styled('div')({
     borderRadius: '352px',
     filter: 'blur(1000px)'
   });
+
+export const BlurBig = styled('div')({
+    background: '#C3E1E6',
+    opacity: 0.6,
+    width: '560px',
+    height: '560px',
+    borderRadius: '352px',
+    filter: 'blur(1000px)'
+  });
