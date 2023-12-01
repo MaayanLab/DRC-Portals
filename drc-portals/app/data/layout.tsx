0591f3e3cbc13017c51208547729867ebad0bde4
Daniel Clarke <u8sand@gmail.com>
2023-12-01 16:38:30 -0500

    Distinguish portal titles

--- drc-portals/app/data/layout.tsx
+++ drc-portals/app/data/layout.tsx
@@ -2,8 +2,11 @@ import Grid  from '@mui/material/Grid'
 import Container  from '@mui/material/Container'
 import Header  from '@/components/Header/data'
 import Footer  from '@/components/Footer/data'
+import { Metadata } from 'next'
 
-
+export const metadata: Metadata = {
+  title: 'CFDE Data Portal',
+}
 
 export default function DataLayout({
   children,
