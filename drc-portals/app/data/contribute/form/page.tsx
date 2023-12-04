f11b3ca2fd18c87352b7554006a9fb7d0d7a2caa
stephanieolaiya <stephanieolaiya@gmail.com>
2023-12-04 14:03:43 -0500

    fixes #47

--- drc-portals/app/data/contribute/form/page.tsx
+++ drc-portals/app/data/contribute/form/page.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import FormControl from '@mui/material/FormControl';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import { FileDrop } from './FileDrop'
-import ThemedBox from './ThemedBox';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
-import ThemedStack from './ThemedStack';
 import { S3UploadForm } from './S3UploadForm';
 import prisma from '@/lib/prisma';
 import { redirect } from 'next/navigation';
-import { Link } from '@mui/material';
-import Status from './Status';
-import DCCSelect from './DCCSelect';
+import { Alert } from '@mui/material';
+
 
 export default async function UploadForm() {
   const session = await getServerSession(authOptions)
@@ -28,54 +18,19 @@ export default async function UploadForm() {
   if (user === null ) return redirect("/auth/signin?callbackUrl=/data/contribute/form")
 
   if (!(user.role === 'UPLOADER' || user.role === 'DRC_APPROVER' || user.role === 'ADMIN')) {return <p>Access Denied. This page is only accessible to DCC Uploaders and DRC Approvers</p>}
-  if (!user.dcc) return redirect("/data/contribute/account")
+  if (!user.email) return ( 
+  <Alert severity="warning"> Email not updated on user account. Please enter email on the Accounts Page</Alert>
+  // redirect("/data/contribute/account")
+  );
+
+  if (!user.dcc) return ( 
+    <Alert severity="warning"> User has no affiliated DCCs. Please contact the DRC to update your information</Alert>
+    // redirect("/data/contribute/account")
+    );
 
 
   return (
     <S3UploadForm {...user}>
-      {/* <Container className="mt-10">
-        <Typography variant="h3" className='text-center p-5'>Data and Metadata Upload Form</Typography>
-        <Grid container spacing={4} className='p-5' justifyContent="center">
-          <ThemedBox>
-            <TextField
-              label="Uploader Name"
-              name='name'
-              disabled
-              defaultValue={user.name}
-              inputProps={{style: {fontSize: 16}}} // font size of input text
-              InputLabelProps={{style: {fontSize: 16}}} // font size of input label
-            />
-          </ThemedBox>
-          <ThemedBox>
-            <TextField
-              label="Email"
-              name='email'
-              disabled
-              defaultValue={user.email}
-              inputProps={{style: {fontSize: 16}}}
-              InputLabelProps={{style: {fontSize: 16}}}
-            />
-          </ThemedBox>
-          <ThemedBox>
-            <DCCSelect dccOptions={user.dcc} />
-          </ThemedBox>
-        </Grid>
-
-        <Typography  variant="subtitle1" className='text-center p-5'>Please upload a zipped file containing your data/metdata files and a manifest.json file detailing files information. See {' '}
-        <Link href='/example_manifest.json' color="secondary" download>manifest.json template</Link>
-        </Typography>
-        <ThemedStack>
-          <FileDrop name="files[]" />
-        </ThemedStack>
-        <Status />
-        <ThemedBox style={{ display: 'flex', justifyContent: 'center' }} className='p-5'>
-          <FormControl>
-            <Button variant="contained" color="tertiary" style={{ minWidth: '200px', maxHeight: '100px' }} type="submit" sx={{ marginTop: 2, marginBottom: 10 }}>
-              Submit Form
-            </Button>
-          </FormControl>
-        </ThemedBox>
-      </Container> */}
     </S3UploadForm>
   );
 }
