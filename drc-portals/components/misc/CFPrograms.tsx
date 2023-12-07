d8ac8bd72bbd4f51f0b3159c09fb471b1bbd452d
John Erol Evangelista <je.evangelista04@gmail.com>
2023-12-07 17:46:16 -0500

    mod cfprograms

--- drc-portals/components/misc/CFPrograms.tsx
+++ drc-portals/components/misc/CFPrograms.tsx
@@ -8,7 +8,7 @@ import prisma from '@/lib/prisma'
 
 
 
-async function CFPrograms({spacing=4, className}:{spacing: GridSize, className?: string | null}) {
+async function CFPrograms({spacing=4, className, baseEndpoint}:{spacing: GridSize, className?: string | null, baseEndpoint: string}) {
     const dccs = await prisma.dCC.findMany({
       where: {
         cfde_partner: true
@@ -19,7 +19,7 @@ async function CFPrograms({spacing=4, className}:{spacing: GridSize, className?:
         {dccs.map(dcc=>(
           <Grid item xs={6} md={spacing} key={dcc.short_label} className="flex items-center justify-center relative" sx={{height: 50, padding: 5, margin: 1}}>
             <Tooltip title={dcc.short_label} placement="top">
-              <Link href={`/info/dcc/${dcc.short_label}`}>
+              <Link href={`${baseEndpoint}/${dcc.short_label}`}>
                 <Image className={className || ''}  src={dcc.icon || ''} alt={dcc.id} fill={true} style={{objectFit: "contain"}}/>
               </Link>
             </Tooltip>
