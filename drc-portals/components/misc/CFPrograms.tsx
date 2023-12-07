17ebd73f4ff546ccb645e9b22b5f10ae2e264b66
John Erol Evangelista <je.evangelista04@gmail.com>
2023-12-07 17:03:17 -0500

    modified dcc icons

--- drc-portals/components/misc/CFPrograms.tsx
+++ drc-portals/components/misc/CFPrograms.tsx
@@ -15,16 +15,13 @@ async function CFPrograms({spacing=4, className}:{spacing: GridSize, className?:
       }
     })
     return (
-      <Grid container direction="row" spacing={2} justifyContent={"center"}>
+      <Grid container direction="row" spacing={2} justifyContent={"center"} sx={{marginTop: 5}}>
         {dccs.map(dcc=>(
-          <Grid item xs={6} md={spacing} key={dcc.short_label} className="flex items-center justify-center relative" sx={{height: 50, padding: 5, marginTop: 5}}>
-            <Tooltip title={dcc.short_label}>
+          <Grid item xs={6} md={spacing} key={dcc.short_label} className="flex items-center justify-center relative" sx={{height: 50, padding: 5, margin: 1}}>
+            <Tooltip title={dcc.short_label} placement="top">
               <Link href={`/info/dcc/${dcc.short_label}`}>
-                { (dcc.icon || "").indexOf(".svg") > -1 ?
-                  <Image className={className || ''}  src={dcc.icon || ''} alt={dcc.id} width={120} height={120}/> :
-                  <Image className={className || ''}  src={dcc.icon || ''} alt={dcc.id} fill={true} style={{objectFit: "contain"}}/>
-                }
-                </Link>
+                <Image className={className || ''}  src={dcc.icon || ''} alt={dcc.id} fill={true} style={{objectFit: "contain"}}/>
+              </Link>
             </Tooltip>
           </Grid>
         ))}
