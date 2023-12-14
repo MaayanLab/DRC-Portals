557a27af3324fd32dac9a8d928a5b244e1b35fe6
John Erol Evangelista <je.evangelista04@gmail.com>
2023-12-14 17:14:41 -0500

    added chips and citation export

--- drc-portals/app/info/publications/PublicationsServer.tsx
+++ drc-portals/app/info/publications/PublicationsServer.tsx
@@ -55,7 +55,6 @@ export default async function PublicationsServer({
             dcc: true
           }
         },
-        ...include
       },
     })
     return (
--- drc-portals/components/misc/Publication/PublicationComponent.tsx
+++ drc-portals/components/misc/Publication/PublicationComponent.tsx
@@ -1,7 +1,18 @@
 import Link from "next/link";
-import { Typography, Button, Divider } from "@mui/material";
-import { Publication } from "@prisma/client";
-export default function PublicationComponent({publications, chipped=false}: {publications: Publication[], chipped?:Boolean}) {
+import { Typography, Button, Divider, Chip } from "@mui/material";
+import { Prisma } from "@prisma/client";
+import ExportCitation from "./ExportCitation";
+
+type PublicationWithDCC = Prisma.PublicationGetPayload<{
+    include: {
+        dccs: {
+          include: {
+            dcc: true
+          }
+        }
+    }
+  }>
+export default function PublicationComponent({publications, chipped=false}: {publications: PublicationWithDCC[], chipped?:Boolean}) {
     return (
         <>
             {publications.map((pub, i)=>(
@@ -12,25 +23,32 @@ export default function PublicationComponent({publications, chipped=false}: {pub
                                 <Typography color="secondary" variant="caption">
                                     {pub.authors}. {pub.year}. <b>{pub.title}{!pub.title.endsWith(".") && "."}</b> {pub.journal}. {pub.volume}. {pub.page}
                                 </Typography>
-                                <div className="flex space-x-2 justify-end">
-                                { pub.pmid && 
-                                    <Link target="_blank" rel="noopener noreferrer" href={`https://pubmed.ncbi.nlm.nih.gov/${pub.pmid}/`}>
-                                        <Button color="secondary">PubMed</Button>
-                                    </Link>
-                                }
-                                { pub.pmcid && 
-                                    <Link target="_blank" rel="noopener noreferrer" href={`https://www.ncbi.nlm.nih.gov/pmc/articles/${pub.pmcid}/`}>
-                                        <Button color="secondary">PMC</Button>
-                                    </Link>
-                                }
-                                { pub.doi && 
-                                    <Link target="_blank" rel="noopener noreferrer" href={`https://www.doi.org/${pub.doi}`}>
-                                        <Button color="secondary">DOI</Button>
-                                    </Link>
-                                }
-                                {/* { pub.pmcid && 
-                                    <ExportCitation pmc={`${pub.pmcid}`}/>
-                                } */}
+                                <div className="flex space-x-1 items-center justify-end">
+                                    { pub.pmid && 
+                                        <Link target="_blank" rel="noopener noreferrer" href={`https://pubmed.ncbi.nlm.nih.gov/${pub.pmid}/`}>
+                                            <Button color="secondary">PubMed</Button>
+                                        </Link>
+                                    }
+                                    { pub.pmcid && 
+                                        <Link target="_blank" rel="noopener noreferrer" href={`https://www.ncbi.nlm.nih.gov/pmc/articles/${pub.pmcid}/`}>
+                                            <Button color="secondary">PMC</Button>
+                                        </Link>
+                                    }
+                                    { pub.doi && 
+                                        <Link target="_blank" rel="noopener noreferrer" href={`https://www.doi.org/${pub.doi}`}>
+                                            <Button color="secondary">DOI</Button>
+                                        </Link>
+                                    }
+                                    { pub.pmcid && 
+                                        <ExportCitation pmcid={`${pub.pmcid}`}/>
+                                    }
+                                    { pub.landmark && 
+                                        <Chip label={"Landmark"} color="primary" sx={{borderRadius: 2, paddingLeft: 0, paddingRight: 0}}/>
+                                    }
+                                    {   (pub.dccs).map(({dcc})=>(
+                                            <Chip key={dcc.short_label} label={dcc.short_label} color="primary" sx={{borderRadius: 2, paddingLeft: 0, paddingRight: 0}}/>
+                                        ))
+                                    }
                                 </div>
                             </>
                         :
