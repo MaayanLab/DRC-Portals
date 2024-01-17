0b15483d638a344726c271d6f32457e3324e562d
mano-at-sdsc <mano@sdsc.edu>
2024-01-17 11:56:42 -0800

    Query to flatten table

--- drc-portals/biosample_join.sql
+++ drc-portals/biosample_join.sql
@@ -6,10 +6,15 @@ gene,
 collection_id_namespace, collection_local_id,
 substance,
 persistent_id
-from c2m2.fl_biosample 
-full join c2m2.biosample_disease on (c2m2.fl_biosample.local_id = c2m2.biosample_disease.biosample_local_id and c2m2.fl_biosample.id_namespace = c2m2.biosample_disease.biosample_id_namespace)
-full join c2m2.biosample_from_subject on (c2m2.fl_biosample.local_id = c2m2.biosample_from_subject.biosample_local_id and c2m2.fl_biosample.id_namespace = c2m2.biosample_from_subject.biosample_id_namespace)
-full join c2m2.biosample_gene on (c2m2.fl_biosample.local_id = c2m2.biosample_gene.biosample_local_id and c2m2.fl_biosample.id_namespace = c2m2.biosample_gene.biosample_id_namespace)
-full join c2m2.biosample_in_collection on (c2m2.fl_biosample.local_id = c2m2.biosample_in_collection.biosample_local_id and c2m2.fl_biosample.id_namespace = c2m2.biosample_in_collection.biosample_id_namespace)
-full join c2m2.biosample_substance on (c2m2.fl_biosample.local_id = c2m2.biosample_substance.biosample_local_id and c2m2.fl_biosample.id_namespace = c2m2.biosample_substance.biosample_id_namespace)
-limit 1;
+from 
+(c2m2.biosample b 
+full join c2m2.biosample_disease bd on (b.local_id = bd.biosample_local_id and b.id_namespace = bd.biosample_id_namespace)
+full join c2m2.biosample_from_subject bs on (b.local_id = bs.biosample_local_id and b.id_namespace = bs.biosample_id_namespace)
+full join c2m2.biosample_gene bg on (b.local_id = bg.biosample_local_id and b.id_namespace = bg.biosample_id_namespace)
+full join c2m2.biosample_in_collection bc on (b.local_id = bc.biosample_local_id and b.id_namespace = bc.biosample_id_namespace)
+full join c2m2.biosample_substance bsub on (b.local_id = bsub.biosample_local_id and b.id_namespace = bsub.biosample_id_namespace)
+full join c2m2.substance subs on (bsub.substance = subs.id)
+full join c2m2.anatomy ana on (b.anatomy = ana.id)
+full join c2m2.disease dis on (bd.anatomy = dis.id)
+
+) tmp limit 10;
