342c6689f46589aebdb13a60b13a7336fc6a6e80
mano-at-sdsc <mano@sdsc.edu>
2024-01-17 17:42:21 -0800

    Updated query to write a flattened table and join other tables

--- drc-portals/biosample_join.sql
+++ drc-portals/biosample_join.sql
@@ -1,34 +1,43 @@
 /* How to run: on psql prompt: be in the folder with this file or give full path:
-\i biosample_join.sql
+\i biosample_join.sql;
  */
 
-CREATE TABLE c2m2.fl2_biosample as (SELECT DISTINCT
+/* It is good convention to use alias for tables especially if two tables may 
+have the same colum name and we want one in the output, then it is easier to 
+refer by the alias instead of along table name. */
+
+CREATE TABLE c2m2.fl_biosample as (SELECT DISTINCT
 id_namespace, local_id, project_id_namespace, project_local_id, persistent_id, creation_time, sample_prep_method, anatomy,
 association_type, disease,
 subject_id_namespace, subject_local_id, age_at_sampling,
 gene,
 collection_id_namespace, collection_local_id,
-substance,
-persistent_id
-from c2m2.fl_biosample 
+substance
+from c2m2.biosample 
     full join c2m2.biosample_disease
-        on (
-            c2m2.fl_biosample.local_id = c2m2.biosample_disease.biosample_local_id and
-            c2m2.fl_biosample.id_namespace = c2m2.biosample_disease.biosample_id_namespace)
+        on (c2m2.biosample.local_id = c2m2.biosample_disease.biosample_local_id and
+        c2m2.biosample.id_namespace = c2m2.biosample_disease.biosample_id_namespace)
 
     full join c2m2.biosample_from_subject
-        on (c2m2.fl_biosample.local_id = c2m2.biosample_from_subject.biosample_local_id and 
-        c2m2.fl_biosample.id_namespace = c2m2.biosample_from_subject.biosample_id_namespace)
+        on (c2m2.biosample.local_id = c2m2.biosample_from_subject.biosample_local_id and 
+        c2m2.biosample.id_namespace = c2m2.biosample_from_subject.biosample_id_namespace)
 
     full join c2m2.biosample_gene 
-        on (c2m2.fl_biosample.local_id = c2m2.biosample_gene.biosample_local_id and 
-        c2m2.fl_biosample.id_namespace = c2m2.biosample_gene.biosample_id_namespace)
+        on (c2m2.biosample.local_id = c2m2.biosample_gene.biosample_local_id and 
+        c2m2.biosample.id_namespace = c2m2.biosample_gene.biosample_id_namespace)
 
     full join c2m2.biosample_in_collection
-        on (c2m2.fl_biosample.local_id = c2m2.biosample_in_collection.biosample_local_id and
-        c2m2.fl_biosample.id_namespace = c2m2.biosample_in_collection.biosample_id_namespace)
+        on (c2m2.biosample.local_id = c2m2.biosample_in_collection.biosample_local_id and
+        c2m2.biosample.id_namespace = c2m2.biosample_in_collection.biosample_id_namespace)
 
     full join c2m2.biosample_substance 
-    on (c2m2.fl_biosample.local_id = c2m2.biosample_substance.biosample_local_id and
-    c2m2.fl_biosample.id_namespace = c2m2.biosample_substance.biosample_id_namespace)
+        on (c2m2.biosample.local_id = c2m2.biosample_substance.biosample_local_id and
+        c2m2.biosample.id_namespace = c2m2.biosample_substance.biosample_id_namespace)
+)
 ;
+
+/* Just for ref:
+ORDER BY project_id_namespace, project_local_id, subject_id_namespace, subject_local_id, id_namespace, local_id,
+persistent_id, creation_time, sample_prep_method, anatomy, association_type, disease, age_at_sampling, gene,
+collection_id_namespace, collection_local_id, substance
+*/
--- drc-portals/example_query_biosample.sql
+++ drc-portals/example_query_biosample.sql
@@ -9,33 +9,45 @@ select *
 -- substance,
 -- persistent_id
 
-from c2m2.fl2_biosample 
+from c2m2.fl_biosample 
 
 --- JOIN ALL TABLES ---
 
     full join c2m2.anatomy
-        on (c2m2.fl2_biosample.anatomy = c2m2.anatomy.id)
+        on (c2m2.fl_biosample.anatomy = c2m2.anatomy.id)
 
     full join c2m2.disease
-        on (c2m2.fl2_biosample.disease = c2m2.disease.id)
+        on (c2m2.fl_biosample.disease = c2m2.disease.id)
 
     full join c2m2.gene
-        on (c2m2.fl2_biosample.gene = c2m2.gene.id)
+        on (c2m2.fl_biosample.gene = c2m2.gene.id)
 
     full join c2m2.substance
-        on (c2m2.fl2_biosample.substance = c2m2.substance.id)
+        on (c2m2.fl_biosample.substance = c2m2.substance.id)
 
     full join c2m2.project
-        on (c2m2.fl2_biosample.project_local_id = c2m2.project.local_id)
+        on (c2m2.fl_biosample.project_local_id = c2m2.project.local_id and
+        c2m2.fl_biosample.project_id_namespace = c2m2.project.id_namespace) 
+        /* we are not defining the new table fl2_biosample; just creating and populating it directly.
+        We need to keep track of mapping of the columns in the new table as they related to the original tables.*/
     
     full join c2m2.subject
-        on (c2m2.fl2_biosample.subject_local_id = c2m2.subject.local_id)
-    
+        on (c2m2.fl_biosample.subject_local_id = c2m2.subject.local_id and
+        c2m2.fl_biosample.project_id_namespace = c2m2.subject.project_id_namespace)
+
+    /* join with subject_role_taxonomy and ncbi_taxonomy */
+    full join c2m2.subject_role_taxonomy
+        on (c2m2.fl_biosample.subject_local_id = c2m2.subject_role_taxonomy.subject_local_id and
+        c2m2.subject.id_namespace = c2m2.subject_role_taxonomy.subject_id_namespace)
+
+    full join c2m2.ncbi_taxonomy
+        on (c2m2.subject_role_taxonomy.taxonomy_id = c2m2.ncbi_taxonomy.id)
+
     full join c2m2.collection
-        on (c2m2.fl2_biosample.collection_local_id = c2m2.collection.local_id)
+        on (c2m2.fl_biosample.collection_local_id = c2m2.collection.local_id)
     
     full join c2m2.sample_prep_method
-        on (c2m2.fl2_biosample.sample_prep_method = c2m2.sample_prep_method.id)
+        on (c2m2.fl_biosample.sample_prep_method = c2m2.sample_prep_method.id)
     
 --- CONDITIONS FOR SEARCH TERM ---
 
@@ -56,4 +68,4 @@ from c2m2.fl2_biosample
         c2m2.collection.description like '%liver biopsy%' or
         c2m2.sample_prep_method.name like '%liver biopsy%' or
         c2m2.sample_prep_method.description like '%liver biopsy%'
-;
+        limit 5;
