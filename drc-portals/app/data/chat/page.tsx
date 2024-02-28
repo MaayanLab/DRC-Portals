7905235f56fd8c8d4b07ddda0efde5c7df2aee2e
giacomomarino <77803629+giacomomarino@users.noreply.github.com>
2024-02-28 15:48:19 -0500

    update chat description

--- drc-portals/app/data/chat/page.tsx
+++ drc-portals/app/data/chat/page.tsx
@@ -7,7 +7,7 @@ export default function ChatPage() {
     <>
     <Typography variant="h2" color="secondary" className='pt-3'>CFDE Chatbot</Typography>
     <Typography variant="subtitle1" color="secondary" className='p-3'>
-        The CFDE chatbot utilizes the OpenAI API along with registred workflows implemented in the <Link href={"https://playbook-workflow-builder.cloud/"} target='_blank' className='underline'>Playbook Workflow Builder</Link> to answer queries related to DCC data types. Once a process and input is selected the result is visualized directly in the chat. 
+        The CFDE chatbot utilizes the OpenAI Assistants API with registred workflows implemented in the <Link href={"https://playbook-workflow-builder.cloud/"} target='_blank' className='underline'>Playbook Workflow Builder</Link> to answer queries related to DCC data types and general questions related to the CFDE and DCCs. Results of the registered workflows are visualized directly in the chat and are available for expansion and modificaiton in the Playbook Workflow Builder. 
     </Typography>
     <Chat/>
     </>)
