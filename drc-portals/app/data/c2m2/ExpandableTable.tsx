2cd30a36e778a74344b566735861787f0763d80e
susrinivasan <susrinivasan@ucsd.edu>
2024-02-29 15:12:24 -0800

    Added Accordion tables

---
+++ drc-portals/app/data/c2m2/ExpandableTable.tsx
@@ -0,0 +1,10 @@
+// ExpandableTable.tsx
+import React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { AccordionDetails } from '@mui/material';
+import SearchablePagedTable, { Description } from "@/app/data/c2m2/SearchablePagedTable";
+import Button from '@mui/material/Button';
+
