0bc60f091f31f7a6bb374f51875af24671bd0199
susrinivasan <susrinivasan@ucsd.edu>
2024-03-14 13:00:34 -0700

    Fixed the TagComponent to appear in a Box inside SearchablePagedTable and fixed the parameters accordingly.

--- drc-portals/app/data/c2m2/SearchablePagedTable.tsx
+++ drc-portals/app/data/c2m2/SearchablePagedTable.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Paper, Stack, Grid, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow, Typography } from "@mui/material"
+import { Box, Paper, Stack, Grid, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow, Typography } from "@mui/material"
 import FormPagination from "./FormPagination"
 import SearchField from "./SearchField"
 import Link from "next/link"
@@ -8,6 +8,7 @@ import { NodeType } from "@prisma/client"
 import { type_to_string } from "../processed/utils"
 import { Prosto_One } from "next/font/google"
 import FindInPageOutlinedIcon from '@mui/icons-material/FindInPageOutlined'
+import TagComponent from "./TagComponent";
 
 export function LinkedTypedNode({
   id,
@@ -66,8 +67,8 @@ export function PreviewButton(props: {
 
 export default function SearchablePagedTable(props: React.PropsWithChildren<{
   label?: string,
-  filternames?: string,
   q: string, p: number, r: number, count?: number,
+  t?: { type: string; entity_type: string | null; }[] | undefined,
   columns: React.ReactNode[],
   rows: React.ReactNode[][],
 }>) {
@@ -78,7 +79,7 @@ export default function SearchablePagedTable(props: React.PropsWithChildren<{
         <Grid item xs={12} sx={{ marginBottom: 5 }}>
           <Stack direction={"row"} alignItems={"center"} justifyContent={'space-between'}>
             <Typography variant="h5" color="secondary" className="whitespace-nowrap">
-              {props.filternames || props.label}
+              {props.label}
             </Typography>
             {/*<form action="" method="GET">*/}
             {/*  <SearchField q={props.q} placeholder={`Search ${props.label}`} />*/}
@@ -87,73 +88,76 @@ export default function SearchablePagedTable(props: React.PropsWithChildren<{
         </Grid>
       }
       <Grid item xs={12}>
-  {props.rows.length === 0 ? (
-    <>No results</>
-  ) : (
-    <Stack spacing={1}>
-  <FormPagination p={props.p} r={props.r} count={props.count} />
-  
-  <div style={{ overflowX: 'hidden', marginBottom: '1px' }}> {/* Hide the table header overflow */}
-    <TableContainer component={Paper} elevation={0} variant="rounded-top">
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {props.columns.map((column, i) => (
-              <TableCell 
-                key={i} 
-                component="th" 
-                align="center" // Changed to left
-                sx={{ 
-                  // minWidth: 150, // Set a consistent minWidth for all cells
-                  padding: '8px', // Set consistent padding for header cells
-                }} 
-              >
-              <Typography variant='h6' color="secondary" style={{ textAlign: 'left' }}>{column}</Typography> {/* Ensure text is also left-aligned if needed */}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-      </Table>
-    </TableContainer>
-  </div>
+        {props.rows.length === 0 ? (
+          <>No results</>
+        ) : (
+          <Stack spacing={1}>
+            <Box display="inline-block">
+              <TagComponent q={props.q} t={props.t} />
+            </Box>
+            <FormPagination p={props.p} r={props.r} count={props.count} />
 
-  <div style={{ overflowY: 'auto', maxHeight: '700px' }}> {/* Adjust maxHeight to account for the table header */}
-    <TableContainer component={Paper} elevation={0} variant="rounded-top">
-      <Table aria-label="simple table">
-        <TableBody>
-          {props.rows.map((row, i) => (
-            <TableRow
-              key={i}
-              sx={{
-                '&:last-child td, &:last-child th': { border: 0 },
-              }}
-            >
-            {row.map((cell, j) => (
-              <TableCell 
-                sx={{ 
-                  // minWidth: 150, // Set a consistent minWidth for all cells
-                  padding: '8px', // Set consistent padding for body cells
-                  maxWidth: 300, 
-                  overflowWrap: 'break-word', 
-                  textAlign: 'left', // Changed to left
-                }} 
-                key={j}
-                align="left"
+            <div style={{ overflowX: 'hidden', marginBottom: '1px' }}> {/* Hide the table header overflow */}
+              <TableContainer component={Paper} elevation={0} variant="rounded-top">
+                <Table aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      {props.columns.map((column, i) => (
+                        <TableCell
+                          key={i}
+                          component="th"
+                          align="center" // Changed to left
+                          sx={{
+                            // minWidth: 150, // Set a consistent minWidth for all cells
+                            padding: '8px', // Set consistent padding for header cells
+                          }}
+                        >
+                          <Typography variant='h6' color="secondary" style={{ textAlign: 'left' }}>{column}</Typography> {/* Ensure text is also left-aligned if needed */}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+                </Table>
+              </TableContainer>
+            </div>
 
-              >
-              {cell}
-              </TableCell>
-          ))}
-          </TableRow>
-        ))}
-        </TableBody>
-    </Table>
-  </TableContainer>
-</div>
-</Stack>
+            <div style={{ overflowY: 'auto', maxHeight: '700px' }}> {/* Adjust maxHeight to account for the table header */}
+              <TableContainer component={Paper} elevation={0} variant="rounded-top">
+                <Table aria-label="simple table">
+                  <TableBody>
+                    {props.rows.map((row, i) => (
+                      <TableRow
+                        key={i}
+                        sx={{
+                          '&:last-child td, &:last-child th': { border: 0 },
+                        }}
+                      >
+                        {row.map((cell, j) => (
+                          <TableCell
+                            sx={{
+                              // minWidth: 150, // Set a consistent minWidth for all cells
+                              padding: '8px', // Set consistent padding for body cells
+                              maxWidth: 300,
+                              overflowWrap: 'break-word',
+                              textAlign: 'left', // Changed to left
+                            }}
+                            key={j}
+                            align="left"
 
-  )}
-</Grid>
+                          >
+                            {cell}
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </div>
+          </Stack>
+
+        )}
+      </Grid>
 
     </Grid>
   )
