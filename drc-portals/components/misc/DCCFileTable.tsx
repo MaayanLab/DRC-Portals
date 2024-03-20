8f2e9caddfee3b8cc90e572c1a6bc0a8b8fdfa9c
Sherry Xie <39966933+sxie04@users.noreply.github.com>
2024-03-20 14:48:07 -0400

    remove creator column

--- drc-portals/components/misc/DCCFileTable.tsx
+++ drc-portals/components/misc/DCCFileTable.tsx
@@ -57,9 +57,8 @@ export function DCCFileTable(props : {fileInfo: dccAsset[], isCode: boolean}) {
         <Table sx={{mb:2}}>
         <TableHead>
           <TableRow sx={{backgroundColor: '#edf1f7'}}>
-            <TableCell width='35%' sx={{border:0, fontWeight: 'bold', fontSize: '12pt'}}>Name</TableCell>
+            <TableCell width='65%' sx={{border:0, fontWeight: 'bold', fontSize: '12pt'}}>Name</TableCell>
             <TableCell width='15%' sx={{border:0, fontWeight: 'bold', fontSize: '12pt'}} align="center">Date Modified</TableCell>
-            <TableCell sx={{border:0, fontWeight: 'bold', fontSize: '12pt'}} align="center">Creator</TableCell>
             <TableCell width='10%' sx={{border:0, fontWeight: 'bold', fontSize: '12pt'}} align="center">DCC Approved</TableCell>
             <TableCell width='10%' sx={{border:0, fontWeight: 'bold', fontSize: '12pt'}} align="center">DRC Approved</TableCell>
           </TableRow>
@@ -68,11 +67,10 @@ export function DCCFileTable(props : {fileInfo: dccAsset[], isCode: boolean}) {
           {props.fileInfo.map((item, idx) => {
             return (
               <TableRow key={idx}>
-                <TableCell style={{wordBreak: "break-word"}} width='30%' sx={{border:0}}>
+                <TableCell width='65%' style={{wordBreak: "break-word"}} sx={{border:0}}>
                   <NameCell item={item}></NameCell>
                 </TableCell>
-                <TableCell align="center" sx={{border:0, fontSize: '11pt'}}>{item.lastmodified}</TableCell>
-                <TableCell style={{wordBreak: "break-word"}} align="center" sx={{border:0, fontSize: '11pt'}}>{item.creator}</TableCell>
+                <TableCell width='15%' align="center" sx={{border:0, fontSize: '11pt'}}>{item.lastmodified}</TableCell>
                 <TableCell width='10%' align="center" sx={{border:0}}>
                   {item.dccapproved ? (<CheckCircle sx={{color:"#7187C3"}} />) : (<span />)}
                 </TableCell>
@@ -88,10 +86,9 @@ export function DCCFileTable(props : {fileInfo: dccAsset[], isCode: boolean}) {
         <Table sx={{mb:2}}>
           <TableHead>
             <TableRow sx={{backgroundColor: '#edf1f7'}}>
-              <TableCell width='30%' sx={{border:0, fontWeight:'bold', fontSize:'12pt'}}>Filename</TableCell>
+              <TableCell width='55%' sx={{border:0, fontWeight:'bold', fontSize:'12pt'}}>Filename</TableCell>
               <TableCell width='10%' sx={{border:0, fontWeight: 'bold', fontSize: '12pt'}} align="center">Filesize</TableCell>
               <TableCell width='15%' sx={{border:0, fontWeight: 'bold', fontSize: '12pt'}} align="center">Date Modified</TableCell>
-              <TableCell sx={{border:0, fontWeight: 'bold', fontSize: '12pt'}} align="center">Creator</TableCell>
               <TableCell width='10%' sx={{border:0, fontWeight: 'bold', fontSize: '12pt'}} align="center">DCC Approved</TableCell>
               <TableCell width='10%' sx={{border:0, fontWeight: 'bold', fontSize: '12pt'}} align="center">DRC Approved</TableCell>
             </TableRow>
@@ -100,14 +97,13 @@ export function DCCFileTable(props : {fileInfo: dccAsset[], isCode: boolean}) {
           {props.fileInfo.map((item, idx) => {
             return (
               <TableRow key={idx}>
-                <TableCell style={{wordBreak: "break-word"}} width='30%' sx={{border:0}}>
+                <TableCell width='55%' style={{wordBreak: "break-word"}} sx={{border:0}}>
                   <Link color="#3470e5" fontSize="11pt" className="underline" href={item.link} target="_blank" rel="noopener">
                     {item.filename}
                   </Link>
                 </TableCell>
                 <TableCell width="10%" align="center" sx={{border:0, fontSize: '11pt'}}>{item.size}</TableCell>
                 <TableCell width="15%" align="center" sx={{border:0, fontSize: '11pt'}}>{item.lastmodified}</TableCell>
-                <TableCell style={{wordBreak: "break-word"}} align="center" sx={{border:0, fontSize: '11pt'}}>{item.creator}</TableCell>
                 <TableCell width='10%' align="center" sx={{border:0}}>
                   {item.dccapproved ? (<CheckCircle sx={{color:"#7187C3"}} />) : (<span />)}
                 </TableCell>
