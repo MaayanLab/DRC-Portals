90dbe0a06ab94f2bf59b657a3870087d7ddc87a4
Daniel Clarke <u8sand@gmail.com>
2024-03-27 13:22:42 -0400

    Fixup twitter cronjob

--- twitter/app.py
+++ twitter/app.py
@@ -1,5 +1,7 @@
 import os
-import psycopg
+import json
+import psycopg2
+from urllib.parse import urlparse
 from twikit import Client
 
 client = Client('en-US')
@@ -13,32 +15,39 @@ def kvstore_get(cur, key):
   row = cur.fetchone()
   if row is None: return None
   _key, value = row
-  return value
+  return json.loads(value)
 
 def kvstore_upsert(cur, key, value):
   cur.execute('''
     insert into "kvstore" ("key", "value")
     values (%s, %s)
-    on conflict do update set "value" = EXCLUDED."value";
-  ''', (key, value))
+    on conflict ("key") do update set "value" = EXCLUDED."value";
+  ''', (key, json.dumps(value)))
 
-with psycopg.connect(os.environ['DATABASE_URL']) as conn:
-  with conn.cursor() as cur:
-    cookies = kvstore_get(cur, 'twitter-cookies')
-    if not cookies:
-      client.login(
-        auth_info_1=os.environ['TWITTER_USERNAME'],
-        auth_info_2=os.environ['TWITTER_EMAIL'],
-        password=os.environ['TWITTER_PASSWORD'],
-      )
-      cookies = client.get_cookies()
-      kvstore_upsert(cur, 'twitter-cookies', cookies)
-    else:
-      client.set_cookies(cookies)
-    #
-    tweets = [
-      tweet._data
-      for tweet in client.get_user_by_screen_name(os.environ['TWITTER_TWEETS_FROM']).get_tweets(tweet_type='Tweets')
-    ]
-    kvstore_upsert(cur, 'twitter-tweets', tweets)
-    conn.commit()
+uri = urlparse(os.environ['DATABASE_URL'])
+conn = psycopg2.connect(
+  database = uri.path[1:],
+  user = uri.username,
+  password = uri.password,
+  host = uri.hostname,
+  port = uri.port
+)
+with conn.cursor() as cur:
+  cookies = kvstore_get(cur, 'twitter-cookies')
+  if not cookies:
+    client.login(
+      auth_info_1=os.environ['TWITTER_USERNAME'],
+      auth_info_2=os.environ['TWITTER_EMAIL'],
+      password=os.environ['TWITTER_PASSWORD'],
+    )
+    cookies = client.get_cookies()
+    kvstore_upsert(cur, 'twitter-cookies', cookies)
+  else:
+    client.set_cookies(cookies)
+  #
+  tweets = [
+    tweet._data
+    for tweet in client.get_user_by_screen_name(os.environ['TWITTER_TWEETS_FROM']).get_tweets(tweet_type='Tweets')
+  ]
+  kvstore_upsert(cur, 'twitter-tweets', tweets)
+  conn.commit()
