bc9889dd11e8b196d82ba5bd8df0a226bde9d68d
Daniel Clarke <u8sand@gmail.com>
2024-04-02 18:15:11 -0400

    [database/c2m2]: Fixup ingest_CV_slim

--- database/C2M2/ingest_CV_slim.py
+++ database/C2M2/ingest_CV_slim.py
@@ -1,4 +1,5 @@
 import sys
+import json
 import pathlib
 sys.path.append(str(pathlib.Path(__file__).parent.parent))
 from ingest_common import connection
@@ -9,8 +10,8 @@ cur = connection.cursor()
 for f in pathlib.Path(__file__).parent.glob('CV/*.tsv'):
   cur.execute(
     f'''
-      DROP TABLE IF EXISTS c2m2.{f.stem} RESTRICT;
-      CREATE TABLE c2m2.{f.stem}(
+      DROP TABLE IF EXISTS "c2m2"."{f.stem}" RESTRICT;
+      CREATE TABLE "c2m2"."{f.stem}"(
         id VARCHAR NOT NULL,
         name VARCHAR NOT NULL,
         description VARCHAR NOT NULL,
@@ -20,25 +21,39 @@ for f in pathlib.Path(__file__).parent.glob('CV/*.tsv'):
   )
   with f.open('r') as fr:
     columns = next(fr).strip().split('\t')
-    cur.copy_from(fr, f'"c2m2"."{f.stem}"', null='', columns=columns, sep='\t')
+    cur.copy_expert(f'''COPY "c2m2"."{f.stem}" ({','.join(map(json.dumps, columns))}) from STDIN with CSV DELIMITER '\t';''', fr)
 
 # Ingest slim (and associated ontology) tables into a schema called 'slim', because c2m2 also has tables like anatomy, disease etc., which is likely to be a much smaller subset of the corresponding tables in the slim schema.
 # There is also the table dbgap_study_id.tsv ; for now, it will be in slim schema, if needed later, can be put in a schema called dbgap.
+try:
+  cur.execute('create schema "slim";')
+except:
+  connection.rollback()
+
 for f in pathlib.Path(__file__).parent.glob('slim/*.tsv'):
-  cur.execute(
-    f'''
-      DROP TABLE IF EXISTS slim.{f.stem} RESTRICT;
-      CREATE TABLE slim.{f.stem}(
-        id VARCHAR NOT NULL,
-        name VARCHAR NOT NULL,
-        description VARCHAR DEFAULT NULL,
-        synonyms VARCHAR DEFAULT NULL,
-        original_term_id VARCHAR NOT NULL,
-        slim_term_id VARCHAR NOT NULL,
-        PRIMARY KEY(id)
-      );
-    '''
-  )
+  if f.stem.endswith('_slim'):
+    cur.execute(
+      f'''
+        DROP TABLE IF EXISTS slim."{f.stem}" RESTRICT;
+        CREATE TABLE slim."{f.stem}" (
+          original_term_id VARCHAR NOT NULL,
+          slim_term_id VARCHAR NOT NULL,
+          pk_id SERIAL PRIMARY KEY
+        );
+      '''
+    )
+  else:
+    cur.execute(
+      f'''
+        DROP TABLE IF EXISTS slim."{f.stem}" RESTRICT;
+        CREATE TABLE slim."{f.stem}" (
+          id VARCHAR NOT NULL PRIMARY KEY,
+          name VARCHAR NOT NULL,
+          description VARCHAR DEFAULT NULL,
+          synonyms VARCHAR DEFAULT NULL
+        );
+      '''
+    )
   with f.open('r') as fr:
     columns = next(fr).strip().split('\t')
-    cur.copy_from(fr, f'"slim"."{f.stem}"', null='', columns=columns, sep='\t')
+    cur.copy_expert(f'''COPY "slim"."{f.stem}" ({','.join(map(json.dumps, columns))}) from STDIN with CSV DELIMITER '\t';''', fr)
