d4a5d6d444ccf64998241615a88d631c94d88eba
stephanieolaiya <stephanieolaiya@gmail.com>
2024-04-12 11:48:49 -0400

    fixes #220

--- drc-portals/app/data/contribute/uploaded/page.tsx
+++ drc-portals/app/data/contribute/uploaded/page.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Container from '@mui/material/Container'
-import { Alert, Link, Typography, Grid } from '@mui/material';
+import { Alert, Link, Typography, Grid, Button } from '@mui/material';
 import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 import prisma from '@/lib/prisma';
@@ -14,11 +14,11 @@ export default async function UserFiles() {
     const session = await getServerSession(authOptions)
     if (!session) return redirect("/auth/signin?callback=/data/contribute/uploaded")
 
-    const dccInfo =  await prisma.dCC.findMany()
-    const dccMapping : {[key: string]: string} = {}
+    const dccInfo = await prisma.dCC.findMany()
+    const dccMapping: { [key: string]: string } = {}
     dccInfo.map((dcc) => {
         dcc.short_label ? dccMapping[dcc.short_label] = dcc.label : dccMapping[dcc.label] = dcc.label
-        })
+    })
 
     const user = await prisma.user.findUnique({
         where: {
@@ -30,14 +30,14 @@ export default async function UserFiles() {
                     dcc: {
                         select: {
                             label: true,
-                            short_label:true
+                            short_label: true
                         }
-                    }, 
+                    },
                     fileAsset: true,
                     codeAsset: true,
                 }
             },
-            dccs : true
+            dccs: true
         },
     })
 
@@ -45,37 +45,52 @@ export default async function UserFiles() {
     // if user is not an uploader or approver, then they should not have acccess to this page
     if (user.role === 'USER') {
         return (
-            <>
-                <Nav />
-                <p>Access Denied. This page is only accessible to DCC Uploaders, DCC Approvers and DRC Approvers</p>
-            </>)
+            <Grid container spacing={2} sx={{ mt: 2 }}>
+                <Grid md={2} xs={12}>
+                    <Nav />
+                </Grid>
+                <Grid md={10} xs={12}>
+                    <Alert severity="warning">Access Denied. This page is only accessible to DCC Uploaders and DCC & DRC Approvers</Alert>
+                </Grid>
+            </Grid>
+        )
     }
-
     if (!user.email) return (
-        <>
-            <Nav />
-            <Alert severity="warning"> Email not updated on user account. Please enter email on the My Account Page</Alert>
-        </>
+        <Grid container spacing={2} sx={{ mt: 2 }}>
+            <Grid md={2} xs={12}>
+                <Nav />
+            </Grid>
+            <Grid md={10} xs={12}>
+                <Alert severity="warning" action={
+                    <Button color="inherit" size="small" href='/data/contribute/account'>
+                        GO TO MY ACCOUNT
+                    </Button>
+                }> Email not updated on user account. Please enter email on the My Account Page</Alert>
+            </Grid>
+        </Grid>
     );
 
     if (user.dccs.length === 0) return (
-        <>
-            <Nav />
-            <Alert severity="warning"> User has no affiliated DCCs. Please contact the DRC to update your information</Alert>
-        </>
+        <Grid container spacing={2} sx={{ mt: 2 }}>
+            <Grid md={2} xs={12}>
+                <Nav />
+            </Grid>
+            <Grid md={10} xs={12}>
+                <Alert severity="warning"> User has no affiliated DCCs. Please contact the DRC to update your information</Alert>    </Grid>
+        </Grid>
     );
 
-    let userDCCArray : string[] = []
-    user.dccs.forEach((dcc: DCC) => {if (dcc.short_label) {userDCCArray.push(dcc.short_label)}})
-    
+    let userDCCArray: string[] = []
+    user.dccs.forEach((dcc: DCC) => { if (dcc.short_label) { userDCCArray.push(dcc.short_label) } })
+
     const allFiles = await prisma.dccAsset.findMany({
         include: {
             dcc: {
                 select: {
                     label: true,
-                    short_label:true
+                    short_label: true
                 }
-            }, 
+            },
             fileAsset: true,
             codeAsset: true,
         },
@@ -97,7 +112,7 @@ export default async function UserFiles() {
     let headerText;
 
     if (user.role === 'UPLOADER') {
-        userFiles = user.dccAsset.filter((asset) => asset.deleted === false).map(obj => ({ ...obj, assetType:  obj.fileAsset ? obj.fileAsset?.filetype :  obj.codeAsset?.type ?? '' }))
+        userFiles = user.dccAsset.filter((asset) => asset.deleted === false).map(obj => ({ ...obj, assetType: obj.fileAsset ? obj.fileAsset?.filetype : obj.codeAsset?.type ?? '' }))
 
         // userFiles = allFiles
         headerText = <Typography variant="subtitle1" color="#666666" className='' sx={{ mb: 3, ml: 2 }}>
@@ -110,7 +125,7 @@ export default async function UserFiles() {
         </Typography>
 
     } else if (user.role === 'DCC_APPROVER') {
-        userFiles = allFiles.map(obj => ({ ...obj, assetType:  obj.fileAsset ? obj.fileAsset?.filetype :  obj.codeAsset?.type ?? '' }))
+        userFiles = allFiles.map(obj => ({ ...obj, assetType: obj.fileAsset ? obj.fileAsset?.filetype : obj.codeAsset?.type ?? '' }))
         headerText = <Typography variant="subtitle1" color="#666666" className='' sx={{ mb: 3, ml: 2 }}>
             These are all assets that have been uploaded/submitted for your affiliated DCCs.
             Expand a row to see additional information for a file or code asset.
@@ -120,7 +135,7 @@ export default async function UserFiles() {
             and current statuses of each file and the steps to approve a file or change its current status.
         </Typography>
     } else {
-        userFiles = allFiles.map(obj => ({ ...obj, assetType:  obj.fileAsset ? obj.fileAsset?.filetype :  obj.codeAsset?.type ?? '' }))
+        userFiles = allFiles.map(obj => ({ ...obj, assetType: obj.fileAsset ? obj.fileAsset?.filetype : obj.codeAsset?.type ?? '' }))
         headerText = <Typography variant="subtitle1" color="#666666" className='' sx={{ mb: 3, ml: 2 }}>
             These are all assets that have been uploaded/submitted for all the DCCs.
             Expand a row to see additional information for a file or code asset.
