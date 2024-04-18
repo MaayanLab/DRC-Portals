0be0a777d79ca17977fde6b43a1368f7518811c1
shivaramakrishna99 <shivaramakrishna.srinivasan@gmail.com>
2024-04-18 10:16:17 -0700

    organised tags with reset

--- drc-portals/app/data/c2m2/TagComponent.tsx
+++ drc-portals/app/data/c2m2/TagComponent.tsx
@@ -1,13 +1,15 @@
 'use client'
 import React from 'react';
-import { Chip } from '@mui/material';
+import { Chip, Typography, Button } from '@mui/material';
 
 const TagComponent = ({
     q,
     t,
+    onReset,
 }: {
     q: string;
     t: { type: string; entity_type: string | null; }[] | undefined;
+    onReset: () => void;
 }) => {
     const handleDelete = (tagToDelete: { type: string; entity_type: string | null }) => {
         let baseUrl = window.location.origin + window.location.pathname;
@@ -31,33 +33,91 @@ const TagComponent = ({
         window.location.href = `${baseUrl}?${updatedParams.toString()}`;
     };
 
+    const handleReset = () => {
+        let baseUrl = window.location.origin + window.location.pathname;
+        let updatedParams = new URLSearchParams(window.location.search);
+
+        // Clear all tags from the URL
+        updatedParams.delete('t');
+        // Redirect to the updated URL
+        window.location.href = `${baseUrl}?${updatedParams.toString()}`;
+    };
+
     const renderQueryChip = () => {
         if (!q) return null;
         return (
-            <Chip
-                key="q"
-                label={`Query: ${q}`}
-                //onDelete={() => handleDelete({ type: 'q', entity_type: null })}
-                style={{ margin: '4px', fontSize: '1.0rem', color: '#0047AB' }}
-            />
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '8px' }}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Typography variant="subtitle1" style={{ marginRight: '8px', fontWeight: 700 }}>
+                    Query
+                </Typography>
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    <Chip
+                        key="q"
+                        label={q}
+                        variant="outlined"
+                        style={{ margin: '4px', fontSize: '1.0rem', color: '#0047AB' }}
+                    />
+                </div>
+            </div>
+            <div>
+                <Button onClick={handleReset} variant="outlined" color="secondary">
+                        Reset
+                    </Button>
+            </div>
+            </div>
         );
     };
 
-    const renderTags = () => {
-        return t ? t.map((tag, index) => (
-            <Chip
-                key={`tag-${index}`}
-                label={`${tag.type}: ${tag.entity_type || 'N/A'}`}
-                onDelete={() => handleDelete(tag)}
-                style={{ margin: '4px', fontSize: '1.0rem', color: '#0047AB' }}
-            />
-        )) : null;
+
+    const renderTagsByType = () => {
+        if (!t) return null;
+
+        // Group tags by their type
+        const tagGroups: { [key: string]: { type: string; entity_type: string | null }[] } = {};
+        t.forEach(tag => {
+            if (!tagGroups[tag.type]) {
+                tagGroups[tag.type] = [];
+            }
+            tagGroups[tag.type].push(tag);
+        });
+
+        const typeDisplayNames = {
+            'dcc': 'Common Fund Program',
+            'taxonomy': 'Species',
+            'anatomy': 'Anatomy',
+            'disease': 'Disease',
+            'gene': 'Gene',
+            'data_type': 'Data Type'
+            // Add more mappings as needed
+        };
+
+
+        // Render tags grouped by type
+        return Object.keys(tagGroups).map((type, index) => (
+            <div key={`tag-group-${index}`} style={{ display: 'flex', alignItems: 'center' }}>
+                <Typography variant="subtitle1" style={{ marginRight: '8px', fontWeight: 700 }}>
+                    {typeDisplayNames[type.toLowerCase()] || type}
+                </Typography>
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    {tagGroups[type].map((tag, tagIndex) => (
+                        <Chip
+                            key={`tag-${index}-${tagIndex}`}
+                            label={`${tag.entity_type || 'N/A'}`}
+                            onDelete={() => handleDelete(tag)}
+                            style={{ margin: '4px', fontSize: '1.0rem', color: '#0047AB' }}
+                        />
+                    ))}
+                </div>
+            </div>
+
+        ));
     };
 
     return (
         <>
             {renderQueryChip()}
-            {renderTags()}
+            {renderTagsByType()}
         </>
     );
 };
