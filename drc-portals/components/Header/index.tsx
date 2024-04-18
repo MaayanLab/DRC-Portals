b3d5e3a1af59e6eafe5a9fd1b4f273e5733c10af
John Erol Evangelista <je.evangelista04@gmail.com>
2024-04-18 16:36:38 -0400

    removed menufication of header

--- drc-portals/components/Header/index.tsx
+++ drc-portals/components/Header/index.tsx
@@ -3,14 +3,55 @@ import { getServerSession } from 'next-auth'
 
 import AppBar from '@mui/material/AppBar'
 import Container from '@mui/material/Container'
-import Nav from './Nav'
+import Link from 'next/link';
+import { 
+	Grid,
+	Button,
+	Toolbar,
+	Typography,
+	Stack
+} from '@mui/material';
+
+import UserComponent from '../misc/LoginComponents/UserComponent'
+import { DataComponent } from './DataComponent';
+import SearchParamSearchField from '@/app/data/processed/SearchParamSearchField'
+import { Logo } from '../styled/Logo'
+import Navigation from './Navigation';
 
 export default async function Header() {
   const session = await getServerSession(authOptions) 
   return (
     <Container maxWidth="lg">
       <AppBar position="static" sx={{color: "#000"}}>
-        <Nav session={session}/>
+      <Toolbar>
+			  <Grid container justifyContent={"space-between"} alignItems={"center"} spacing={2}>
+          <Grid item>
+            <Logo title="CFDE Workbench" size='large' color="secondary"/>
+          </Grid>
+          <Grid item>
+            <Stack direction={"row"} alignItems={"center"} spacing={2}>
+              <Link href="/info">
+                <Typography variant="nav"><b>Information Portal</b></Typography>
+              </Link>
+              <Link href={"/data"}>
+                <Typography variant="nav"><b>Data Portal</b></Typography>
+              </Link>
+              {/* <Link href={"mailto:help@cfde.cloud"}>
+                <Typography variant="nav">Support</Typography>
+              </Link> */}
+              <UserComponent session={session}/>
+            </Stack>
+				  </Grid>
+          <Grid item xs={12}> 
+                <Navigation />
+          </Grid>
+          <DataComponent>
+            <Grid item xs={12} sx={{textAlign: 'right'}}>
+              <SearchParamSearchField />
+            </Grid>
+          </DataComponent>
+        </Grid>
+      </Toolbar>
       </AppBar>
     </Container>
   )
