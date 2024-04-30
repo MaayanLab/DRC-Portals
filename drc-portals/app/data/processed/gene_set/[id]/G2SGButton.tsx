d588f7e7ccceb5d7ce78133e078caaf008bbfb26
stephanieolaiya <stephanieolaiya@gmail.com>
2024-04-30 15:12:12 -0400

    clean up code

--- drc-portals/app/data/processed/gene_set/[id]/G2SGButton.tsx
+++ drc-portals/app/data/processed/gene_set/[id]/G2SGButton.tsx
@@ -29,24 +29,3 @@ export default function G2SGButton(props: React.PropsWithChildren<{ title: React
         >Submit</Button>
     </CardButton>
 }
-
-
-// export async function GET(request: Request, { params }: { params: { id: string } }) {
-//     const geneSet = await getItem(params.id)
-//     if (!geneSet.data.geneSet) return new Response(JSON.stringify({error: 'Not Found'}), { status: 404 })
-//     const req = await fetch('https://g2sg.cfde.cloud/api/addGeneset', {
-//       headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json',
-//       },
-//       method: 'POST',
-//       body: JSON.stringify({
-//         term: geneSet.data.geneSet.term,
-//         genes: geneSet.data.geneSet.genes.nodes.map(gene => gene.symbol),
-//         description: `Rummagene ${geneSet.data.geneSet.description}`,
-//       }),
-//     })
-//     const { session_id } = await req.json()
-//     if (!session_id) return new Response(JSON.stringify({error: 'Failed to Register Gene Set'}), { status: 500 })
-//     redirect(`https://g2sg.cfde.cloud/analyze/${session_id}`)
-//   }
