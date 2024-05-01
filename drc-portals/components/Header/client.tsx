be1cf0ed0952108b23c6e3914fe8fee19d4d1599
John Erol Evangelista <je.evangelista04@gmail.com>
2024-05-01 14:44:48 -0400

    added underline to header

---
+++ drc-portals/components/Header/client.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { Typography } from "@mui/material"
+import { usePathname } from "next/navigation"
+
+export  function TextNav({title, path}: {title: string, path: string}) {
+	const pathname = usePathname()
+	let sx
+	if ((pathname.indexOf(path) !== -1 && path !== '') || (pathname.indexOf('/processed') > -1 && path === "")) {
+		console.log(pathname, path)
+		sx = {textDecoration: "underline", textDecorationThickness: 2}
+	}
+	return(
+		<Typography variant="nav" sx={sx}><b>{title}</b></Typography>
+	)
+}
