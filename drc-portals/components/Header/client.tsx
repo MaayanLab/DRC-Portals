2d87ea2387e7893b615e4c977191e1254bd15d0e
John Erol Evangelista <je.evangelista04@gmail.com>
2024-05-01 16:00:22 -0400

    added underline to home

--- drc-portals/components/Header/client.tsx
+++ drc-portals/components/Header/client.tsx
@@ -3,9 +3,9 @@ import { Typography } from "@mui/material"
 import { usePathname } from "next/navigation"
 
 export  function TextNav({title, path}: {title: string, path: string}) {
-	const pathname = usePathname()
+	const pathname = usePathname().replace("/info", "/").replace("/data", "/")
 	let sx
-	if ((pathname.indexOf(path) !== -1 && path !== '') || (pathname.indexOf('/processed') > -1 && path === "")) {
+	if ((pathname.indexOf(path) !== -1 && path !== '') || ((pathname.indexOf('/processed') > -1 || pathname === "/") && path === "")) {
 		sx = {textDecoration: "underline", textDecorationThickness: 2}
 	}
 	return(
