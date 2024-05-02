e3ca5a0863b26dc0636b0b53c088de36feeb8f04
stephanieolaiya <stephanieolaiya@gmail.com>
2024-05-02 17:27:35 -0400

    g2sg cookies implementation initial

--- drc-portals/app/data/processed/gene_set/[id]/G2SGButton.tsx
+++ drc-portals/app/data/processed/gene_set/[id]/G2SGButton.tsx
@@ -5,6 +5,9 @@ import Image from "next/image"
 import G2SGIcon from '@/public/img/icons/g2sg-logo.png'
 import CardButton from "@/app/data/processed/CardButton"
 
+import * as React from 'react';
+import Cookies from "js-cookie"
+
 export default function G2SGButton(props: React.PropsWithChildren<{ title: React.ReactNode, description: React.ReactNode, body: any }>) {
     return <CardButton
         icon={<Image src={G2SGIcon} alt="Get-Gene-Set-Go" height={64} />}
@@ -15,16 +18,35 @@ export default function G2SGButton(props: React.PropsWithChildren<{ title: React
             color="secondary"
             size="small"
             onClick={async () => {
-                const req = await fetch('https://g2sg.cfde.cloud/api/addGeneset', {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(props.body),
-                })
-                const { session_id } = await req.json()
-                window.open(`https://g2sg.cfde.cloud/analyze/${session_id}`, '_blank')
+                const currentG2SGSession = Cookies.get('session_id')
+                if (currentG2SGSession === undefined) {
+                    const req = await fetch('http://localhost:3001/api/addGeneset', {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(props.body),
+                    })
+                    const { session_id } = await req.json()
+                    window.open(`http://localhost:3001/analyze/${session_id}`, '_blank')
+                } else {
+                    const bodyWithSession = { ...props.body, session_id: currentG2SGSession }
+                    const req = await fetch('http://localhost:3001/api/addToSession', {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(bodyWithSession),
+                    })
+                    if (req.status === 400) {
+                        const errorText = await req.json()
+                    } else {
+                        const { session_id } = await req.json()
+                        window.open(`http://localhost:3001/analyze/${session_id}`, '_blank')
+                    }
+                }
             }}
         >Submit</Button>
     </CardButton>
