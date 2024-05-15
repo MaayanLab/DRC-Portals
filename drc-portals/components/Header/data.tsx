0e90d3c44e2779daa3491d7b87481561f757d73d
shivaramakrishna99 <shivaramakrishna.srinivasan@gmail.com>
2024-05-15 14:16:33 -0700

    adding review page

--- drc-portals/components/Header/data.tsx
+++ drc-portals/components/Header/data.tsx
@@ -26,6 +26,7 @@ const nav = [
   {title: "Use Cases", href: "/data/usecases"},
   {title: "Tools & Workflows", href: "/data/tools_and_workflows"},
   {title: "Submit", href: "/data/submit"},
+  {title: "Review", href: "/data/review"},
   {title: "Documentation", href: "/info/documentation"},
   
 ]
