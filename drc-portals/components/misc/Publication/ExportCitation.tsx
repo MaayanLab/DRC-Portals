b320c417aa9464e2cd18538a407219e3517cc678
John Erol Evangelista <je.evangelista04@gmail.com>
2024-05-15 16:58:56 -0400

    updated export citation

--- drc-portals/components/misc/Publication/ExportCitation.tsx
+++ drc-portals/components/misc/Publication/ExportCitation.tsx
@@ -1,10 +1,10 @@
 'use client'
 import { Button, Menu, MenuItem, Typography } from "@mui/material"
 import { Cite, util } from '@citation-js/core'
-import { useState } from "react"
+import { useState, useMemo, cache } from "react"
 import fileDownload from 'js-file-download';
-require('@citation-js/plugin-csl')
-require('@citation-js/plugin-ris')
+import '@citation-js/plugin-csl'
+import '@citation-js/plugin-ris'
 
 const fetch_pmcid = (pmcid:string) => {
     pmcid = pmcid.replace('PMC', '')
@@ -14,45 +14,41 @@ const fetch_pmcid = (pmcid:string) => {
     return util.fetchFileAsync(url, { headers })
 }
 
+const resolve_citation = cache(async (pmcid: string) => {
+    const pmc = pmcid.replace('PMC', '')
+    const res = await fetch(`https://api.ncbi.nlm.nih.gov/lit/ctxp/v1/pmc/?format=csl&id=${pmc}`)
+    const val = await res.json()
+    
+    return val
+})
+
 const formats = ['Harvard', 'APA', 'RIS']
 const ExportCitation = ({pmcid}: {pmcid:string}) => {
-    const [citation, setCitation] = useState(null)
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         setAnchorEl(event.currentTarget);
       };
-      const handleClose = () => {
-        setAnchorEl(null);
-      };
-
-    const resolve_citation = async () => {
-        if (!citation) {
-            const pmc = pmcid.replace('PMC', '')
-            const res = await fetch(`https://api.ncbi.nlm.nih.gov/lit/ctxp/v1/pmc/?format=csl&id=${pmc}`)
-            const val = await res.json()
-            const c = new Cite(val)
-            setCitation(c)
-            return c
-        } else return citation
-        
-    }
+    const handleClose = () => {
+    setAnchorEl(null);
+    };
+    
 
     const download_citation = async (format: string) => {
-        const citation = await resolve_citation()
-        console.log(citation)
+        const val = await resolve_citation(pmcid)
+        const c = new Cite(val)
         let formatted = ''
         if (format === 'RIS') {
-            formatted = citation.format('ris')
+            formatted = c.format('ris')
         } else if (format === 'Harvard') {
-            formatted = citation.format('bibliography',  {
+            formatted = c.format('bibliography',  {
                 format: 'text',
                 template: 'harvard1',
                 lang: 'en-US'
               })
         } else  {
-            formatted = citation.format('bibliography',  {
+            formatted = c.format('bibliography',  {
                 format: 'text',
                 template: 'apa',
                 lang: 'en-US'
