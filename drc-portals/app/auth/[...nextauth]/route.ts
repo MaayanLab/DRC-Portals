b2c1e1e345653a57810931f38032ba2c98381da6
Daniel Clarke <u8sand@gmail.com>
2024-05-16 12:13:28 -0400

    Fixup HEAD override

--- drc-portals/app/auth/[...nextauth]/route.ts
+++ drc-portals/app/auth/[...nextauth]/route.ts
@@ -1,15 +1,11 @@
 import NextAuth from 'next-auth'
 import { authOptions } from '@/lib/auth'
-import { NextApiRequest, NextApiResponse } from 'next'
 
-// const handler = NextAuth(authOptions)
-// export { handler as GET, handler as POST }
+async function HEAD(req: Request) {
+    return new Response('', {
+        status: 200,
+    })
+}
 
-async function auth(req: NextApiRequest, res: NextApiResponse) {
-    if(req.method === "HEAD") {
-        return res.status(200).end()
-    }
-    return await NextAuth(req, res, authOptions)
-  }
-
-  export { auth as GET, auth as POST }
+const handler = NextAuth(authOptions)
+export { HEAD, handler as GET, handler as POST }
