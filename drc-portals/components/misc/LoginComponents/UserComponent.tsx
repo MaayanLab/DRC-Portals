7fd2d4509add35e8a0d2bac4ed9a41a11a1456fe
Daniel Clarke <u8sand@gmail.com>
2024-05-16 17:03:48 -0400

    Add keycloak account links

--- drc-portals/components/misc/LoginComponents/UserComponent.tsx
+++ drc-portals/components/misc/LoginComponents/UserComponent.tsx
@@ -1,11 +1,30 @@
 import Signin from "./Signin";
 import UserAvatar from "./UserAvatar";
+import MenuItem from '@mui/material/MenuItem';
+import { SignOutLink } from '@/lib/auth/links';
+import { AccountLink } from './MyAccount';
 import { Session } from 'next-auth';
+import getKeycloakInfo from "@/lib/auth/keycloakInfo";
 
-export default function UserComponent ({session}: {session: Session | null})  {
+export default async function UserComponent ({session}: {session: Session | null})  {
     if (session === null) {
         return <Signin/>
     } else {
-        return <UserAvatar session={session}/>
+        const userInfo = await getKeycloakInfo(session.user)
+        return (
+            <UserAvatar session={session}>
+                <MenuItem>
+                    <AccountLink />
+                </MenuItem>
+                {userInfo?.roles?.includes('ADMIN') ?
+                    <MenuItem>
+                        <a href={`https://auth.cfde.cloud/admin/cfde/console/`}>Admin Console</a>
+                    </MenuItem>
+                    : null}
+                <MenuItem>
+                    <SignOutLink>Logout</SignOutLink>
+                </MenuItem>
+            </UserAvatar>
+        )
     }
 }
