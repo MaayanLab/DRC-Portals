d5438ee5034fe6b305c81fcbbfed1e5e6d9f991f
shivaramakrishna99 <shivaramakrishna.srinivasan@gmail.com>
2024-05-23 11:41:29 -0700

    data dump in review page

--- drc-portals/app/data/review/page.tsx
+++ drc-portals/app/data/review/page.tsx
@@ -1,5 +1,4 @@
-'use client'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import TableContainer from '@mui/material/TableContainer';
@@ -9,47 +8,36 @@ import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import prisma from '@/lib/prisma';
-import { QueryForm } from './QueryForm';
+import SQL from '@/lib/prisma/raw';
 
-export default function ReviewPage() {
-    const [tableData, setTableData] = useState([]);
-
-    const fetchData = async (query) => {
-        try {
-            const result = await prisma.$queryRaw(query);
-            setTableData(result);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
-
-    const handleQueryGenerated = (query) => {
-        fetchData(query);
-    };
+export default async function ReviewPage() {
+    
+    const result = await prisma.$queryRaw`SELECT * FROM sparc.anatomy LIMIT 10`;
 
     return (
         <>
-            <QueryForm onQueryGenerated={handleQueryGenerated} />
-            {tableData.length > 0 && (
+            {result.length > 0 && (
                 <Grid item xs={12}>
-                    <Typography variant="h6">Table Data</Typography>
-                    <TableContainer component={Table}>
-                        <TableHead>
-                            <TableRow>
-                                {Object.keys(tableData[0]).map((key) => (
-                                    <TableCell key={key}>{key}</TableCell>
-                                ))}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {tableData.map((row, index) => (
-                                <TableRow key={index}>
-                                    {Object.values(row).map((value, index) => (
-                                        <TableCell key={index}>{value}</TableCell>
+                    <Typography variant="h6">Preview</Typography>
+                    <TableContainer>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    {Object.keys(result[0]).map((key) => (
+                                        <TableCell key={key}>{key}</TableCell>
                                     ))}
                                 </TableRow>
-                            ))}
-                        </TableBody>
+                            </TableHead>
+                            <TableBody>
+                                {result.map((row, rowIndex) => (
+                                    <TableRow key={rowIndex}>
+                                        {Object.values(row).map((value, cellIndex) => (
+                                            <TableCell key={cellIndex}>{value}</TableCell>
+                                        ))}
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
                     </TableContainer>
                 </Grid>
             )}
