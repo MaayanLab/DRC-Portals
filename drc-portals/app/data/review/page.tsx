8f52dfe002d62793087a86aa9650191484dfdc83
shivaramakrishna99 <shivaramakrishna.srinivasan@gmail.com>
2024-05-23 18:40:21 -0700

    simultaneously rendering dropdowns and table

--- drc-portals/app/data/review/page.tsx
+++ drc-portals/app/data/review/page.tsx
@@ -1,12 +1,19 @@
 import prisma from '@/lib/prisma';
+import { Prisma } from '@prisma/client';
 import ReviewDisplay from './ReviewDisplay';
+import QueryForm from './QueryForm';
+
 
 export default async function ReviewPage() {
 
-    const result = await prisma.$queryRaw`SELECT * FROM sparc.biosample LIMIT 10`;
+    const mySchema = 'gtex';
+    const myTable = 'file';
+    const query = Prisma.sql`SELECT * FROM ${Prisma.join([Prisma.sql`${Prisma.raw(mySchema + '.' + myTable)}`])} LIMIT 10;`;
+    const result = await prisma.$queryRaw(query);
 
     return (
         <>
+        <QueryForm />
             <ReviewDisplay result={result}/>        
         </>
     );
