abe2950059080d3ca9deebf9906342752a6298a4
mano-at-sdsc <mano@sdsc.edu>
2024-05-27 23:33:28 -0700

    Almost passing schema name as a parameter (reusing q) for this page (review2)

--- drc-portals/app/data/review2/page.tsx
+++ drc-portals/app/data/review2/page.tsx
@@ -13,23 +13,24 @@ export default async function ReviewPage(props: PageProps) {
     // Set the schemaname in q for now for this page
     const searchParams = useSanitizedSearchParams(props);
 
-    const mySchema = 'gtex';
+    // Mano: for counts for tables:
+    const mySchema = searchParams.q ?? 'c2m2'; // 'gtex'
+
     const myTable = 'file';
     const query = Prisma.sql`SELECT * FROM ${Prisma.join([Prisma.sql`${Prisma.raw(mySchema + '.' + myTable)}`])} LIMIT 10;`;
     const result = await prisma.$queryRaw(query);
 
-    // Mano: for counts for tables:
-    const schemaname = searchParams.q ?? 'c2m2';
     //const countstrs: string[] |undefined = await CountDisplay(props);
     //const countstrs_with_br = countstrs?.join("<br>")
     //console.log("countstr:", countstrs);
     //console.log("countstr:", countstrs_with_br);
-    const tables_counts = await CountDisplay(props, schemaname);
+    const tables_counts = await CountDisplay(props, mySchema);
     console.log("tables_counts:", tables_counts);
     return (
         <>
         {/*<QueryForm />*/}
         <ReviewDisplay result={result}/>
+        Count of rows in tables from schema: {mySchema}
         <ReviewDisplay result={tables_counts}/>
         {/*countstrs_with_br*/}
         {/*countstrs.map((countstr, index) => (countstr)) */}
