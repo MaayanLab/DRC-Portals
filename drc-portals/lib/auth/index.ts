c702c24a453f8a6c12f2b00a0b4986b8d0ec1863
Daniel Clarke <u8sand@gmail.com>
2024-05-28 15:47:11 -0400

    Ensure keycloak UserInfo is up to date

--- drc-portals/lib/auth/index.ts
+++ drc-portals/lib/auth/index.ts
@@ -10,32 +10,36 @@ import GlobusProvider from '@/lib/auth/providers/globus'
 import type { OAuthConfig } from 'next-auth/providers/index'
 import PrismaAdapterEx from './adapters/prisma'
 import { z } from 'zod'
-import { jwtDecode } from "jwt-decode";
 
-function parseKeycloakUserinfo(accessToken: string) {
-  if (!accessToken) return
-  const userInfo = jwtDecode(accessToken)
+async function getKeycloakUserInfo(accessToken: string) {
+  const req = await fetch(`https://auth.cfde.cloud/realms/cfde/protocol/openid-connect/userinfo`, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+  if (!req.ok) throw new Error(`Keycloak UserInfo returned ${req.status}`)
+  const userInfo = await req.json()
   const userInfoParsed = z.object({
-    iss: z.literal('https://auth.cfde.cloud/realms/cfde'),
     name: z.string(),
+    given_name: z.string(),
+    family_name: z.string(),
     email: z.string(),
+    preferred_username: z.string(),
     resource_access: z.object({
       'DRC-Portal': z.object({
         roles: z.string().array(),
-      }),
-    }),
-  }).safeParse(userInfo)
-  if (!userInfoParsed.success) return
-  const roles = userInfoParsed.data.resource_access["DRC-Portal"].roles.filter(role => role.startsWith('role:')).map(role => role.slice('role:'.length))
-  const dccs = userInfoParsed.data.resource_access["DRC-Portal"].roles.filter(role => role.startsWith('dcc:')).map(role => role.slice('dcc:'.length)).filter((dcc) => dcc !== '*')
+      }).passthrough(),
+    }).passthrough(),
+  }).passthrough().safeParse(userInfo)
+  if (!userInfoParsed.success) throw new Error('UserInfo is Corrupted')
   return {
-    name: userInfoParsed.data.name,
-    email: userInfoParsed.data.email,
-    roles,
-    dccs,
+    ...userInfoParsed.data,
+    roles: userInfoParsed.data.resource_access["DRC-Portal"].roles.filter(role => role.startsWith('role:')).map(role => role.slice('role:'.length)),
+    dccs: userInfoParsed.data.resource_access["DRC-Portal"].roles.filter(role => role.startsWith('dcc:')).map(role => role.slice('dcc:'.length)).filter((dcc) => dcc !== '*'),
   }
 }
-type KeycloakUserInfo = ReturnType<typeof parseKeycloakUserinfo>
+type KeycloakUserInfo = ReturnType<typeof getKeycloakUserInfo> extends Promise<infer T> ? T : never
 
 declare module 'next-auth' {
   interface Session {
@@ -122,21 +126,24 @@ const hostName = process.env.NEXTAUTH_URL ? new URL(process.env.NEXTAUTH_URL).ho
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapterEx(),
   providers,
-  session: { strategy: 'jwt' },
+  session: {
+    maxAge: 60*60*10,
+    strategy: 'jwt',
+  },
   callbacks: {
     async jwt({ token, account, user }) {
       if (account) {
         // Persist the OAuth access_token and or the user id to the token right after signin
         token.id = user?.id
-        token.keycloakInfo = parseKeycloakUserinfo(account.access_token as string)
+        token.accessToken = account.access_token
       }
       return token
     },
-    session({ session, token }) {
+    async session({ session, token }) {
       const id = token.sub ?? token.id
       if (typeof id !== 'string') throw new Error('Missing user id')
       session.user.id = id
-      session.keycloakInfo = token.keycloakInfo as KeycloakUserInfo
+      session.keycloakInfo = await getKeycloakUserInfo(token.accessToken as string)
       if (session.keycloakInfo) {
         session.user.name = session.keycloakInfo.name
         session.user.email = session.keycloakInfo.email
