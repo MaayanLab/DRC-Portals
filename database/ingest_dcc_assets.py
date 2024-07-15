7467f86844e0684372820ae973891d9936b29024
Daniel Clarke <u8sand@gmail.com>
2024-07-15 17:42:18 -0400

    Update ingest scripts with entity_type/pagerank

--- database/ingest_dcc_assets.py
+++ database/ingest_dcc_assets.py
@@ -10,7 +10,7 @@ dcc_assets = current_dcc_assets()
 # Ingest C2M2
 
 dcc_asset_helper = TableHelper('dcc_asset_node', ('id', 'link',), pk_columns=('id',))
-node_helper = TableHelper('node', ('id', 'type', 'label', 'description', 'dcc_id',), pk_columns=('id',))
+node_helper = TableHelper('node', ('id', 'type', 'entity_type', 'label', 'description', 'pagerank', 'dcc_id',), pk_columns=('id',), add_columns=('pagerank',))
 
 with dcc_asset_helper.writer() as dcc_asset:
   with node_helper.writer() as node:
@@ -24,6 +24,8 @@ with dcc_asset_helper.writer() as dcc_asset:
         dcc_id=file['dcc_id'],
         id=dcc_asset_id,
         type='dcc_asset',
+        entity_type=None,
         label=f"{file['dcc_short_label']}_{file['filename']}",
         description=f"A {file['filetype']} processed data file from {file['dcc_short_label']}",
+        pagerank=1,
       ))
