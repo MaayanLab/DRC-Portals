671dcf1c8dd6c7b3544dc5e4867fd59df52f3859
Daniel Clarke <u8sand@gmail.com>
2024-07-16 19:23:16 -0400

    Remove some suspense which breaks layout caching

--- drc-portals/app/data/processed/search/@tab/[[...type]]/page.tsx
+++ drc-portals/app/data/processed/search/@tab/[[...type]]/page.tsx
@@ -1,11 +1,10 @@
-import { FancyTab } from "@/components/misc/FancyTabs";
 import AllSearchPages from '@/app/data/processed/search/AllSearchPages'
 import { SearchQueryComponent as C2M2SearchQueryComponent} from '@/app/data/c2m2/search/SearchQueryComponent'
 import React from "react";
 
-export default function TypeTab(props: any) {
+export default function TypeTab(props: { params: { type?: string | string[] }, searchParams: Record<string, string> }) {
   return <>
-    <React.Suspense fallback={<FancyTab id="c2m2" label="Cross-Cut Metadata" loading />}><C2M2SearchQueryComponent tab {...props} /></React.Suspense>
-    <React.Suspense fallback={<FancyTab id="all" label="Processed Data" loading />}><AllSearchPages {...props} /></React.Suspense>
+    <AllSearchPages {...props} />
+    <C2M2SearchQueryComponent tab {...props} />
   </>
 }
