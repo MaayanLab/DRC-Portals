2dd5326f4daf9ef79c89287c62168ccfaff81423
Daniel Clarke <u8sand@gmail.com>
2024-07-18 11:40:54 -0400

    Add more loading pages

---
+++ drc-portals/app/data/processed/c2m2_file/loading.tsx
@@ -0,0 +1,43 @@
+import { mdiArrowLeft } from "@mdi/js";
+import Icon from "@mdi/react";
+import { Button } from "@mui/material";
+import Link from "next/link";
+import React from "react";
+import ListingPageLayout from "@/app/data/processed/ListingPageLayout";
+import SearchablePagedTable from "@/app/data/processed/SearchablePagedTable";
+
+export default function Page() {
+  return (
+    <ListingPageLayout
+      count={0}
+      maxCount={100}
+      footer={
+        <Link href="/data">
+          <Button
+            sx={{textTransform: "uppercase"}}
+            color="primary"
+            variant="contained"
+            startIcon={<Icon path={mdiArrowLeft} size={1} />}>
+              BACK TO SEARCH
+          </Button>
+        </Link>
+      }
+    >
+      <SearchablePagedTable
+        loading
+        q={''}
+        p={1}
+        r={10}
+        count={0}
+        columns={[
+          <>&nbsp;</>,
+          <>Label</>,
+          <>Description</>,
+          <>Data Type</>,
+          <>Assay Type</>,
+        ]}
+        rows={[]}
+      />
+    </ListingPageLayout>
+  )
+}
---
+++ drc-portals/app/data/processed/kg_relation/[id]/loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LandingPageLayout from "@/app/data/processed/LandingPageLayout";
+
+export default function Page() {
+  return (
+    <LandingPageLayout
+      title={"Loading..."}
+      subtitle={""}
+      description={""}
+      metadata={[]}
+    />
+  )
+}
