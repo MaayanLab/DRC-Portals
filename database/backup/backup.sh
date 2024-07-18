e062a195567ab74e31ddfae573bf60ed5b3e1c5c
Daniel Clarke <u8sand@gmail.com>
2024-07-18 17:23:38 -0400

    Backup more tables daily

--- database/backup/backup.sh
+++ database/backup/backup.sh
@@ -5,5 +5,10 @@ export AWS_SECRET_ACCESS_KEY="$S3_POSTGRES_BACKUP_AWS_SECRET_ACCESS_KEY"
 export AWS_DEFAULT_REGION="$S3_POSTGRES_BACKUP_AWS_DEFAULT_REGION"
 
 PGPASSWORD="${POSTGRES_PASSWORD}" pg_dump -h "${POSTGRES_HOST}" -U "${POSTGRES_USER}" -Fc -Z 9 "${POSTGRES_DB}" \
-  --column-inserts -a -t '"public"."User"' -t '"public"."Account"' -t '"public"."Session"' -t '"public"."dcc_assets"' -t '"public"."code_assets"' -t '"public"."file_assets"' \
-  | aws s3 cp - "s3://${S3_POSTGRES_BACKUP_PREFIX}/$(date +%Y-%m-%d).pgdump"
+  -t '"public"."User"' -t '"public"."Account"' -t '"public"."Session"' -t '"public"."_DCCToUser"' \
+  -t '"public"."dcc_assets"' -t '"public"."code_assets"' -t '"public"."file_assets"' \
+  | aws s3 cp - "s3://${S3_POSTGRES_BACKUP_PREFIX}/$(date +%Y-%m-%d)-public.pgdump"
+
+PGPASSWORD="${POSTGRES_PASSWORD}" pg_dump -h "${POSTGRES_HOST}" -U "${POSTGRES_USER}" -Fc -Z 9 "${POSTGRES_DB}" \
+  -n keycloak \
+  | aws s3 cp - "s3://${S3_POSTGRES_BACKUP_PREFIX}/$(date +%Y-%m-%d)-keycloak.pgdump"
