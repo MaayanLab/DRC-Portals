7a9da86b7e098347a040190f4ecb20fcd709994c
Daniel Clarke <u8sand@gmail.com>
2024-08-07 17:31:17 -0400

    Fixes #287

--- drc-portals/components/Header/client.tsx
+++ drc-portals/components/Header/client.tsx
@@ -3,9 +3,9 @@ import { Typography } from "@mui/material"
 import { usePathname } from "next/navigation"
 
 export  function TextNav({title, path}: {title: string, path: string}) {
-	const pathname = usePathname().replace("/info", "/").replace("/data", "/")
+	const pathname = usePathname().replace(/^\/info/, "/").replace(/^\/data/, "/")
 	let sx
-	if ((pathname.indexOf(path) !== -1 && path !== '') || ((pathname.indexOf('/processed') > -1 || pathname === "/") && path === "")) {
+	if ((pathname.indexOf(path) !== -1 && path !== '') || ((/^\/(search|c2m2|processed|$)/.exec(pathname) !== null) && path === "")) {
 		sx = {textDecoration: "underline", textDecorationThickness: 2}
 	}
 	if (pathname.indexOf('training_and_outreach/cfde-webinar-series')>-1 && path==='/training_and_outreach') {
