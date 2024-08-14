2e2571f15823e9cf7c0842cf553148f0ddc6942a
Ethan Sanchez <ethan.dsanch@gmail.com>
2024-08-14 16:49:15 -0700

    Add "Rotate" btns to chart toolbar
    Also slightly simplifies the zoom logic.

--- drc-portals/components/prototype/components/CytoscapeChart/ChartToolbar.tsx
+++ drc-portals/components/prototype/components/CytoscapeChart/ChartToolbar.tsx
@@ -1,4 +1,6 @@
 import FitScreenIcon from "@mui/icons-material/FitScreen";
+import Rotate90DegreesCwIcon from "@mui/icons-material/Rotate90DegreesCw";
+import Rotate90DegreesCcwIcon from "@mui/icons-material/Rotate90DegreesCcw";
 import ZoomInIcon from "@mui/icons-material/ZoomIn";
 import ZoomOutIcon from "@mui/icons-material/ZoomOut";
 import { IconButton, Paper, Tooltip } from "@mui/material";
@@ -6,6 +8,7 @@ import Divider from "@mui/material/Divider";
 import { LayoutOptions } from "cytoscape";
 
 import { CustomToolbarFnFactory, CytoscapeReference } from "../../types/cy";
+import { lockD3ForceNode } from "../../utils/cy";
 
 type ChartToolbarProps = {
   cyRef: CytoscapeReference;
@@ -25,15 +28,34 @@ export default function ChartToolbar(cmpProps: ChartToolbarProps) {
       let currentZoom = cy.zoom();
       const zoomModifier =
         (currentZoom / (currentZoom + 1)) * (zoomOut ? -1 : 1);
-      const currentDimensions = cy.elements().boundingBox();
+      const center = { x: cy.width() / 2, y: cy.height() / 2 };
 
       cy.zoom({
         level: currentZoom + zoomModifier,
-        renderedPosition: {
-          x: currentDimensions.w / 2,
-          y: currentDimensions.h / 2,
-        },
+        renderedPosition: center,
       });
+    }
+  };
+
+  const handleRotate = (angle: number) => {
+    const cy = cyRef.current;
+    if (cy !== undefined && layout !== undefined) {
+      const center = { x: cy.width() / 2, y: cy.height() / 2 };
+      const rad = angle * (Math.PI / 180); // Convert angle to radians
+
+      cy.batch(() => {
+        cy.nodes().forEach((node) => {
+          const pos = node.position();
+          const x = pos.x - center.x;
+          const y = pos.y - center.y;
+          const newX = x * Math.cos(rad) - y * Math.sin(rad) + center.x;
+          const newY = x * Math.sin(rad) + y * Math.cos(rad) + center.y;
+
+          node.position({ x: newX, y: newY });
+          lockD3ForceNode(node);
+        });
+      });
+      cy.layout(layout).run();
     }
   };
 
@@ -59,6 +81,17 @@ export default function ChartToolbar(cmpProps: ChartToolbarProps) {
         ? null
         : [...customTools.map((factoryFn) => factoryFn(cyRef, layout))]}
       <Divider orientation="vertical" variant="middle" flexItem />
+
+      <Tooltip title="Rotate Clockwise" arrow>
+        <IconButton aria-label="rotate-cw" onClick={() => handleRotate(90)}>
+          <Rotate90DegreesCwIcon />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Rotate Counter Clockwise" arrow>
+        <IconButton aria-label="rotate-ccw" onClick={() => handleRotate(-90)}>
+          <Rotate90DegreesCcwIcon />
+        </IconButton>
+      </Tooltip>
       <Tooltip title="Zoom In" arrow>
         <IconButton aria-label="zoom-in" onClick={() => handleZoom(false)}>
           <ZoomInIcon />
