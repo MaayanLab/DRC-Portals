2a8f024718e182905d0f56d819e650a42deaa777
John Erol Evangelista <je.evangelista04@gmail.com>
2024-10-03 13:33:12 -0400

    added see more past events

--- drc-portals/components/misc/Outreach/ClientExpander.tsx
+++ drc-portals/components/misc/Outreach/ClientExpander.tsx
@@ -10,14 +10,12 @@ export const ClientExpander = ({children}: {children: React.ReactNode}) => {
 	const pathname = usePathname()
 	const searchParams = useSearchParams()
 	const filter = searchParams.get('filter')
-	const query_parser = parseAsJson<OutreachParams>().withDefault({type: ['outreach', 'training'], tags:[], expand_filter: true, status: ['active', 'recurring', 'past'], cfde_specific: true})
+	const query_parser = parseAsJson<OutreachParams>().withDefault({type: ['outreach', 'training'], tags:[], expand_filter: false, status: ['active', 'recurring', 'past'], cfde_specific: true})
     const parsedParams: OutreachParams = query_parser.parseServerSide(filter || undefined)
 	
 	useEffect(()=>{
-		const query: OutreachParams = {
-			...parsedParams,
-			expand_filter: ['xs', 'sm'].indexOf(width) === -1
-		}
+		console.log(width)
+		const query: OutreachParams = parsedParams
 		router.push(`${pathname}?filter=${JSON.stringify(query)}`)
 	},[])
 	return children
