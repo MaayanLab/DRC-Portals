d5419940586c178ec09edf4deef85011244718df
Daniel Clarke <u8sand@gmail.com>
2024-10-07 14:48:17 -0400

    Add GSE Link for Gene Sets
    Fixes #272

--- drc-portals/app/data/processed/gene_set/[id]/modules.tsx
+++ drc-portals/app/data/processed/gene_set/[id]/modules.tsx
@@ -4,6 +4,7 @@
  */
 import PWBButton from "@/app/data/processed/PWBButton";
 import G2SGButton from "./G2SGButton";
+import GSEButton from "./GSEButton";
 import prisma from "@/lib/prisma/slow";
 import { cache } from "react";
 
@@ -41,6 +42,18 @@ const modules: {
       }}
       title="GeneSetCart"
       description={<>GeneSetCart helps you to fetch gene sets from various data sources, augment, combine with set operations, visualize and analyze these gene sets in a single session. Start a new session with {item.node.label}.</>}
+    />,
+  },
+  {
+    compatible: (item) => true,
+    button: async ({ item }) => <GSEButton
+      body={{
+        "term": item.node.label,
+        "genes": await getGeneSet(item.id),
+        "description": item.node.description
+      }}
+      title="CFDE Gene Set Enrichment (GSE)"
+      description={<>CFDE-GSE illuminates connections between the input gene set and various CF gene sets that overlap with the queried gene set. Query CFDE-GSE with {item.node.label}.</>}
     />,
   },
 ]
