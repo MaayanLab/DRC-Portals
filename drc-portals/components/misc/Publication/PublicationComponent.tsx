d94b92efaff25aac9d2836e6238bd9ee8392ead7
Heesu Kim <heesukim628@gmail.com>
2024-11-14 10:17:24 -0500

    Add r03 to publication & camel case journal names

--- drc-portals/components/misc/Publication/PublicationComponent.tsx
+++ drc-portals/components/misc/Publication/PublicationComponent.tsx
@@ -10,17 +10,34 @@ type PublicationWithDCC = Prisma.PublicationGetPayload<{
             dcc: true
           }
         }
+        r03s: {
+            include: {
+              r03: true
+            }
+          }
     }
   }>
 export default function PublicationComponent({publications}: {publications: PublicationWithDCC[]}) {
+    const toCamelCase = (str: String) => {
+        return str.toLowerCase().split(' ').map(word =>
+            word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        ).join(' ');
+    };
     return (
         <>
             {publications.map((pub, i)=>(
                 <>
                     <div key={i} className="mb-2 space-x-1">
                         <>
-                            <Typography color="secondary" variant="caption">
-                                {pub.authors}. {pub.year}. <b>{pub.title}{!pub.title.endsWith(".") && "."}</b> {pub.journal}. {pub.volume}. {pub.page}
+                            <Typography color="secondary" variant="caption"  sx={{ wordBreak: 'break-all',   overflowWrap: 'break-word', display: 'inline-block', maxWidth: '100%'  }}>
+                                {pub.authors}. {pub.year}. <b>{pub.title}{!pub.title.endsWith(".") && "."}</b> 
+                                {pub.journal&& toCamelCase(pub.journal)}
+                                . {pub.volume}. {pub.page}
+                                { pub.doi && 
+                                    <Link target="_blank" rel="noopener noreferrer" href={`https://www.doi.org/${pub.doi}`}>
+                                       <Typography  variant="caption"  > https://www.doi.org/${pub.doi}</Typography>
+                                  </Link>
+                                }
                             </Typography>
                             <div className="flex space-x-1 items-center justify-end">
                                 { pub.pmid && 
@@ -48,6 +65,11 @@ export default function PublicationComponent({publications}: {publications: Publ
                                         <Chip key={dcc.short_label} label={dcc.short_label} color="primary" sx={{borderRadius: 2, paddingLeft: 0, paddingRight: 0}}/>
                                     ))
                                 }
+                                 {pub.r03s && pub.r03s.length > 0 && (
+                                    pub.r03s.map((r03, index) => (
+                                        <Chip key={index} label={'R03'} color="primary" sx={{borderRadius: 2, paddingLeft: 0, paddingRight: 0}} />
+                                    ))
+                                )}
                             </div>
                         </>
                     </div>
