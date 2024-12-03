a4c0d38b3948ff4380d2c119c467b4d006fe740b
Heesu Kim <heesukim628@gmail.com>
2024-12-03 15:02:48 -0500

    Update CFDE webinar page

--- drc-portals/components/misc/YoutubeEmbed.tsx
+++ drc-portals/components/misc/YoutubeEmbed.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 
-const YoutubeEmbed = ({ embedId }: {embedId: string}) => (
-  <div className="video-responsive">
+
+const aspectRatio = 9/16;
+const YoutubeEmbed = ({ embedId, widthPercentage = 80  }: {embedId: string, widthPercentage?: number}) => (
+
+  // <div className="video-responsive">
+  <div style={{ 
+    width: `${widthPercentage}%`,
+    margin: '0 auto', // Centers the container
+    position: 'relative',
+    paddingTop: `${widthPercentage * aspectRatio}%`, // Maintains aspect ratio
+  }}>
     <iframe
-      width="430"
-      height="240"
+      // width="430"
+      // height="240"
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        border: 0,
+      }}
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
