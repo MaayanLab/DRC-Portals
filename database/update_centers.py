55a05aac78e730d529054773c529210b3353349f
Daniel Clarke <u8sand@gmail.com>
2025-01-13 11:03:44 -0500

    Set up update_centers.py

--- database/update_centers.py
+++ database/update_centers.py
@@ -7,8 +7,7 @@ from uuid import uuid5, NAMESPACE_URL
 import boto3
 from botocore.exceptions import ClientError
 from ingest_common import connection
-import math
-import json
+import pathlib
 if len(sys.argv) == 0 or not sys.argv[1].endswith("tsv"):
 		raise Exception("Please add a tsv file")
 
@@ -48,64 +47,50 @@ now = str(date.today()).replace("-", "")
 df = pd.read_csv(filename, sep="\t")
 
 data = {}
-dcc_partnership = []
-partnership_publication = []
+center_publication = []
 for i, row in df.iterrows():
-	title = row['title']
-	uid = str(uuid5(NAMESPACE_URL, title))
+	label = row['label']
+	uid = str(uuid5(NAMESPACE_URL, label))
 	data[uid] = {
-		"title": row["title"],
+		"label": row["label"],
+		"short_label": row["short_label"],
+		"short_description": row["short_description"],
 		"description": row["description"],
-		"status": row["status"],
-		"website": row["website"],
-		"priority": int(row["priority"] if not math.isnan(row['priority']) else 0),
-		"image": row["image"],
+		"homepage": row["homepage"],
+		"icon": row["icon"],
 		"grant_num": row["grant_num"],
+		"active": row["active"],
 	}
-	if type(row["dccs"]) == str:
-		lead_dccs = [dcc_mapper[i] for i in row["lead_dccs"].strip().split("; ")] if type(row["lead_dccs"]) == str else []
-		for dcc in set([dcc_mapper[i] for i in row["dccs"].strip().split("; ")]):
-			dcc_partnership.append({"partnership_id": uid, "dcc_id": dcc, "lead": dcc in lead_dccs})
-	if type(row["publications"]) == str:
+	if type(row.get("publications")) == str:
 		for pub in set(row["publications"].strip().split("; ")):
-			partnership_publication.append({"partnership_id": uid, "publication_id": pub})
+			center_publication.append({"center_id": uid, "publication_id": pub})
 
-partnership_df = pd.DataFrame.from_dict(data, orient="index").fillna('')
-dcc_partnership_df = pd.DataFrame.from_records(dcc_partnership)
-partnership_publication_df = pd.DataFrame.from_dict(data, orient="index").fillna('')
-partnership_publication_df = pd.DataFrame.from_records(partnership_publication)
-							 
-partnership_file = "partnership_files/%s_partnership.tsv"%now
-dcc_partnership_file = "partnership_files/%s_dcc_partnership.tsv"%now
-partnership_publication_file = "partnership_files/%s_partnership_publication.tsv"%now
+center_df = pd.DataFrame.from_dict(data, orient="index").fillna('')
+center_publication_df = pd.DataFrame.from_records(center_publication, columns=['center_id', 'publication_id'])
 
-partnership_df.index.name = "id"
-partnership_df.to_csv(partnership_file, sep="\t", header=True, quoting=csv.QUOTE_NONE)
-dcc_partnership_df.to_csv(dcc_partnership_file, sep="\t", header=True, index=None)
-partnership_publication_df.to_csv(partnership_publication_file, sep="\t", header=True, index=None)
+center_file = "center_files/%s_center.tsv"%now
+center_publication_file = "center_files/%s_center_publication.tsv"%now
+
+pathlib.Path('center_files').mkdir(exist_ok=True)
+center_df.index.name = "id"
+center_df.to_csv(center_file, sep="\t", header=True, quoting=csv.QUOTE_NONE)
+center_publication_df.to_csv(center_publication_file, sep="\t", header=True, index=None)
 
 print("Uploading to s3")
 
-filename = partnership_file.replace('partnership_files', 'database/partnership_files')
+filename = center_file.replace('center_files', 'database/center_files')
 print(filename)
-upload_file(partnership_file, bucket, filename)
+upload_file(center_file, bucket, filename)
 filename = filename.replace(now, "current")
 print(filename)
-upload_file(partnership_file, bucket, filename)
+upload_file(center_file, bucket, filename)
 
-filename = dcc_partnership_file.replace('partnership_files', 'database/partnership_files')
+filename = center_publication_file.replace('center_files', 'database/publication_files')
 print(filename)
-upload_file(dcc_partnership_file, bucket, filename)
+upload_file(center_publication_file, bucket, filename)
 filename = filename.replace(now, "current")
 print(filename)
-upload_file(dcc_partnership_file, bucket, filename)
-
-filename = partnership_publication_file.replace('partnership_files', 'database/publication_files')
-print(filename)
-upload_file(partnership_publication_file, bucket, filename)
-filename = filename.replace(now, "current")
-print(filename)
-upload_file(partnership_publication_file, bucket, filename)
+upload_file(center_publication_file, bucket, filename)
 
 # ingest
 print("ingesting...")
@@ -113,26 +98,22 @@ print("ingesting...")
 cur = connection.cursor()
 
 cur.execute('''
-	DELETE FROM dcc_partnerships;
+	DELETE FROM center_publications;
 ''')
 
 cur.execute('''
-	DELETE FROM partnership_publications;
-''')
-
-cur.execute('''
-	DELETE FROM partnerships;
+	DELETE FROM centers;
 ''') 
 
 cur.execute('''
-	create table partnerships_tmp
-	as table partnerships
+	create table centers_tmp
+	as table centers
 	with no data;
 ''')
 
-with open(partnership_file, 'r') as fr:
+with open(center_file, 'r') as fr:
 		columns = next(fr).strip().split('\t')
-		cur.copy_from(fr, 'partnerships_tmp',
+		cur.copy_from(fr, 'centers_tmp',
 			columns=columns,
 			null='',
 			sep='\t',
@@ -140,29 +121,29 @@ with open(partnership_file, 'r') as fr:
 column_string = ", ".join(columns)
 set_string = ",\n".join(["%s = excluded.%s"%(i,i) for i in columns])
 cur.execute('''
-		insert into partnerships (%s)
+		insert into centers (%s)
 			select %s
-			from partnerships_tmp
+			from centers_tmp
 			on conflict (id)
 				do update
 				set %s
 		;
 	'''%(column_string, column_string, set_string))
-cur.execute('drop table partnerships_tmp;')
+cur.execute('drop table centers_tmp;')
 
 connection.commit()
 
 cur = connection.cursor()
 cur.execute('''
-	create table dcc_partnerships_tmp
-	as table dcc_partnerships
+	create table center_publications_tmp
+	as table center_publications
 	with no data;
 ''')
 
 
-with open(dcc_partnership_file, 'r') as fr:
+with open(center_publication_file, 'r') as fr:
 		columns = next(fr).strip().split('\t')
-		cur.copy_from(fr, 'dcc_partnerships_tmp',
+		cur.copy_from(fr, 'center_publications_tmp',
 			columns=columns,
 			null='',
 			sep='\t',
@@ -171,42 +152,13 @@ with open(dcc_partnership_file, 'r') as fr:
 column_string = ", ".join(columns)
 
 cur.execute('''
-		insert into dcc_partnerships (%s)
+		insert into center_publications (%s)
 			select %s
-			from dcc_partnerships_tmp
+			from center_publications_tmp
 			on conflict 
 				do nothing
 		;
 	'''%(column_string, column_string))
-cur.execute('drop table dcc_partnerships_tmp;')
-connection.commit()
-
-cur = connection.cursor()
-cur.execute('''
-	create table partnership_publications_tmp
-	as table partnership_publications
-	with no data;
-''')
-
-
-with open(partnership_publication_file, 'r') as fr:
-		columns = next(fr).strip().split('\t')
-		cur.copy_from(fr, 'partnership_publications_tmp',
-			columns=columns,
-			null='',
-			sep='\t',
-		)
-
-column_string = ", ".join(columns)
-
-cur.execute('''
-		insert into partnership_publications (%s)
-			select %s
-			from partnership_publications_tmp
-			on conflict 
-				do nothing
-		;
-	'''%(column_string, column_string))
-cur.execute('drop table partnership_publications_tmp;')
+cur.execute('drop table center_publications_tmp;')
 connection.commit()
 
