e641f815875650d8de9cd89f14c7044ba7b86477
Daniel Clarke <u8sand@gmail.com>
2025-01-30 17:11:24 -0500

    Ingest things one dcc at a time

--- database/ingest_dcc_assets.py
+++ database/ingest_dcc_assets.py
@@ -12,9 +12,9 @@ dcc_assets = current_dcc_assets()
 dcc_asset_helper = TableHelper('dcc_asset_node', ('id', 'link',), pk_columns=('id',))
 node_helper = TableHelper('node', ('id', 'type', 'entity_type', 'label', 'description', 'pagerank', 'dcc_id',), pk_columns=('id',), add_columns=('pagerank',))
 
-with dcc_asset_helper.writer() as dcc_asset:
-  with node_helper.writer() as node:
-    for _, file in tqdm(dcc_assets.iterrows(), total=dcc_assets.shape[0], desc='Processing DCC Assets...'):
+for _, file in tqdm(dcc_assets.iterrows(), total=dcc_assets.shape[0], desc='Processing DCC Assets...'):
+  with dcc_asset_helper.writer() as dcc_asset:
+    with node_helper.writer() as node:
       dcc_asset_id = str(uuid5(uuid0, file['link']))
       dcc_asset.writerow(dict(
         id=dcc_asset_id,
