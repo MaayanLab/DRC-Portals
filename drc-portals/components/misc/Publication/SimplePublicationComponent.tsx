d28ca8b9cd4334cf0cf68cfc960b9dc12a332c56
Heesu Kim <heesukim628@gmail.com>
2025-03-04 13:28:47 -0500

    Update /centers/drc page

--- drc-portals/components/misc/Publication/SimplePublicationComponent.tsx
+++ drc-portals/components/misc/Publication/SimplePublicationComponent.tsx
@@ -4,13 +4,16 @@ import { Publication } from "@prisma/client";
 import ExportCitation from "./ExportCitation";
 
 export default function SimplePublicationComponent({publications}: {publications: Publication[]}) {
+    function addPeriodIfNeeded(text: string | undefined) {
+        return text && !text.endsWith(".") ? "." : "";
+       }
     return (
         <>
             {publications.map((pub, i)=>(
                     <div key={i} className="mb-5 space-x-1">
                         <Link target="_blank" rel="noopener noreferrer" href={`https://pubmed.ncbi.nlm.nih.gov/${pub.pmid}/`}>
                             <Typography color="secondary" variant="caption">
-                                {pub.authors}. {pub.year}. <b>{pub.title}{!pub.title.endsWith(".") && "."}</b> {pub.journal}. {pub.volume}. {pub.page}
+                                {pub.authors}. {pub.year}. <b>{pub.title}{addPeriodIfNeeded(pub.title)}</b> {pub.journal}. {pub.volume}. {pub.page}
                             </Typography>
                         </Link>
                     </div>
