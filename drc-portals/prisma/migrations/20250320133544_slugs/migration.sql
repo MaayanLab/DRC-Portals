07cc2dc6a1244c4e17b15273ce0479b91219eae7
Daniel Clarke <u8sand@gmail.com>
2025-03-20 09:40:46 -0400

    Add slug migration

---
+++ drc-portals/prisma/migrations/20250320133544_slugs/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[type,entity_type,slug]` on the table `node` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `slug` to the `node` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "node" ADD COLUMN     "slug" TEXT;
+
+UPDATE "node" SET "slug" = "id"::TEXT;
+
+ALTER TABLE "node" ALTER COLUMN "slug" SET NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "node_type_entity_type_slug_key" ON "node"("type", "entity_type", "slug");
