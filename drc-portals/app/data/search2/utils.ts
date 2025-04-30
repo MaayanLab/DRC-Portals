4802690d3117efe8b09ad9ff300a7f83d6102505
Daniel Clarke <u8sand@gmail.com>
2025-04-30 14:03:04 -0400

    Fixup performance of partial exact count

--- drc-portals/app/data/search2/utils.ts
+++ drc-portals/app/data/search2/utils.ts
@@ -18,6 +18,7 @@ export async function search_entity_partial_exact(search: string, limit: number,
     .selectFrom('pdp.entity')
     .where('searchable', '@@', sql<string>`websearch_to_tsquery('english', ${search})`)
     .select('id')
+    .orderBy('id')
     .$if(cursor !== undefined, qb => {
       if (!cursor) return qb
       return qb.where('pagerank', '<', cursor.pagerank).where('slug', '>', cursor.slug)
