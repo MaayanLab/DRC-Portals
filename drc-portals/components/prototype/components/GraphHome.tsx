cdd11f395e5c0c249706266e0ef404198d15a23c
Ethan Sanchez <ethan.dsanch@gmail.com>
2025-07-22 12:20:45 -0700

    Update layout on graph home page
    Also updates the search field to be ignored on `/graph` pages.

--- drc-portals/components/prototype/components/GraphHome.tsx
+++ drc-portals/components/prototype/components/GraphHome.tsx
@@ -29,84 +29,24 @@ interface Tool {
 
 const GRAPH_QUERY_TOOLS: Tool[] = [
   {
-    label: "Search",
+    label: "Step 1: Search",
     image: "/img/graph/GQI_carousel_1.png",
     description:
-      "Enter a keyword (e.g., human, blood, asthma, RNA-seq, TP53, aspirin) to search ontology-encoded metadata, such as species, anatomy, disease/phenotype, gene, compound, and assay, to discover entities like Subject, Biosample, and File.",
+      "Enter a keyword (e.g., human, blood, asthma) to discover entities.",
   },
   {
-    label: "Explore",
+    label: "Step 2: Explore",
     image: "/img/graph/GQI_carousel_2.png",
     description:
-      "Use node menu options (point the cursor/mouse to a node) — Expand (to explore related entities), Filter (to narrow by values), and Prune (to trim paths) — to build your query one step at a time.",
+      "Use node menu options — Expand, Filter, Prune — to build your query one step at a time.",
   },
   {
-    label: "View/Download",
+    label: "Step 3: View/Download",
     image: "/img/graph/GQI_carousel_3.png",
     description:
       "See results in Tabular or Network View, and export them as JSON to share, revisit, or analyze further.",
   },
 ];
-
-const CarouselCard = ({ tool }: { tool: Tool }) => (
-  <Box
-    sx={{
-      minHeight: { xs: 150, sm: 150, md: 300, lg: 300, xl: 300 },
-      width: { xs: 300, sm: 300, md: 640, lg: 640, xl: 640 },
-      textAlign: "center",
-      border: 1,
-      borderRadius: 5,
-      borderColor: "rgba(81, 123, 154, 0.5)",
-      padding: 2,
-    }}
-  >
-    <Box
-      className="flex flex-col"
-      sx={{ minHeight: 300, boxShadow: "none", background: "#FFF" }}
-    >
-      <div className="flex grow items-center justify-center relative">
-        <Image
-          src={tool.image || tool.icon || "/img/favicon.png"}
-          alt={tool.label}
-          fill={true}
-          style={{ objectFit: "contain" }}
-        />
-      </div>
-    </Box>
-  </Box>
-);
-
-const ClientCarousel = ({
-  children,
-  title,
-  height,
-}: {
-  children: React.ReactNode;
-  title?: string;
-  height?: number;
-}) => {
-  return (
-    <Stack spacing={1} alignItems={"center"}>
-      {title && (
-        <Typography variant="subtitle2" color="secondary">
-          {title}
-        </Typography>
-      )}
-      <div style={{ position: "relative" }}>
-        <Carousel
-          height={height || 370}
-          sx={{
-            minHeight: { xs: 350, sm: 400, md: 350, lg: 350, xl: 350 },
-            width: "100vw",
-          }}
-          indicators={true}
-        >
-          {children}
-        </Carousel>
-      </div>
-    </Stack>
-  );
-};
 
 export default function GraphHome() {
   const router = useRouter();
@@ -126,19 +66,11 @@ export default function GraphHome() {
           }}
           className="flex"
         >
-          <BlurBig
-            sx={{ position: "absolute", left: "-20%" }}
-            className="pointer-events-none"
-          />
-          <BlurBig
-            sx={{ position: "absolute", right: "-15%" }}
-            className="pointer-events-none"
-          />
           <Container maxWidth="lg" className="m-auto">
             <Grid container spacing={2} alignItems={"center"}>
               <Grid item xs={12}>
                 <Stack
-                  spacing={4}
+                  spacing={2}
                   justifyContent={"center"}
                   alignItems={"center"}
                 >
@@ -155,66 +87,78 @@ export default function GraphHome() {
                         sx={{ textTransform: "uppercase" }}
                         color="secondary"
                       >
-                        Learn More
+                        Quickstart
+                      </Button>
+                    </Link>
+
+                    <Link href="/info/documentation/gqi">
+                      <Button
+                        sx={{ textTransform: "uppercase" }}
+                        color="secondary"
+                      >
+                        Documentation
                       </Button>
                     </Link>
                   </div>
 
-                  <ClientCarousel title="">
-                    {GRAPH_QUERY_TOOLS.map((tool, i) => (
-                      <Container key={i} maxWidth="lg">
-                        <Grid container spacing={2}>
-                          <Grid
-                            item
-                            xs={12}
-                            sm={7}
-                            sx={{
-                              display: {
-                                xs: "block",
-                                sm: "block",
-                                md: "none",
-                                lg: "none",
-                                xl: "none",
-                              },
-                            }}
-                          >
-                            <CarouselCard tool={tool} />
+                  <Box sx={{width: "100%"}}>
+                    <Carousel
+                      autoPlay={false}
+                      cycleNavigation={false}
+                      indicators={true}
+                      navButtonsAlwaysVisible={true}
+                      sx={{
+                        minHeight: 350,
+                        minWidth: { xs: 350, sm: 600, md: 800, lg: 800, xl: 800 },
+                      }}
+                    >
+                      {GRAPH_QUERY_TOOLS.map((tool, i) => (
+                        <Container key={i} maxWidth="lg">
+                          <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6} md={5}>
+                              <Stack spacing={2}>
+                                <Typography variant="h3" color="secondary.dark">
+                                  {tool.label}
+                                </Typography>
+                                <Typography variant="subtitle1">
+                                  {tool.description}
+                                </Typography>
+                              </Stack>
+                            </Grid>
+                            <Grid item xs={12} sm={6} md={7}>
+                              <Box sx={{display: "flex", justifyContent: "center"}}>
+                              <Box
+                                sx={{
+                                  minHeight: { xs: 150, sm: 150, md: 300, lg: 300, xl: 300 },
+                                  width: { xs: 300, sm: 300, md: 640, lg: 640, xl: 640 },
+                                  textAlign: "center",
+                                  border: 1,
+                                  borderRadius: 5,
+                                  borderColor: "rgba(81, 123, 154, 0.5)",
+                                  padding: 2,
+                                }}
+                              >
+                                <Box
+                                  className="flex flex-col"
+                                  sx={{ minHeight: 300, boxShadow: "none", background: "#FFF" }}
+                                >
+                                  <div className="flex grow items-center justify-center relative">
+                                    <Image
+                                      src={tool.image || tool.icon || "/img/favicon.png"}
+                                      alt={tool.label}
+                                      fill={true}
+                                      style={{ objectFit: "contain" }}
+                                    />
+                                  </div>
+                                </Box>
+                              </Box>
+                              </Box>
+                            </Grid>
                           </Grid>
-                          <Grid item xs={12} sm={5}>
-                            <Stack
-                              direction="column"
-                              alignItems="flex-start"
-                              spacing={2}
-                              sx={{ height: "90%" }}
-                            >
-                              <Typography variant="h3" color="secondary.dark">
-                                {tool.label}
-                              </Typography>
-                              <Typography variant="subtitle1">
-                                {tool.description}
-                              </Typography>
-                            </Stack>
-                          </Grid>
-                          <Grid
-                            item
-                            xs={12}
-                            sm={7}
-                            sx={{
-                              display: {
-                                xs: "none",
-                                sm: "none",
-                                md: "block",
-                                lg: "block",
-                                xl: "block",
-                              },
-                            }}
-                          >
-                            <CarouselCard tool={tool} />
-                          </Grid>
-                        </Grid>
-                      </Container>
-                    ))}
-                  </ClientCarousel>
+                        </Container>
+                      ))}
+                    </Carousel>
+                  </Box>
                 </Stack>
               </Grid>
             </Grid>
