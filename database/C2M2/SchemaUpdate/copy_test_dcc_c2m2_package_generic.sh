e894412fd20e29c668d33f6389873a448b59351e
mano-at-sdsc <mano@sdsc.edu>
2025-08-15 13:02:15 -0700

    Updated test scripts in SchemaUpdate re any change in ontology files, etc

--- database/C2M2/SchemaUpdate/copy_test_dcc_c2m2_package_generic.sh
+++ database/C2M2/SchemaUpdate/copy_test_dcc_c2m2_package_generic.sh
@@ -2,12 +2,13 @@
 #
 # Do not include the ending / in folder paths (relative or absolute)
 # Call syntax (multiline): be in the correct folder: outside DRC-Portals, e.g., 
-# ./copy_update_test_dcc_c2m2_package_for_access_url.sh \
+# ./copy_test_dcc_c2m2_package_generic.sh \
 # ~/DRC/DRC-Portals/database/C2M2/SchemaUpdate/ingest/c2m2s/Metabolomics/MW_submission_packet_20241211/MW_submission_packet_20241211 \
 # Metabolomics \
 # validation-logs/202503/validation_result-Metabolomics.log \
-# append_random_access_url_to_file.sh \
-# ~/DRC/DRC-Portals/database/C2M2/SchemaUpdate [0|1]
+# [append_random_access_url_to_file.sh|donothing.sh] \
+# ~/DRC/DRC-Portals/database/C2M2/SchemaUpdate \
+# [0|1|2]
 
 echo "              --------------------- Script $0: Started   ---------------------"
 
@@ -17,6 +18,7 @@ vlogf="$3" # validation log file, e.g., validation-logs/202503/validation_result
 scriptfile_for_update="$4" # script to run to make changes in the seelect tsv files: e.g., append_random_access_url_to_file.sh
 schemaupdate_dir="$5" # ....../DRC-Portals/database/C2M2/SchemaUpdate
 
+# if onlyTest = 2 then it will copy and test; if 1, it will only test
 if [[ $# -lt 6 ]]; then
 	onlyTest=0
 else
@@ -25,9 +27,17 @@ fi
 
 curdir="$PWD"
 
-schema_json_file=C2M2_datapackage_file_access_url.json
+schema_json_file=C2M2_datapackage.json
+schema_json_file_default=C2M2_datapackage.json
 main_target_file=file.tsv
 
+if [[ "$onlyTest" == "0" ]]; then
+	echo "onlyTest = ${onlyTest}, copy, update and test will be performed";
+elif [[ "$onlyTest" == "1" ]]; then
+	echo "onlyTest = ${onlyTest}, only test will be performed";
+elif [[ "$onlyTest" == "2" ]]; then
+	echo "onlyTest = ${onlyTest}, copy and test (no update) will be performed";
+fi
 #------------------------------------------------------------------------------------
 # Test submission prep py script
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -37,7 +47,7 @@ main_target_file=file.tsv
 # for the case of file.tsv access_url testing.
 #<<<<<<<<<<<<<<<<<<<
 # Assuming this script will be operated from the folder ~/CFDE/C2M2_sub, create folder KidsFirst (this is outside DRC-Portals)
-if [[ "$onlyTest" == "0" ]]; then
+if [[ "$onlyTest" == "0" || "$onlyTest" == "2" ]]; then
 	mkdir -p "${tdir}"
 	echo "Created directory if did not exist: ${tdir}";
 	#cp -R ${sdir}/* ${tdir}/.
@@ -45,17 +55,17 @@ if [[ "$onlyTest" == "0" ]]; then
 	echo "Copied files from directory ${sdir} to ${tdir}";
 	cd "${tdir}" &&	dos2unix *.tsv
 	echo "Applied command dos2unix to tsv files inside ${tdir}";
-	#cp "${schemaupdate_dir}/empty_tsvs"/*biofluid* . # Not for access_url test
-	cp "${schemaupdate_dir}"/${schema_json_file} ./C2M2_datapackage.json
-	#cp "${schemaupdate_dir}"/C2M2_datapackage.json ./.
-	#echo "Copied biofluid related empty files and updated c2m2 json schema file to ${tdir}";
+	cp "${schemaupdate_dir}"/${schema_json_file} ./"${schema_json_file_default}"
+	echo "Copied the updated c2m2 json schema file to ${tdir}";
 	cd "${curdir}"
-	cp "${schemaupdate_dir}"/"${scriptfile_for_update}" .
-	./${scriptfile_for_update} "${tdir}"
-	echo "Copied ${scriptfile_for_update} to current folder and ran the script";
-	# inspect file.tsv to check if access_url column with some random entries got added
-	echo "------ Inspect the top few lines of the file ${main_target_file} below if the desired update seems successful ------";
-	head "${tdir}"/${main_target_file}
+	if [[ "$onlyTest" == "0" ]]; then
+		cp "${schemaupdate_dir}"/"${scriptfile_for_update}" .
+		./${scriptfile_for_update} "${tdir}"
+		echo "Copied ${scriptfile_for_update} to current folder and ran the script";
+		# inspect file.tsv to check if access_url column with some random entries got added
+		echo "------ Inspect the top few lines of the file ${main_target_file} below if the desired update seems successful ------";
+		head "${tdir}"/${main_target_file}
+	fi
 else
 	echo "onlyTest = ${onlyTest}, so, it assumes that the files are already present at ${tdir}";
 fi
@@ -103,7 +113,7 @@ cd "${curdir}"
 vlogdir="$(dirname ${vlogf})"
 mkdir -p "${vlogdir}"
 # call frictionless, it must exist on path
-frictionless validate "${tdir}"/C2M2_datapackage.json > "${vlogf}"
+frictionless validate "${tdir}/${schema_json_file_default}" > "${vlogf}"
 echo "---------- Check the content of ${vlogf} below, all files should be VALID, else find out the issue, fix it and run this script again ----------";
 cat "${vlogf}"
 # copy back *biofluid* files and biosample.tsv to sdir
@@ -112,10 +122,12 @@ if [ -f "${vlogf}" ] ; then
 	if [[ "$invalid_linecount" == "0" ]] ; then
 		echo "GREAT NEWS: All files in the C2M2 package are VALID";
 		# Do not copy back yet
-		# cp "${tdir}"/biosample.tsv "${sdir}/."
-		# cp "${tdir}"/*biofluid* "${sdir}/."
+		## cp "${tdir}"/biosample.tsv "${sdir}/."
+		## cp "${tdir}"/*biofluid* "${sdir}/."
 	else
 		echo "BAD NEWS: $invalid_linecount lines have the keyword INVALID";
+		echo "Source: $sdir";
+		echo "Target: $tdir";
 	fi
 else
     echo "Error: Log file ${vlogf} does not exist";
