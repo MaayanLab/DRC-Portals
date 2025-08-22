f99ccdd30c519e27e1d5ee65ec57384ffe97fecc
John Erol Evangelista <je.evangelista04@gmail.com>
2025-08-22 12:30:36 -0400

    text edit

--- drc-portals/app/data/cross/page.tsx
+++ drc-portals/app/data/cross/page.tsx
@@ -12,7 +12,7 @@ export default async function GMTCross() {
 						<Typography variant="h3" color="secondary.dark" sx={{ mb: 2, mt: 2 }}>COMMON FUND GENE SET CROSSING</Typography>
 					</div>
 					<Typography variant="subtitle1" color="#666666" sx={{ mb: 3 }}>
-						Try crossing gene sets created from datasets produced by Common Fund programs to discover unexpected statistically significant overlap between sets across programs. The crossed gene set pairs are ranked by the Fisher’s exact test p-value of the overlap. You can explore and export the overlapping genes, and receive an explanation about the unexpected overlap.
+						Try crossing gene sets created from datasets produced by Common Fund programs to discover unexpected statistically significant overlaps between sets across programs. The crossed gene set pairs are ranked by the Fisher’s exact test p-value of the overlap. You can explore and export the overlapping genes, and receive an explanation about the unexpected overlap.
 					</Typography>
 					<GMTCrossLayout sessionInfo={null} />
 				</Container>
