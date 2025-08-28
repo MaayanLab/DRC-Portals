613b861b181e960d7f0d44eb0f2da14683bc0de7
mano-at-sdsc <mano@sdsc.edu>
2025-08-28 15:49:07 -0700

    Better formatting of nested loops

--- database/C2M2/sanitize_C2M2_tables_for_keywords.sql
+++ database/C2M2/sanitize_C2M2_tables_for_keywords.sql
@@ -135,114 +135,114 @@ END;
 $$;
 
 -------------------------
-        DO $$
-        DECLARE
-            test_only BOOLEAN := FALSE; --- OR TRUE; --- or FALSE
-            schema_name text;
-            table_name text;
-            keyword_from_array text;
-            /* Select one of the schema lines from below, keep others commented */
-            --- schemas text[] := ARRAY['_4dn', 'exrna', 'gtex', 'glygen', 'hmp', 'hubmap', 'idg', 'kidsfirst', 'lincs', 'metabolomics', 'motrpac', 'sparc', 'sennet'];  -- your schema names
-            /* To manually/artificially parallelize, you can break this list into two, select one of them in one call.
-                Then, select the other line here, and call in another terminal, change the name of the log file when calling.
-            */
-            --- schemas text[] := ARRAY['_4dn', 'exrna', 'gtex', 'glygen', 'hmp', 'hubmap'];  -- your schema names
-            --- schemas text[] := ARRAY['idg', 'kidsfirst', 'lincs', 'metabolomics', 'motrpac', 'sparc', 'sennet'];  -- your schema names
-            --- schemas text[] := ARRAY['motrpac'];  -- your schema names
-            schemas text[] := ARRAY['c2m2'];  --- BE CAREFUL WITH THIS ONE AS THIS IS THE MAIN SCHEMA -- your schema names
+DO $$
+DECLARE
+    test_only BOOLEAN := FALSE; --- OR TRUE; --- or FALSE
+    schema_name text;
+    table_name text;
+    keyword_from_array text;
+    /* Select one of the schema lines from below, keep others commented */
+    --- schemas text[] := ARRAY['_4dn', 'exrna', 'gtex', 'glygen', 'hmp', 'hubmap', 'idg', 'kidsfirst', 'lincs', 'metabolomics', 'motrpac', 'sparc', 'sennet'];  -- your schema names
+    /* To manually/artificially parallelize, you can break this list into two, select one of them in one call.
+        Then, select the other line here, and call in another terminal, change the name of the log file when calling.
+    */
+    --- schemas text[] := ARRAY['_4dn', 'exrna', 'gtex', 'glygen', 'hmp', 'hubmap'];  -- your schema names
+    --- schemas text[] := ARRAY['idg', 'kidsfirst', 'lincs', 'metabolomics', 'motrpac', 'sparc', 'sennet'];  -- your schema names
+    --- schemas text[] := ARRAY['motrpac'];  -- your schema names
+    schemas text[] := ARRAY['c2m2'];  --- BE CAREFUL WITH THIS ONE AS THIS IS THE MAIN SCHEMA -- your schema names
 
-            --- To get the names all schemas
-            /* 
-            SELECT '{' || string_agg(quote_literal(schema_name), ', ') || '}' AS schema_array
-            FROM information_schema.schemata
-            WHERE catalog_name = 'drc' AND (schema_name NOT IN ('pg_catalog', 'information_schema', 'public', 'c2m2', 'ercc', 'slim') AND schema_name NOT ILIKE 'pg_%');    
-            */
+    --- To get the names all schemas
+    /*
+    SELECT '{' || string_agg(quote_literal(schema_name), ', ') || '}' AS schema_array
+    FROM information_schema.schemata
+    WHERE catalog_name = 'drc' AND (schema_name NOT IN ('pg_catalog', 'information_schema', 'public', 'c2m2', 'ercc', 'slim') AND schema_name NOT ILIKE 'pg_%');
+    */
 
-            /* 
-            Note that the searchable column does not include information from the columns
-            id_namespace$|creation_time|size_in_bytes
-            */
+    /*
+    Note that the searchable column does not include information from the columns
+    id_namespace$|creation_time|size_in_bytes
+    */
 
-            tables text[] := ARRAY['analysis_type', 'anatomy', 'assay_type', 'biofluid', 'biosample',
-                                    'collection', 'compound', 'data_type', 'disease', 'file', 'file_format',
-                                    'gene', 'id_namespace', 'ncbi_taxonomy', 'phenotype', 'project', 'protein',
-                                    'sample_prep_method', 'subject', 'substance'];
-            keywords text := 'gender OR inclusion OR diversity OR equity OR lgbt OR trans-gen OR transgen'; ---  OR women 
-            --- If use trans for exclusion, entire MoTrPAC data is gone as project name is: Molecular Transducers of Physical Acitivity Consortium
-            keywords_array text[] := ARRAY['gender', 'inclusion', 'diversity', 'equity', 'lgbt', 'trans-gen', 'transgen']; ---  OR women 
-            keywords_array_wildcard text[];
+    tables text[] := ARRAY['analysis_type', 'anatomy', 'assay_type', 'biofluid', 'biosample',
+                            'collection', 'compound', 'data_type', 'disease', 'file', 'file_format',
+                            'gene', 'id_namespace', 'ncbi_taxonomy', 'phenotype', 'project', 'protein',
+                            'sample_prep_method', 'subject', 'substance'];
+    keywords text := 'gender OR inclusion OR diversity OR equity OR lgbt OR trans-gen OR transgen'; ---  OR women
+    --- If use trans for exclusion, entire MoTrPAC data is gone as project name is: Molecular Transducers of Physical Acitivity Consortium
+    keywords_array text[] := ARRAY['gender', 'inclusion', 'diversity', 'equity', 'lgbt', 'trans-gen', 'transgen']; ---  OR women
+    keywords_array_wildcard text[];
 
-            tbl RECORD;
-            row_count BIGINT;
+    tbl RECORD;
+    row_count BIGINT;
 
-        BEGIN
-            keywords_array_wildcard := ARRAY(SELECT '%' || k || '%' FROM unnest(keywords_array) AS k);
+BEGIN
+    keywords_array_wildcard := ARRAY(SELECT '%' || k || '%' FROM unnest(keywords_array) AS k);
 
-            FOREACH schema_name IN ARRAY schemas
+    FOREACH schema_name IN ARRAY schemas
+    LOOP
+        RAISE NOTICE 'Processing schema: %', schema_name;
+        -------------------------------------------------------
+        ----------------------
+        RAISE NOTICE '---- Before deletion ----';
+        FOR tbl IN (SELECT tablename FROM pg_tables WHERE schemaname = schema_name)
+        LOOP
+            EXECUTE format('SELECT COUNT(*) FROM %I.%I', schema_name, tbl.tablename) INTO row_count;
+            RAISE NOTICE 'Table %.%, Row count: %', schema_name, tbl.tablename, row_count;
+        END LOOP;
+        ----------------------
+
+        FOREACH table_name IN ARRAY tables
+        LOOP
+            RAISE NOTICE '    Processing table: %.%', schema_name, table_name;
+            --- RAISE NOTICE '      Before deletion';
+            --- EXECUTE format('SELECT count(*) FROM %I.%I;', schema_name, table_name);
+            --- BEGIN --- FOR Transaction control
+                --- SAVEPOINT loop_point;
+
+                --- EXECUTE format('DELETE FROM %I.%I WHERE searchable @@ websearch_to_tsquery(''english'', %L) RETURNING *',
+                ---    schema_name, table_name, keywords);
+            FOREACH keyword_from_array IN ARRAY keywords_array
             LOOP
-                RAISE NOTICE 'Processing schema: %', schema_name;
-                -------------------------------------------------------
-                ----------------------
-                RAISE NOTICE '---- Before deletion ----';
-                FOR tbl IN (SELECT tablename FROM pg_tables WHERE schemaname = schema_name)
-                LOOP
-                    EXECUTE format('SELECT COUNT(*) FROM %I.%I', schema_name, tbl.tablename) INTO row_count;
-                    RAISE NOTICE 'Table %.%, Row count: %', schema_name, tbl.tablename, row_count;
-                END LOOP;
-                ----------------------
-
-                FOREACH table_name IN ARRAY tables
-                LOOP
-                    RAISE NOTICE '    Processing table: %.%', schema_name, table_name;
-                    --- RAISE NOTICE '      Before deletion';
-                    --- EXECUTE format('SELECT count(*) FROM %I.%I;', schema_name, table_name);
-                    --- BEGIN --- FOR Transaction control
-                        --- SAVEPOINT loop_point;
-                    
-                        --- EXECUTE format('DELETE FROM %I.%I WHERE searchable @@ websearch_to_tsquery(''english'', %L) RETURNING *',
-                        ---    schema_name, table_name, keywords);
-                    FOREACH keyword_from_array IN ARRAY keywords_array
-                    LOOP
-                        IF NOT test_only THEN
-                            --- EXECUTE format('DELETE FROM %I.%I WHERE searchable @@ websearch_to_tsquery(''english'', %L) RETURNING *',
-                            ---    schema_name, table_name, keyword_from_array);
-                            --- EXECUTE format('DELETE FROM %I.%I WHERE searchable ilike %L',
-                            ---    schema_name, table_name, '%' || keyword_from_array || '%' );
-                            --- Call stored procedure
-                            CALL delete_matching_rows(schema_name, table_name, keyword_from_array);
-                            --- RAISE NOTICE '      Only testing';
-                        ELSE
-                            RAISE NOTICE '      Only testing';
-                        END IF;
-                    END LOOP;
-
-                        --- RELEASE SAVEPOINT loop_point;
-                        --- ROLLBACK TO SAVEPOINT loop_point;
-                    --- ROLLBACK; --- FOR Transaction control
-                    --- COMMIT; --- FOR Transaction control
-                    --- END; --- FOR Transaction control
-                END LOOP;
-
-                ----------------------
-                RAISE NOTICE '---- After deletion ----';
-                FOR tbl IN (SELECT tablename FROM pg_tables WHERE schemaname = schema_name)
-                LOOP
-                    EXECUTE format('SELECT COUNT(*) FROM %I.%I', schema_name, tbl.tablename) INTO row_count;
-                    RAISE NOTICE 'Table %.%, Row count: %', schema_name, tbl.tablename, row_count;
-                END LOOP;
-                FOR tbl IN (SELECT tablename FROM pg_tables WHERE schemaname = schema_name)
-                LOOP
-                    --- EXECUTE format('SELECT COUNT(*) FROM %I.%I WHERE searchable @@ websearch_to_tsquery(''english'', %L)',
-                    ---    schema_name, table_name, keywords) INTO row_count;
-                    EXECUTE format('SELECT COUNT(*) FROM %I.%I WHERE searchable ILIKE ANY ($1)',
-                        schema_name, table_name) INTO row_count USING keywords_array_wildcard;                        
-                    RAISE NOTICE '                Table %.%, Row count with keywords: %', schema_name, tbl.tablename, row_count;
-                END LOOP;
-                ----------------------
-                -------------------------------------------------------
+                IF NOT test_only THEN
+                    --- EXECUTE format('DELETE FROM %I.%I WHERE searchable @@ websearch_to_tsquery(''english'', %L) RETURNING *',
+                    ---    schema_name, table_name, keyword_from_array);
+                    --- EXECUTE format('DELETE FROM %I.%I WHERE searchable ilike %L',
+                    ---    schema_name, table_name, '%' || keyword_from_array || '%' );
+                    --- Call stored procedure
+                    CALL delete_matching_rows(schema_name, table_name, keyword_from_array);
+                    --- RAISE NOTICE '      Only testing';
+                ELSE
+                    RAISE NOTICE '      Only testing';
+                END IF;
             END LOOP;
-            --- RAISE NOTICE 'Done Processing tables in schemas';
-        END $$;
+
+                --- RELEASE SAVEPOINT loop_point;
+                --- ROLLBACK TO SAVEPOINT loop_point;
+            --- ROLLBACK; --- FOR Transaction control
+            --- COMMIT; --- FOR Transaction control
+            --- END; --- FOR Transaction control
+        END LOOP;
+
+        ----------------------
+        RAISE NOTICE '---- After deletion ----';
+        FOR tbl IN (SELECT tablename FROM pg_tables WHERE schemaname = schema_name)
+        LOOP
+            EXECUTE format('SELECT COUNT(*) FROM %I.%I', schema_name, tbl.tablename) INTO row_count;
+            RAISE NOTICE 'Table %.%, Row count: %', schema_name, tbl.tablename, row_count;
+        END LOOP;
+        FOR tbl IN (SELECT tablename FROM pg_tables WHERE schemaname = schema_name)
+        LOOP
+            --- EXECUTE format('SELECT COUNT(*) FROM %I.%I WHERE searchable @@ websearch_to_tsquery(''english'', %L)',
+            ---    schema_name, table_name, keywords) INTO row_count;
+            EXECUTE format('SELECT COUNT(*) FROM %I.%I WHERE searchable ILIKE ANY ($1)',
+                schema_name, table_name) INTO row_count USING keywords_array_wildcard;
+            RAISE NOTICE '                Table %.%, Row count with keywords: %', schema_name, tbl.tablename, row_count;
+        END LOOP;
+        ----------------------
+        -------------------------------------------------------
+    END LOOP;
+    --- RAISE NOTICE 'Done Processing tables in schemas';
+END $$;
 -------------------------
 
 --- RAISE EXCEPTION 'Currently in testing phase, exiting now!';
@@ -263,45 +263,45 @@ Some very specific deletions
 
 ------------------------- To print records with keyworkds in public schema, node and entity_node tables
 --- Do not worry about Inclusion conjunctivitis
-        DO $$
-        DECLARE
-            schema_name text;
-            table_name text;
-            keyword_from_array text;
-            schemas text[] := ARRAY['public'];  --- BE CAREFUL WITH THIS ONE AS THIS IS THE MAIN SCHEMA -- your schema names
+DO $$
+DECLARE
+    schema_name text;
+    table_name text;
+    keyword_from_array text;
+    schemas text[] := ARRAY['public'];  --- BE CAREFUL WITH THIS ONE AS THIS IS THE MAIN SCHEMA -- your schema names
 
-            tables text[] := ARRAY['node'];
-            keywords_array text[] := ARRAY['gender', 'inclusion', 'diversity', 'equity', 'lgbt', 'trans-gen', 'transgen']; ---  OR women 
+    tables text[] := ARRAY['node'];
+    keywords_array text[] := ARRAY['gender', 'inclusion', 'diversity', 'equity', 'lgbt', 'trans-gen', 'transgen']; ---  OR women
 
-            sql_query TEXT;
-            record_result RECORD;
-        BEGIN
-            FOREACH schema_name IN ARRAY schemas
+    sql_query TEXT;
+    record_result RECORD;
+BEGIN
+    FOREACH schema_name IN ARRAY schemas
+    LOOP
+        RAISE NOTICE 'Processing schema: %', schema_name;
+        -------------------------------------------------------
+
+        FOREACH table_name IN ARRAY tables
+        LOOP
+            RAISE NOTICE '    Processing table: %.%', schema_name, table_name;
+
+            FOREACH keyword_from_array IN ARRAY keywords_array
             LOOP
-                RAISE NOTICE 'Processing schema: %', schema_name;
-                -------------------------------------------------------
-
-                FOREACH table_name IN ARRAY tables
+                RAISE NOTICE 'Keyword: %', keyword_from_array;
+                sql_query := format('SELECT * FROM %I.%I WHERE searchable @@ websearch_to_tsquery(''english'', %L);',
+                                schema_name, table_name, keyword_from_array);
+                RAISE NOTICE 'Executing: %', sql_query;
+                FOR record_result IN EXECUTE sql_query
                 LOOP
-                    RAISE NOTICE '    Processing table: %.%', schema_name, table_name;
-
-                    FOREACH keyword_from_array IN ARRAY keywords_array
-                    LOOP
-                        RAISE NOTICE 'Keyword: %', keyword_from_array;
-                        sql_query := format('SELECT * FROM %I.%I WHERE searchable @@ websearch_to_tsquery(''english'', %L);',
-                                        schema_name, table_name, keyword_from_array);
-                        RAISE NOTICE 'Executing: %', sql_query;
-                        FOR record_result IN EXECUTE sql_query
-                        LOOP
-                            -- Print each row to the psql console.
-                            RAISE NOTICE 'Result: %', record_result;
-                        END LOOP;
-                    END LOOP;
+                    -- Print each row to the psql console.
+                    RAISE NOTICE 'Result: %', record_result;
                 END LOOP;
-                -------------------------------------------------------
             END LOOP;
-            --- RAISE NOTICE 'Done Processing tables in schemas';
-        END $$;
+        END LOOP;
+        -------------------------------------------------------
+    END LOOP;
+    --- RAISE NOTICE 'Done Processing tables in schemas';
+END $$;
 -------------------------
 
 /*
