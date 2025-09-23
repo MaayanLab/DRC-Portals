6e81533c773f9f9b9669a83e8fb2a7fd8819eabc
Daniel Clarke <u8sand@gmail.com>
2025-09-23 17:43:56 -0400

    Add CFDE Gene Set Enrichment PWB pre-built workflow

--- drc-portals/app/data/processed/gene_set/[slug]/modules.tsx
+++ drc-portals/app/data/processed/gene_set/[slug]/modules.tsx
@@ -23,13 +23,45 @@ const modules: {
     compatible: (item) => true,
     button: async ({ item }) => <PWBButton
       body={{
-        data: { gene_set: { type: "Input[Set[Gene]]", value: { set: await getGeneSet(item.id), description: item.label } } },
-        workflow: [
-          { id: "input_gene_set", type: "Input[Set[Gene]]", data: { id: "gene_set" } },
+        "data":{
+          "gene-set": {
+            "type":"Input[Set[Gene]]",
+            "value":{
+              "description": item.description,
+              "set": await getGeneSet(item.id),
+            }
+          }
+        },
+        "workflow":[
+          {"id":"input-gene-set","type":"Input[Set[Gene]]","data":{"id":"gene-set"}},
+          {"id":"enrichment-results","type":"CFDEGSEGenesetSearch","inputs":{"geneset":{"id":"input-gene-set"}}},
+            {"id":"gtex.0","type":"ExtractCFDEGSEGenesetSearch[GTEx_Tissues_V8_2023]","inputs":{"searchResults":{"id":"enrichment-results"}}},
+              {"id":"gtex.1","type":"EnrichrScoredTToScoredT[Tissue]","inputs":{"enrichrscored":{"id":"gtex.0"}}},
+              {"id":"gtex.2","type":"BarChartFrom[Scored[Tissue]]","inputs":{"terms":{"id":"gtex.1"}}},
+            {"id":"lincs.0","type":"ExtractCFDEGSEGenesetSearch[LINCS_L1000_Chem_Pert_Consensus_Sigs]","inputs":{"searchResults":{"id":"enrichment-results"}}},
+              {"id":"lincs.1","type":"EnrichrScoredTToScoredT[Drug]","inputs":{"enrichrscored":{"id":"lincs.0"}}},
+              {"id":"lincs.2","type":"BarChartFrom[Scored[Drug]]","inputs":{"terms":{"id":"lincs.1"}}},
+            {"id":"idg.0","type":"ExtractCFDEGSEGenesetSearch[IDG_Drug_Targets_2022]","inputs":{"searchResults":{"id":"enrichment-results"}}},
+              {"id":"idg.1","type":"EnrichrScoredTToScoredT[Drug]","inputs":{"enrichrscored":{"id":"idg.0"}}},
+              {"id":"idg.2","type":"BarChartFrom[Scored[Drug]]","inputs":{"terms":{"id":"idg.1"}}},
+            {"id":"hubmap.0","type":"ExtractCFDEGSEGenesetSearch[HuBMAP_ASCTplusB_augmented_2022]","inputs":{"searchResults":{"id":"enrichment-results"}}},
+              {"id":"hubmap.1","type":"EnrichrScoredTToScoredT[Tissue]","inputs":{"enrichrscored":{"id":"hubmap.0"}}},
+              {"id":"hubmap.2","type":"BarChartFrom[Scored[Tissue]]","inputs":{"terms":{"id":"hubmap.1"}}},
+            {"id":"glygen.0","type":"ExtractCFDEGSEGenesetSearch[GlyGen_Glycosylated_Proteins_2022]","inputs":{"searchResults":{"id":"enrichment-results"}}},
+              {"id":"glygen.1","type":"EnrichrScoredTToScoredT[glycan]","inputs":{"enrichrscored":{"id":"glygen.0"}}},
+              {"id":"glygen.2","type":"BarChartFrom[Scored[glycan]]","inputs":{"terms":{"id":"glygen.1"}}},
+            {"id":"metabolomics.0","type":"ExtractCFDEGSEGenesetSearch[Metabolomics_Workbench_Metabolites_2022]","inputs":{"searchResults":{"id":"enrichment-results"}}},
+              {"id":"metabolomics.1","type":"EnrichrScoredTToScoredT[Metabolite]","inputs":{"enrichrscored":{"id":"metabolomics.0"}}},
+              {"id":"metabolomics.2","type":"BarChartFrom[Scored[Metabolite]]","inputs":{"terms":{"id":"metabolomics.1"}}},
+            {"id":"motrpac.0","type":"ExtractCFDEGSEGenesetSearch[MoTrPAC_2023]","inputs":{"searchResults":{"id":"enrichment-results"}}},
+              {"id":"motrpac.1","type":"EnrichrScoredTToScoredT[Tissue]","inputs":{"enrichrscored":{"id":"motrpac.0"}}},
+              {"id":"motrpac.2","type":"BarChartFrom[Scored[Tissue]]","inputs":{"terms":{"id":"motrpac.1"}}},
         ],
+        "metadata":{"title":"CFDE Gene Set Enrichment"}
       }}
-      title="Playbook Workflow Builder"
-      description={<>The Playbook Workflow Builder helps you interactively construct workflows leveraging CFDE APIs without code. Start a new workflow with {item.label}.</>}
+      title="Playbook Workflow Builder: Gene Set Enrichment"
+      description={<>Perform Gene Set Enrichment with this gene set against Common Fund program's gene sets, identifying significant overlaps using a pre-built PWB workflow. Run the workflow with {item.label}.</>}
+      mode="report"
     />,
   },
   {
@@ -54,6 +86,19 @@ const modules: {
       }}
       title="CFDE Gene Set Enrichment (GSE)"
       description={<>CFDE-GSE illuminates connections between the input gene set and various CF gene sets that overlap with the queried gene set. Query CFDE-GSE with {item.label}.</>}
+    />,
+  },
+  {
+    compatible: (item) => true,
+    button: async ({ item }) => <PWBButton
+      body={{
+        data: { gene_set: { type: "Input[Set[Gene]]", value: { set: await getGeneSet(item.id), description: item.label } } },
+        workflow: [
+          { id: "input_gene_set", type: "Input[Set[Gene]]", data: { id: "gene_set" } },
+        ],
+      }}
+      title="Playbook Workflow Builder"
+      description={<>The Playbook Workflow Builder helps you interactively construct workflows leveraging CFDE APIs without code. Start a new workflow with {item.label}.</>}
     />,
   },
 ]
