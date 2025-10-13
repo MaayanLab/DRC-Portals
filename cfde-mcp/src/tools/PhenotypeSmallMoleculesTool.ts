d34257628b9eff5557cf0655cf6b6f3a27ef1c4f
John Erol Evangelista <je.evangelista04@gmail.com>
2025-10-13 16:07:57 -0400

    updated mcp (phenotype description and docs)

--- cfde-mcp/src/tools/PhenotypeSmallMoleculesTool.ts
+++ cfde-mcp/src/tools/PhenotypeSmallMoleculesTool.ts
@@ -23,7 +23,7 @@ const PhenotypeSmallMolecules = [
         text: JSON.stringify({
             function: "PhenotypeSmallMolecules",
             inputType: "PhenotypeInput",
-            output_text: "I can run a function that returns find small molecules that are most likely to induce a given phenotype or biological process",
+            output_text: `I can run a function that extracts MGI Mammalian Phenotype, KEGG, and GO Biological Process gene sets from Enrichr with labels containing the word ${phenotype}. These matching gene sets will be combined using union set opperation before being sent to SigCom LINCS to find reverser and mimicker drugs. The mean across multiple scored drugs is then computed and filtered for FDA Approved Drugs.`,
             phenotype
         })
       }
@@ -31,7 +31,7 @@ const PhenotypeSmallMolecules = [
     structuredContent: {
         function: "PhenotypeSmallMolecules",
         inputType: "PhenotypeInput",
-        output_text: "I can run a function that returns find small molecules that are most likely to induce a given phenotype or biological process",
+        output_text: `I can run a function that extracts MGI Mammalian Phenotype, KEGG, and GO Biological Process gene sets from Enrichr with labels containing the word ${phenotype}. These matching gene sets will be combined using union set opperation before being sent to SigCom LINCS to find reverser and mimicker drugs. The mean across multiple scored drugs is then computed and filtered for FDA Approved Drugs.`,
         phenotype
     }
   }
