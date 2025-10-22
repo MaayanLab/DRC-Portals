dbfc8aa4c412f2be90f78094420f916e9aebc488
Daniel Clarke <u8sand@gmail.com>
2025-10-22 14:28:00 -0400

    "Back to search" on summary page

--- drc-portals/app/data/search2/summary/page.tsx
+++ drc-portals/app/data/search2/summary/page.tsx
@@ -1,6 +1,35 @@
 import React from 'react'
 import SummaryComponent from './SummaryComponent'
+import ListingPageLayout from "@/app/data/processed/ListingPageLayout";
+import Link from '@/utils/link';
+import { Button, Grid, Typography } from '@mui/material';
+import Icon from "@mdi/react";
+import { mdiArrowLeft } from '@mdi/js';
 
 export default async function Page() {
-  return <SummaryComponent />
+  return <ListingPageLayout
+    footer={
+      <Link href="/data">
+        <Button
+          sx={{textTransform: "uppercase"}}
+          color="primary"
+          variant="contained"
+          startIcon={<Icon path={mdiArrowLeft} size={1} />}>
+            BACK TO SEARCH
+        </Button>
+      </Link>
+    }
+  >
+    <Grid container rowGap={2}>
+      <Grid item xs={8}>
+        <Typography variant="h1" color="secondary" sx={{ wordWrap: 'break-word', whiteSpace: 'pre-wrap' }}>
+          Summary
+        </Typography>
+        <br />
+      </Grid>
+      <Grid item xs={12}>
+        <SummaryComponent />
+      </Grid>
+    </Grid>
+  </ListingPageLayout>
 }
