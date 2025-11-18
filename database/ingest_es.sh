4da300cf216a2f85c8358a201426b3a40914c099
Daniel Clarke <u8sand@gmail.com>
2025-11-18 18:00:25 -0500

    More refactoring of the ingest pipeline

--- database/ingest_es.sh
+++ database/ingest_es.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 ELASTICSEARCH_URL=$(dotenv -f ../drc-portals/.env get ELASTICSEARCH_URL)
-INDEX_VERSION=v8
-
-es_put() {
-  method=$1
-  path=$2
-  curl -H'Content-Type: application/json' -X${method} ${ELASTICSEARCH_URL}${path} -d @-
-}
+INDEX_VERSION=v10
 
 es() {
-  method=$1
-  path=$2
-  curl -H'Content-Type: application/json' -X${method} ${ELASTICSEARCH_URL}${path}
+  method=$1; shift
+  path=$1; shift
+  curl -H'Content-Type: application/json' -X${method} ${ELASTICSEARCH_URL}${path} $@
+}
+
+es_put() {
+  es $@ -d @-
 }
 
 # create index for entity
@@ -118,10 +116,10 @@ es_put POST /_aliases << EOF
 EOF
 
 # sanity checks
-es_put POST /entity_${INDEX_VERSION}/_search << EOF
+(es_put POST /entity_${INDEX_VERSION}/_search|jq '.') << EOF
 {"query":{"match_all":{}}, "size":10,"sort":[{"pagerank":{"order": "desc"}}]}
 EOF
-es_put POST /m2m_${INDEX_VERSION}_expanded/_search << EOF
+(es_put POST /m2m_${INDEX_VERSION}/_search|jq '.') << EOF
 {"query":{"match_all":{}}, "size":10}
 EOF
 
@@ -135,199 +133,14 @@ es_put PUT /_enrich/policy/entity_${INDEX_VERSION}_lookup << EOF
   }
 }
 EOF
-es_put POST /_enrich/policy/entity_${INDEX_VERSION}_lookup/_execute
+es POST /_enrich/policy/entity_${INDEX_VERSION}_lookup/_execute
 
-# Here we go from
-# {
-#   id: "ourid",
-#   a_label: "ourlabel",
-#   r_predicate1: "someid",
-#   r_predicate2: "someotherid"
-# }
-# to
-# {
-#   id: "ourid",
-#   a_label: "ourlabel",
-#   r_predicate1_id: "someid",
-#   r_predicate1_a_label: "somelabel",
-#   r_predicate1_r_predicate3: "onehoprelationship",
-#   r_predicate2_id: "someotherid",
-#   r_predicate2_a_label: "someotherlabel",
-#   r_predicate1_r_predicate3: "onehoprelationship",
-# }
-es GET "/entity_${INDEX_VERSION}/_field_caps?fields=r_*" | jq -rc '{
-  "processors": [
-    .fields|keys|.[]|{"enrich":{
-      "policy_name": "entity_" + $INDEX_VERSION + "_lookup",
-      "field": .,
-      "target_field": .,
-      "max_matches": 1,
-      "if": "ctx." + . + " != null"
-    }}
-  ] + [
-    {
-      "script": {
-        "lang": "painless",
-        "source": $source
-      }
-    }
-  ]
-}' --arg INDEX_VERSION $INDEX_VERSION --arg source "
-  List ctxEntrySet = new ArrayList(ctx.entrySet());
-  for (entry in ctxEntrySet) {
-    if (entry.getKey().startsWith('r_')) {
-      for (relEntry in entry.getValue().entrySet()) {
-        ctx[entry.getKey() + '_' + relEntry.getKey()] = relEntry.getValue();
-      }
-      ctx.remove(entry.getKey());
-    }
-  }
-" | es_put PUT /_ingest/pipeline/entity_${INDEX_VERSION}_expanded
-
-# create index for entity_expanded
-es_put PUT /entity_${INDEX_VERSION}_expanded << EOF
-{
-  "settings": {
-    "analysis": {
-      "analyzer": {
-        "custom_analyzer": {
-          "tokenizer": "custom_tokenizer",
-          "filter": [
-            "lowercase",
-            "asciifolding"
-          ]
-        }
-      },
-      "tokenizer": {
-        "custom_tokenizer": {
-          "type": "simple_pattern_split",
-          "pattern": "[^a-zA-Z0-9']",
-          "lowercase": true
-        }
-      }
-    }
-  },
-  "mappings": {
-    "properties": {
-      "id": {"type": "keyword"},
-      "type": {"type": "keyword"},
-      "slug": {"type": "keyword"},
-      "pagerank": {"type": "long"}
-    },
-    "dynamic_templates": [
-      {
-        "attributes": {
-          "match_mapping_type": "string",
-          "match": "a_*",
-          "mapping": {
-            "type": "text",
-            "analyzer": "custom_analyzer",
-            "fields": {
-              "keyword": {
-                "type": "keyword",
-                "ignore_above": 256
-              }
-            }
-          }
-        }
-      },
-      {
-        "relationship_id": {
-          "match_mapping_type": "string",
-          "match": "r_*_id",
-          "mapping": {
-            "type": "keyword"
-          }
-        }
-      },
-      {
-        "relationship_type": {
-          "match_mapping_type": "string",
-          "match": "r_*_type",
-          "mapping": {
-            "type": "keyword"
-          }
-        }
-      },
-      {
-        "relationship_slug": {
-          "match_mapping_type": "string",
-          "match": "r_*_slug",
-          "mapping": {
-            "type": "keyword"
-          }
-        }
-      },
-      {
-        "relationship_pagerank": {
-          "match_mapping_type": "string",
-          "match": "r_*_pagerank",
-          "mapping": {
-            "type": "long"
-          }
-        }
-      },
-      {
-        "relationship_attributes": {
-          "match_mapping_type": "string",
-          "match": "r_*_a_*",
-          "mapping": {
-            "type": "text",
-            "analyzer": "custom_analyzer",
-            "fields": {
-              "keyword": {
-                "type": "keyword",
-                "ignore_above": 256
-              }
-            }
-          }
-        }
-      },
-      {
-        "relationship_relationships": {
-          "match_mapping_type": "string",
-          "match": "r_*_r_*",
-          "mapping": {
-            "type": "keyword"
-          }
-        }
-      }
-    ]
-  }
-}
-EOF
-
-es_put POST /_reindex << EOF
-{
-  "source": {
-    "index": "entity_${INDEX_VERSION}"
-  },
-  "dest": {
-    "index": "entity_${INDEX_VERSION}_expanded",
-    "pipeline": "entity_${INDEX_VERSION}_expanded"
-  }
-}
-EOF
-
-# provide a way to get entity from id
-es_put PUT /_enrich/policy/entity_${INDEX_VERSION}_expanded_lookup << EOF
-{
-  "match": {
-    "indices": "entity_${INDEX_VERSION}_expanded",
-    "match_field": "id",
-    "enrich_fields": ["id", "slug", "type", "pagerank", "a_*", "r_*"]
-  }
-}
-EOF
-es_put POST /_enrich/policy/entity_${INDEX_VERSION}_expanded_lookup/_execute
-
-# expand target_id into full target entity
 es_put PUT /_ingest/pipeline/m2m_${INDEX_VERSION}_target_expanded << EOF
 {
   "processors": [
     {
       "enrich": {
-        "policy_name": "entity_${INDEX_VERSION}_expanded_lookup",
+        "policy_name": "entity_${INDEX_VERSION}_lookup",
         "field": "target_id",
         "target_field": "target"
       }
@@ -348,6 +161,7 @@ es_put PUT /_ingest/pipeline/m2m_${INDEX_VERSION}_target_expanded << EOF
   ]
 }
 EOF
+
 # create index for m2m
 es_put PUT /m2m_${INDEX_VERSION}_target_expanded << EOF
 {
@@ -381,203 +195,6 @@ es_put PUT /m2m_${INDEX_VERSION}_target_expanded << EOF
       "target_pagerank": {"type": "long"}
     },
     "dynamic_templates": [
-      {
-        "attributes": {
-          "match_mapping_type": "string",
-          "match": "target_a_*",
-          "mapping": {
-            "type": "text",
-            "analyzer": "custom_analyzer",
-            "fields": {
-              "keyword": {
-                "type": "keyword",
-                "ignore_above": 256
-              }
-            }
-          }
-        }
-      },
-      {
-        "relationship_id": {
-          "match_mapping_type": "string",
-          "match": "target_r_*_id",
-          "mapping": {
-            "type": "keyword"
-          }
-        }
-      },
-      {
-        "relationship_type": {
-          "match_mapping_type": "string",
-          "match": "target_r_*_type",
-          "mapping": {
-            "type": "keyword"
-          }
-        }
-      },
-      {
-        "relationship_slug": {
-          "match_mapping_type": "string",
-          "match": "target_r_*_slug",
-          "mapping": {
-            "type": "keyword"
-          }
-        }
-      },
-      {
-        "relationship_pagerank": {
-          "match_mapping_type": "string",
-          "match": "target_r_*_pagerank",
-          "mapping": {
-            "type": "long"
-          }
-        }
-      },
-      {
-        "relationship_attributes": {
-          "match_mapping_type": "string",
-          "match": "target_r_*_a_*",
-          "mapping": {
-            "type": "text",
-            "analyzer": "custom_analyzer",
-            "fields": {
-              "keyword": {
-                "type": "keyword",
-                "ignore_above": 256
-              }
-            }
-          }
-        }
-      },
-      {
-        "relationship_relationships": {
-          "match_mapping_type": "string",
-          "match": "target_r_*_r_*",
-          "mapping": {
-            "type": "keyword"
-          }
-        }
-      }
-    ]
-  }
-}
-EOF
-
-es_put POST /_reindex << EOF
-{
-  "source": {
-    "index": "m2m_${INDEX_VERSION}"
-  },
-  "dest": {
-    "index": "m2m_${INDEX_VERSION}_target_expanded",
-    "pipeline": "m2m_${INDEX_VERSION}_target_expanded"
-  }
-}
-EOF
-
-es_put PUT /_ingest/pipeline/m2m_${INDEX_VERSION}_expanded << EOF
-{
-  "processors": [
-    {
-      "enrich": {
-        "policy_name": "entity_${INDEX_VERSION}_lookup",
-        "field": "source_id",
-        "target_field": "source"
-      }
-    },
-    {
-      "enrich": {
-        "policy_name": "entity_${INDEX_VERSION}_lookup",
-        "field": "target_id",
-        "target_field": "target"
-      }
-    },
-    {
-      "drop": {
-        "description": "drop links with missing entities",
-        "if": "ctx?.source == null || ctx?.target == null"
-      }
-    },
-    {
-      "script": {
-        "lang": "painless",
-        "source": "
-          for (entry in ctx.source.entrySet()) {
-            ctx['source_' + entry.getKey()] = entry.getValue();
-          }
-          ctx.remove('source');
-          for (entry in ctx.target.entrySet()) {
-            ctx['target_' + entry.getKey()] = entry.getValue();
-          }
-          ctx.remove('target');
-        "
-      }
-    }
-  ]
-}
-EOF
-
-# create index for m2m
-es_put PUT /m2m_${INDEX_VERSION}_expanded << EOF
-{
-  "settings": {
-    "analysis": {
-      "analyzer": {
-        "custom_analyzer": {
-          "tokenizer": "custom_tokenizer",
-          "filter": [
-            "lowercase",
-            "asciifolding"
-          ]
-        }
-      },
-      "tokenizer": {
-        "custom_tokenizer": {
-          "type": "simple_pattern_split",
-          "pattern": "[^a-zA-Z0-9']",
-          "lowercase": true
-        }
-      }
-    }
-  },
-  "mappings": {
-    "properties": {
-      "predicate": {"type": "keyword"},
-      "source_id": {"type": "keyword"},
-      "source_type": {"type": "keyword"},
-      "source_slug": {"type": "keyword"},
-      "source_pagerank": {"type": "long"},
-      "target_id": {"type": "keyword"},
-      "target_type": {"type": "keyword"},
-      "target_slug": {"type": "keyword"},
-      "target_pagerank": {"type": "long"}
-    },
-    "dynamic_templates": [
-      {
-        "source_attributes": {
-          "match_mapping_type": "string",
-          "match": "source_a_*",
-          "mapping": {
-            "type": "text",
-            "analyzer": "custom_analyzer",
-            "fields": {
-              "keyword": {
-                "type": "keyword",
-                "ignore_above": 256
-              }
-            }
-          }
-        }
-      },
-      {
-        "source_relationships": {
-          "match_mapping_type": "string",
-          "match": "source_r_*",
-          "mapping": {
-            "type": "keyword"
-          }
-        }
-      },
       {
         "target_attributes": {
           "match_mapping_type": "string",
@@ -614,8 +231,142 @@ es_put POST /_reindex << EOF
     "index": "m2m_${INDEX_VERSION}"
   },
   "dest": {
-    "index": "m2m_${INDEX_VERSION}_expanded",
-    "pipeline": "m2m_${INDEX_VERSION}_expanded"
+    "index": "m2m_${INDEX_VERSION}_target_expanded",
+    "pipeline": "m2m_${INDEX_VERSION}_target_expanded"
+  }
+}
+EOF
+
+es DELETE /_enrich/policy/m2m_${INDEX_VERSION}_target_expanded_lookup
+es_put PUT /_enrich/policy/m2m_${INDEX_VERSION}_target_expanded_lookup << EOF
+{
+  "match": {
+    "indices": "m2m_${INDEX_VERSION}_target_expanded",
+    "match_field": "source_id",
+    "enrich_fields": ["predicate", "target_*"],
+    "query": {"query_string": {"query":"-predicate:inv_*"}}
+  }
+}
+EOF
+
+es POST /_enrich/policy/m2m_${INDEX_VERSION}_target_expanded_lookup/_execute
+
+es_put PUT /_ingest/pipeline/entity_${INDEX_VERSION}_expanded << EOF
+{
+  "processors": [
+    {
+      "enrich": {
+        "policy_name": "m2m_${INDEX_VERSION}_target_expanded_lookup",
+        "field": "id",
+        "target_field": "targets",
+        "max_matches": 128
+      }
+    }
+  ]
+}
+EOF
+
+es_put PUT /entity_${INDEX_VERSION}_expanded << EOF
+{
+  "settings": {
+    "analysis": {
+      "analyzer": {
+        "custom_analyzer": {
+          "tokenizer": "custom_tokenizer",
+          "filter": [
+            "lowercase",
+            "asciifolding"
+          ]
+        }
+      },
+      "tokenizer": {
+        "custom_tokenizer": {
+          "type": "simple_pattern_split",
+          "pattern": "[^a-zA-Z0-9']",
+          "lowercase": true
+        }
+      }
+    }
+  },
+  "mappings": {
+    "properties": {
+      "id": {"type": "keyword"},
+      "type": {"type": "keyword"},
+      "slug": {"type": "keyword"},
+      "pagerank": {"type": "long"},
+      "targets": {
+        "properties": {
+          "id": {"type": "keyword"},
+          "type": {"type": "keyword"},
+          "slug": {"type": "keyword"},
+          "pagerank": {"type": "long"}
+        }
+      }
+    },
+    "dynamic_templates": [
+      {
+        "attributes": {
+          "match_mapping_type": "string",
+          "match": "a_*",
+          "mapping": {
+            "type": "text",
+            "analyzer": "custom_analyzer",
+            "fields": {
+              "keyword": {
+                "type": "keyword",
+                "ignore_above": 256
+              }
+            }
+          }
+        }
+      },
+      {
+        "target_attributes": {
+          "match_mapping_type": "string",
+          "match": "targets.a_*",
+          "mapping": {
+            "type": "text",
+            "analyzer": "custom_analyzer",
+            "fields": {
+              "keyword": {
+                "type": "keyword",
+                "ignore_above": 256
+              }
+            }
+          }
+        }
+      },
+      {
+        "relationships": {
+          "match_mapping_type": "string",
+          "match": "r_*",
+          "mapping": {
+            "type": "keyword"
+          }
+        }
+      },
+      {
+        "target_relationships": {
+          "match_mapping_type": "string",
+          "match": "targets.r_*",
+          "mapping": {
+            "type": "keyword"
+          }
+        }
+      }
+    ]
+  }
+}
+EOF
+
+es_put POST /_reindex << EOF
+{
+  "source": {
+    "index": "entity_${INDEX_VERSION}"
+  },
+  "dest": {
+    "index": "entity_${INDEX_VERSION}_expanded",
+    "pipeline": "entity_${INDEX_VERSION}_expanded"
   }
 }
 EOF
