47c121c143229f49379e32d8bb88bfa2003e2dde
John Erol Evangelista <je.evangelista04@gmail.com>
2026-03-20 13:25:17 -0400

    new mcp version

--- cfde-mcp/src/index.ts
+++ cfde-mcp/src/index.ts
@@ -7,7 +7,7 @@ import { initialize_tools } from './tools/index.js';
 import authMiddleWare from './auth.js';
 const server = new McpServer({
     name: 'cfde-mcp-server',
-    version: '0.1.23'
+    version: '0.1.24'
 });
 
 initialize_tools(server)
