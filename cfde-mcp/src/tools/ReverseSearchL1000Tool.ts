ddc380bd678be8a8955b43e82b8b7eaae1f8b2c1
John Erol Evangelista <je.evangelista04@gmail.com>
2026-03-26 17:41:46 -0400

    fixed reverse search

--- cfde-mcp/src/tools/ReverseSearchL1000Tool.ts
+++ cfde-mcp/src/tools/ReverseSearchL1000Tool.ts
@@ -1,6 +1,21 @@
 import { z } from "zod";
 import { fetch_playbook, get_playbook_description } from "./utils.js";
 
+const getL100Sigs = async (gene: string, dir: string, perturb: string) => {
+    const url = `https:///lincs-reverse-search-dashboard.dev.maayanlab.cloud/api/table/${perturb === 'drug' ? 'cp': 'xpr'}/${dir}/${gene}`
+
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+    }})
+    if (!response.ok) {
+        return Response.json({}, { status: response.status })
+    }
+    const tableDataJson = await response.json()
+    return tableDataJson
+};
+
 const ReverseSearchL1000 = [
   "ReverseSearchL1000",
   {
@@ -11,18 +26,18 @@ const ReverseSearchL1000 = [
         "dir": z.enum(["up", "down", "both"]).optional().describe("Please pick a direction."),
         "perturb": z.enum(['drug', 'CRISPR']).optional().describe("Please pick a the type of signatures you are interested in.")
     },
-    outputSchema: {
-        "function": z.string().describe("Function to run"),
-        "inputType": z.string().describe("The type of input"),
-        "methods": z.array(z.string()).describe("Methods describing the workflow"),
-        "geneSymbol": z.string().optional().nullable().describe("Gene symbol"),
-        "dir": z.enum(["up", "down", "both"]).optional().nullable().describe("Please pick a direction."),
-        "perturb": z.enum(['drug', 'CRISPR']).optional().nullable().describe("Please pick a the type of signatures you are interested in."),
-        "id": z.string(),
-        "output": z.array(z.looseObject({
-                    id: z.string(),
-                  })).describe("Analysis returned by the Playbook Workflow Builder"),
-    }
+    // outputSchema: {
+    //     "function": z.string().describe("Function to run"),
+    //     "inputType": z.string().describe("The type of input"),
+    //     "methods": z.array(z.string()).describe("Methods describing the workflow"),
+    //     "geneSymbol": z.string().optional().nullable().describe("Gene symbol"),
+    //     "dir": z.enum(["up", "down", "both"]).optional().nullable().describe("Please pick a direction."),
+    //     "perturb": z.enum(['drug', 'CRISPR']).optional().nullable().describe("Please pick a the type of signatures you are interested in."),
+    //     "id": z.string(),
+    //     "output": z.array(z.looseObject({
+    //                 id: z.string(),
+    //               })).describe("Analysis returned by the Playbook Workflow Builder"),
+    // }
   },
   async ({geneSymbol, dir, perturb}: {geneSymbol: string, dir: "up" | "down" | "both", perturb: "drug" | "CRISPR"}) => {
     const body = {
@@ -52,6 +67,14 @@ const ReverseSearchL1000 = [
         ]
     }
     const {methods, output, id} = await fetch_playbook(body)
+    const sigs: {[key:string]: any} = {}
+    if (dir === "both") {
+        sigs["up"] = await getL100Sigs(geneSymbol, "up", perturb)
+        sigs["down"] = await getL100Sigs(geneSymbol, "up", perturb)
+    }
+    else {
+        sigs[dir] = await getL100Sigs(geneSymbol, dir, perturb)
+    }
     return {
       content: [
         {
@@ -64,7 +87,8 @@ const ReverseSearchL1000 = [
               dir,
               perturb,
               output,
-              id
+              id,
+              sigs
           })
         }
       ],
@@ -76,7 +100,8 @@ const ReverseSearchL1000 = [
           dir,
           perturb,
           output,
-          id
+          id,
+           sigs
       }
     }
   }
