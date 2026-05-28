53b1517d3cacb24d163c476cb3ff4de49f99d189
Daniel Clarke <u8sand@gmail.com>
2026-05-28 15:28:43 -0400

    Add script to help identify non-archived assets

---
+++ database/scruitinize_dcc_assets.py
@@ -0,0 +1,11 @@
+#%%
+from ingest_common import current_dcc_assets
+
+#%%
+dcc_assets = current_dcc_assets()
+
+#%%
+# Ingest C2M2
+for (short_label, filetype), assets in dcc_assets.groupby(['short_label', 'filetype']):
+  if assets['filename'].nunique() != assets.shape[0] or (filetype=='C2M2' and assets.shape[0] > 1):
+    print(assets[['short_label', 'filetype', 'filename', 'lastmodified']])
