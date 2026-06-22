59579addc6869cf3d1dc88a71a5595d64212478c
John Erol Evangelista <je.evangelista04@gmail.com>
2026-06-22 14:39:33 -0400

    removed search from docs

--- drc-portals/app/data/processed/SearchParamSearchField.tsx
+++ drc-portals/app/data/processed/SearchParamSearchField.tsx
@@ -13,6 +13,7 @@ export default function SearchParamSearchField(props: Omit<React.ComponentProps<
     pathname === '/'
     || pathname.startsWith('/info')
     || pathname === '/data'
+    || pathname.includes("/documentation")
     || /^(\/data)?(\/graph|\/enrichment|\/documentation|\/cross|\/explorer|\/processed\/search\/[^\/]+\/(?!c2m2)[^\/]+)$/.exec(pathname) !== null
   ) return null
   return (
