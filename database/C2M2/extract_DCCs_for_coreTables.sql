436ecf944091cc05f8746b30a32de884fe80b02b
mano-at-sdsc <mano@sdsc.edu>
2026-06-26 17:14:49 -0700

    Added code to generate pivote table

--- database/C2M2/extract_DCCs_for_coreTables.sql
+++ database/C2M2/extract_DCCs_for_coreTables.sql
@@ -72,8 +72,11 @@ c2m2.id_namespace_dcc_id on idn.id_namespace = c2m2.id_namespace_dcc_id.id_names
 Generate code to output a table with the three columns table_name, id_namespace and dcc_short_label that will be union of such output by looping over all the tables in the two arrays.
 */
 
+DROP FUNCTION IF EXISTS c2m2.get_table_namespaces();
+
 CREATE OR REPLACE FUNCTION c2m2.get_table_namespaces()
 RETURNS TABLE (
+    srno            integer,
     table_name      text,
     id_namespace    varchar,
     dcc_short_label varchar
@@ -84,6 +87,8 @@ DECLARE
     tbl text;
     namespace_col text;
     sql text;
+    v_srno integer := 0;
+
     tables_core text[] := ARRAY[
         --- 'dcc',
         'project', 'subject', 'biosample', 'collection', 'file'
@@ -98,8 +103,9 @@ DECLARE
         'subject_in_collection',
 
         'biosample_from_subject', 'biosample_disease',
-        'biosample_gene', 'biosample_protein', 'biosample_ptm',
-        'biosample_substance', 'biosample_in_collection',
+        'biosample_gene', 'biosample_protein',
+        'biosample_ptm', 'biosample_substance',
+        'biosample_in_collection',
 
         'collection_anatomy', 'collection_biofluid',
         'collection_compound', 'collection_disease',
@@ -117,10 +123,14 @@ DECLARE
 BEGIN
     FOREACH tbl IN ARRAY tables_core
     LOOP
-        RAISE NOTICE 'Processing core table: %', tbl;
-        sql := format($f$
-            SELECT DISTINCT
-                %L AS table_name,
+        v_srno := v_srno + 1;
+
+        RAISE NOTICE 'Processing table % (SrNo=%)', tbl, v_srno;
+
+        sql := format($fmt$
+            SELECT
+                %s::integer AS srno,
+                %L::text AS table_name,
                 idn.id_namespace,
                 m.dcc_short_label
             FROM (
@@ -130,19 +140,24 @@ BEGIN
             LEFT JOIN c2m2.id_namespace_dcc_id m
                 ON idn.id_namespace = m.id_namespace_id
             ORDER BY m.dcc_short_label
-        $f$, tbl, tbl);
+        $fmt$, v_srno, tbl, tbl);
 
         RETURN QUERY EXECUTE sql;
     END LOOP;
 
     FOREACH tbl IN ARRAY tables_core_related
     LOOP
-        namespace_col := split_part(tbl, '_', 1) || '_id_namespace';
-        RAISE NOTICE 'Processing related table: %, namespace column: %', tbl, namespace_col;
+        v_srno := v_srno + 1;
 
-        sql := format($f$
-            SELECT DISTINCT
-                %L AS table_name,
+        namespace_col := split_part(tbl, '_', 1) || '_id_namespace';
+
+        RAISE NOTICE 'Processing table % (SrNo=%), namespace column=%',
+                     tbl, v_srno, namespace_col;
+
+        sql := format($fmt$
+            SELECT
+                %s::integer AS srno,
+                %L::text AS table_name,
                 idn.id_namespace,
                 m.dcc_short_label
             FROM (
@@ -152,13 +167,113 @@ BEGIN
             LEFT JOIN c2m2.id_namespace_dcc_id m
                 ON idn.id_namespace = m.id_namespace_id
             ORDER BY m.dcc_short_label
-        $f$, tbl, namespace_col, tbl);
+        $fmt$, v_srno, tbl, namespace_col, tbl);
 
         RETURN QUERY EXECUTE sql;
     END LOOP;
+
+    RETURN;
 END;
 $$;
 
 --- Now call/execute:
 --- SELECT * FROM c2m2.get_table_namespaces() ORDER BY table_name, id_namespace;
-SELECT * FROM c2m2.get_table_namespaces() ORDER BY table_name, dcc_short_label;
+--- SELECT * FROM c2m2.get_table_namespaces() ORDER BY table_name, dcc_short_label;
+--- SELECT distinct table_name, dcc_short_label FROM c2m2.get_table_namespaces() ORDER BY table_name, dcc_short_label;
+--- SELECT * FROM c2m2.get_table_namespaces() ORDER BY srno, table_name, id_namespace;
+SELECT DISTINCT srno, table_name, dcc_short_label FROM c2m2.get_table_namespaces() ORDER BY srno, dcc_short_label;
+
+/*
+
+Thanks. Now my output looks like: Many lines omitted
+ srno |          table_name           | dcc_short_label 
+------+-------------------------------+-----------------
+    1 | project                       | 4DN
+    2 | subject                       | MoTrPAC
+    3 | biosample                     | Metabolomics
+    3 | biosample                     | MoTrPAC
+    4 | collection                    | SPARC
+    5 | file                          | KidsFirst
+    5 | file                          | MoTrPAC
+    5 | file                          | SPARC
+    6 | collection_defined_by_project | 4DN
+    6 | collection_defined_by_project | ExRNA
+    7 | subject_disease               | Bridge2AI
+    7 | subject_disease               | Metabolomics
+    8 | subject_phenotype             | Metabolomics
+    9 | subject_race                  | Bridge2AI
+    9 | subject_race                  | SenNet
+   10 | subject_role_taxonomy         | 4DN
+   10 | subject_role_taxonomy         | SenNet
+   12 | subject_in_collection         | MoTrPAC
+   12 | subject_in_collection         | SenNet
+   12 | subject_in_collection         | SPARC
+
+Some more rows. 
+I want an output in a compact format. 
+May be save this table as a temporary table or as part of CTE, from which create output like, 
+where, the first column will be the name of the table, and the rest of the columns will be 
+distinct dcc_short_label. In a row for a table, like project or subject, 
+if the above table has a dcc_short_label, then put 'y' in the new compact table, 
+else leave empty. Is that possible in sql. I think the first task will be to get 
+the unique complete list of dcc_short_label.
+
+*/
+
+DROP TABLE IF EXISTS tmp_table_namespaces;
+
+CREATE TEMP TABLE tmp_table_namespaces AS
+SELECT DISTINCT
+       srno,
+       table_name,
+       dcc_short_label
+FROM c2m2.get_table_namespaces()
+WHERE dcc_short_label IS NOT NULL ORDER BY srno, dcc_short_label;
+
+\copy (SELECT * from tmp_table_namespaces) TO tmp_table_namespaces.tsv WITH DELIMITER E'\t' NULL '' CSV HEADER;
+
+
+DO $$
+DECLARE
+    cols text;
+    sql  text;
+BEGIN
+    SELECT string_agg(
+               format(
+                   'max(CASE WHEN dcc_short_label = %L THEN ''y'' END) AS %I',
+                   dcc_short_label,
+                   dcc_short_label
+               ),
+               E',\n       '
+               ORDER BY dcc_short_label
+           )
+    INTO cols
+    FROM (
+        SELECT DISTINCT dcc_short_label
+        FROM tmp_table_namespaces
+        ORDER BY dcc_short_label
+    ) d;
+
+    sql := format(
+$fmt$
+DROP TABLE IF EXISTS tmp_table_namespaces_pivot;
+
+CREATE TEMP TABLE tmp_table_namespaces_pivot AS
+SELECT
+       srno,
+       table_name,
+       %s
+FROM tmp_table_namespaces
+GROUP BY srno, table_name
+ORDER BY srno;
+$fmt$,
+        cols
+    );
+
+    RAISE NOTICE 'Executing:%', E'\n' || sql;
+
+    EXECUTE sql;
+END $$;
+
+SELECT * FROM tmp_table_namespaces_pivot ORDER BY srno;
+\copy (SELECT * FROM tmp_table_namespaces_pivot ORDER BY srno) TO tmp_table_namespaces_pivot.tsv WITH DELIMITER E'\t' NULL '' CSV HEADER;
