98cc55701e944d0ac310db55da51b01365517a71
Daniel Clarke <u8sand@gmail.com>
2026-07-21 14:48:56 -0400

    Validate our own ARK NAAN

--- drc-portals/app/ark/[ns]/[...path]/route.ts
+++ drc-portals/app/ark/[ns]/[...path]/route.ts
@@ -3,14 +3,11 @@ import type { NextRequest } from "next/server";
 export async function GET(_req: NextRequest, ctx: { params: Promise<{ ns: string, path: string[]  }> }) {
   const { ns, path } = await ctx.params
   const id = path.join('/')
-  // validate OUR ark namespace when we have one
-  if (/^ark:.+?$/.exec(ns) !== null) {
-    if (id === 'servicestatus') {
-      return Response.json({ 'status': 'ok' }, { status: 200 })
-    } else {
-      return Response.json({ 'error': 'NotFound' }, { status: 404 })
-    }
-  } else {
-    return Response.redirect(`https://n2t.net/${ns}/${id}`)
+  // validate our NAAN
+  if (ns !== 'ark:61568') return Response.redirect(`https://n2t.net/${ns}/${id}`)
+  if (id === 'servicestatus') {
+    return Response.json({ 'status': 'ok' }, { status: 200 })
   }
+  // TODO: serve ARK URIs from DB
+  return Response.json({ 'error': 'NotFound' }, { status: 404 })
 }
