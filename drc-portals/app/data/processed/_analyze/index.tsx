2060c6cacc3cc961002dfe7e1760eebd88665985
Daniel Clarke <u8sand@gmail.com>
2026-07-27 13:21:39 -0400

    Fixup uniprotkb card

--- drc-portals/app/data/processed/_analyze/index.tsx
+++ drc-portals/app/data/processed/_analyze/index.tsx
@@ -140,16 +140,16 @@ const modules: {
     </CardButton>,
   },
   {
-    compatible: (item) => item.type === 'protein' && typeof item.a_synonyms !== 'undefined' /* these are from the C2M2 and the ID is a UniProtKB id */,
+    compatible: (item) => item.type === 'protein' && typeof item.a_uniprotkb !== 'undefined',
     button: ({ item }) => <CardButton
-      icon={<img src={'/img/icons/uniprot.svg'} height={64} alt="Gene and Drug Landing Page Aggegrator" className="p-2" />}
+      icon={<img src={'/img/icons/uniprot.svg'} height={64} alt="UniProt Knowledge Base" className="p-2" />}
       title="UniProtKB"
-      description={<>UniProt is the world's leading high-quality, comprehensive and freely accessible resource of protein sequence and functional information Knowledge Base. Find out more about {item.a_id}.</>}
+      description={<>UniProt is the world's leading high-quality, comprehensive and freely accessible resource of protein sequence and functional information Knowledge Base. Find out more about {item.a_uniprotkb}.</>}
     >
       <Button
         color="secondary"
         size="small"
-        href={`https://www.uniprot.org/uniprotkb/${encodeURIComponent(item.a_id)}/entry`}
+        href={`https://www.uniprot.org/uniprotkb/${encodeURIComponent(item.a_uniprotkb)}/entry`}
         target="_blank"
       >Submit</Button>
     </CardButton>,
